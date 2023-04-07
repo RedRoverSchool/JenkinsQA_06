9926bb79c40ffb09c0d6a3918508dec9b00b93f6
Sergey.D <sergei.demyanenko@gmail.com>
2023-04-06 22:55:55 -0400

    Add second test

--- src/test/java/school/redrover/SergeyDTest.java
+++ src/test/java/school/redrover/SergeyDTest.java
@@ -34,4 +34,29 @@ public class SergeyDTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testSecond() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("Web form", title);
+
+        Thread.sleep(2000);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
+
+        driver.quit();
+    }
+
 }
