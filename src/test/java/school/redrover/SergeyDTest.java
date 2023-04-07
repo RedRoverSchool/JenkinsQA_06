fbc23fecc09e09a782887cb8765228e0305abf96
EmelR8 <onizuh8@gmail.com>
2023-04-07 13:33:05 +0300

    add testAndreyD

--- src/test/java/school/redrover/SergeyDTest.java
+++ src/test/java/school/redrover/SergeyDTest.java
@@ -10,6 +10,8 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class SergeyDTest {
 
     @Ignore
@@ -63,4 +65,24 @@ public class SergeyDTest {
 
         driver.quit();
     }
+
+        @Test
+        public void testAndreyD() throws InterruptedException {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+            WebDriver driver = new ChromeDriver(chromeOptions);
+            driver.get("https://openweathermap.org/");
+
+            Thread.sleep(5000);
+
+            WebElement button = driver.findElement(By.xpath("//*[@id=\"desktop-menu\"]/ul/li[1]/a"));
+            button.click();
+
+            String title = driver.getTitle();
+            assertEquals("OpenWeatherMap API guide - OpenWeatherMap", title);
+
+            driver.quit();
+
+        }
 }
