360f1e2b147862bbffbcfe8a0d751155f92f0084
Sergey.D <sergei.demyanenko@gmail.com>
2023-04-06 21:51:28 -0400

    First test

---
+++ src/test/java/school/redrover/SergeyDTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SergeyDTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
+
+        driver.quit();
+    }
+}
