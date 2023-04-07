c185342ad763acadbee2ec86f40f46be4e338f14
Sergey.D <sergei.demyanenko@gmail.com>
2023-04-06 22:07:40 -0400

    First test

--- src/test/java/school/redrover/SergeyDTest.java
+++ src/test/java/school/redrover/SergeyDTest.java
@@ -1,22 +1,23 @@
 package school.redrover;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SergeyDTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void testFirst() throws InterruptedException {
 
-        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://www.google.com/");
 
