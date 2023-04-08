98a2291a72e2637d064576507ec95b76c4d51fe0
thestaskalinin <thestaskalinin@gmail.com>
2023-04-08 00:30:53 -0400

    Fixed errors

--- src/test/java/school/redrover/AlexLeoEpicGroupTest.java
+++ src/test/java/school/redrover/AlexLeoEpicGroupTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
