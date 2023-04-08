5364f1c76e907f0d05187e0b728353f5085d2ec9
Olga Kovalenko <gwch@tut.by>
2023-04-08 11:16:19 +0300

    1st-test

--- src/test/java/school/redrover/HelloWorldTest.java
+++ src/test/java/school/redrover/HelloWorldTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class HelloWorldTest {
 
     @Test
-    public void getUrlKovalenko (){
+    public void getUrlKovalen (){
         for (int i=0;i<3;i++){
             WebDriver driver = new ChromeDriver();
             driver.get("http:/google.com");
