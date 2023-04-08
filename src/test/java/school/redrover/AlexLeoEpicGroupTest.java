ddb6f7b4009e9471365c591726e662a4c6939925
thestaskalinin <thestaskalinin@gmail.com>
2023-04-08 00:43:44 -0400

    fixed

--- src/test/java/school/redrover/AlexLeoEpicGroupTest.java
+++ src/test/java/school/redrover/AlexLeoEpicGroupTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class AlexLeoEpicGroupTest {
@@ -25,6 +26,47 @@ public class AlexLeoEpicGroupTest {
         driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
         WebElement icon = driver.findElement(By.xpath("//span[@class='onsale']"));
         Assert.assertEquals(icon.getText(), "Sale!");
+        driver.quit();
+    }
+
+
+    @Test
+    public void verifySaleSTas_TC_001_04() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://askomdch.com");
+        driver.manage().window().maximize();
+        WebElement saleSign = driver.findElement(By.className("onsale"));
+        Assert.assertEquals(saleSign.getText(), "Sale!");
+
+        driver.quit();
+    }
+    @Test
+    public void buttonOpened(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://askomdch.com/");
+        String url = "https://askomdch.com/store";
+
+        driver.findElement(By.cssSelector("div.wp-block-button>a[href=\"/store\"]")).click();
+        Assert.assertEquals(url, "https://askomdch.com/store");
+        driver.quit();
+    }
+
+    @Test
+    public void colourTest_TC_007_23() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        driver.get("https://askomdch.com/");
+        driver.findElement(By.cssSelector("a[href*='men']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("button[value='Search']")).
+                getCssValue("background-color"), "rgba(49, 151, 214, 1)");
         driver.quit();
     }
 }
