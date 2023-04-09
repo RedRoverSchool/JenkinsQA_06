70a2c4c63247e63ee65821fbe8dd130114ebd7b7
Yuliya48 <kolyabina8@gmail.com>
2023-04-09 08:17:58 -0400

    add new test

--- src/test/java/school/redrover/GroupJavaQaTeamTest.java
+++ src/test/java/school/redrover/GroupJavaQaTeamTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -14,7 +16,10 @@ public class GroupJavaQaTeamTest {
     @Test
     public void testFirst() throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
         String title = driver.getTitle();
@@ -37,7 +42,7 @@ public class GroupJavaQaTeamTest {
         String value = message.getText();
         assertEquals("Received!", value);
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         driver.quit();
     }
