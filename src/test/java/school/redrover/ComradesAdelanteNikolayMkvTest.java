1cec8eced446ef67ae07a8e9e9eed54a258682c0
Nikolai Markow <i@nikomar.ru>
2023-04-09 16:48:35 +0700

    Add first test jenkins

--- src/test/java/school/redrover/ComradesAdelanteNikolayMkvTest.java
+++ src/test/java/school/redrover/ComradesAdelanteNikolayMkvTest.java
@@ -31,17 +31,18 @@ public class ComradesAdelanteNikolayMkvTest {
         Thread.sleep(2000);
         WebElement searchBox = driver.findElement(
                 By.xpath("//input[@id='searchboxinput']"));
-
         searchBox.sendKeys("Бердск");
         searchBox.sendKeys(Keys.RETURN);
-
         Thread.sleep(5000);
 
-        String expectedResult = "https://www.google.com/maps/place/%D0%91%D0%B5%D1%80%D0%B4%D1%81%D0%BA,+%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB./@54.7646848,83.0277085,12z/data=!3m1!4b1!4m6!3m5!1s0x42dfcf8ff0ea4713:0x977d7d9948dc1705!8m2!3d54.7750638!4d83.0800315!16zL20vMDc2MXZt?hl=RU";
-        String actualResult = driver.getCurrentUrl();
+        WebElement text =  driver.findElement(By.xpath("//*[@id='QA0Szd']/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1"));
+
+        String expectedResult = "Бердск";
+        String actualResult = text.getText();
 
         Assert.assertEquals(actualResult,expectedResult);
 
         driver.quit();
     }
+
 }
