1b7edf58cd564753a155e5f1ced6ec66087bb4e3
Yuliya48 <kolyabina8@gmail.com>
2023-04-09 07:49:26 -0400

    add new test

--- src/test/java/school/redrover/GroupJavaQaTeamTest.java
+++ src/test/java/school/redrover/GroupJavaQaTeamTest.java
@@ -4,35 +4,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.Ignore;
+
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class GroupJavaQaTeamTest {
-    @Ignore
+
     @Test
-    public void testCheckTheCartIsEmpty() throws InterruptedException {
+    public void testFirst() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
 
-        String expectedResult = "There's Nothing Here Yet";
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
 
-        driver.get("https://www.homedepot.com/");
+        Thread.sleep(3000);
 
-        WebElement searchButtonCart = driver.findElement(By.xpath("//span[@class ='MyCart__label' ]"));
-        searchButtonCart.click();
+        textBox.sendKeys("Selenium");
 
-        WebElement searchText = driver.findElement(
-                By.xpath("//div[@class='empty-cart__message empty-cart__message--primary' and text()=\"There's Nothing Here Yet\"]"));
+        Thread.sleep(3000);
+
+        submitButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
 
         Thread.sleep(2000);
-
-        String actualResult = searchText.getText();
-        Assert.assertEquals(actualResult, expectedResult);
 
         driver.quit();
     }
