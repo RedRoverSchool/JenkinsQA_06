705859336ab459a5e5f043c892f75945d142fd54
Vidim83 <08091985n@gmail.com>
2023-04-09 21:43:14 +0300

    add first test

--- src/test/java/school/redrover/VidimTest.java
+++ src/test/java/school/redrover/VidimTest.java
@@ -18,30 +18,25 @@ public class VidimTest {
     public void checkWrongEmail() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
-     chromeOptions.addArguments("--remote-allow-origins=*", "--window-size=1920,1080");
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://redrover.school");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         WebElement but = driver.findElement(By.xpath("//*[@id=\"rec544122398\"]/div/div/div[3]/a"));
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
         but.click();
-
         WebElement email = driver.findElement(By.xpath("//*[@id=\"form544122415\"]/div[2]/div[1]/div/input"));
         email.sendKeys("vidim@gmail.");
-
         WebElement name = driver.findElement(By.xpath("//*[@id=\"form544122415\"]/div[2]/div[2]/div/input"));
         name.sendKeys("Vitalii");
-
         WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"form544122415\"]/div[2]/div[3]/div/label"));
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(25000));
         checkbox.click();
 
         WebElement button = driver.findElement(By.xpath("//*[@id=\"form544122415\"]/div[2]/div[5]/button"));
         button.click();
-
         WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"tilda-popup-for-error\"]/div[1]/p"));
         Thread.sleep(1000);
         Assert.assertEquals(errorMessage.getText(), "Please enter a valid email address");
