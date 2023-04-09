06dcab1f45f72b5b1a6111e7040e47be580361d7
Maria Shy <24011989@mail.ru>
2023-04-09 10:54:28 +0300

    fix imports

--- src/test/java/school/redrover/GroupMaxTest.java
+++ src/test/java/school/redrover/GroupMaxTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GroupMaxTest {
 
