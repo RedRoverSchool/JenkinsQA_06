59c8347328aac31fe0cad3a326bdc005f956f600
Kater1naG <katerinagilels@gmail.com>
2023-04-10 15:09:11 -0400

    add another copy test

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -1,45 +1,67 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupForwardTest {
 
+//    @Test
+//    public void testPageHasCommonComponents() throws InterruptedException {
+//
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://yummymarket.com/");
+//        Thread.sleep(5000);
+//
+//
+//        WebElement searchField = driver.findElement(
+//                By.xpath("//input[@type = 'search']")
+//        );
+//        WebElement label = driver.findElement(
+//                By.xpath("//img[@alt= 'Yummy Market']")
+//        );
+//        WebElement navigationBar = driver.findElement(
+//                By.xpath("//nav[@id = 'site-navigation']")
+//        );
+//        WebElement copyrightInformation = driver.findElement(
+//                By.xpath("//div[@class='copyright-bar']")
+//        );
+//
+//        Assert.assertEquals(driver.getTitle(), "Home - Yummy Market");
+//
+//        Assert.assertTrue(searchField.isDisplayed());
+//        Assert.assertTrue(label.isDisplayed());
+//        Assert.assertTrue(navigationBar.isDisplayed());
+//
+//        Assert.assertEquals(copyrightInformation.getText(), "© 2023 Yummy Market inc. All Rights Reserved.");
+//        Assert.assertTrue(copyrightInformation.isDisplayed());
+//
+//        driver.quit();
+//    }
+
     @Test
-    public void testPageHasCommonComponents() throws InterruptedException {
+    public void firstTestCopy() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://yummymarket.com/");
-        Thread.sleep(5000);
 
+        driver.get("https://www.google.com/");
 
-        WebElement searchField = driver.findElement(
-                By.xpath("//input[@type = 'search']")
-        );
-        WebElement label = driver.findElement(
-                By.xpath("//img[@alt= 'Yummy Market']")
-        );
-        WebElement navigationBar = driver.findElement(
-                By.xpath("//nav[@id = 'site-navigation']")
-        );
-        WebElement copyrightInformation = driver.findElement(
-                By.xpath("//div[@class='copyright-bar']")
-        );
+        WebElement textBox = driver.findElement(By.name("q"));
 
-        Assert.assertEquals(driver.getTitle(), "Home - Yummy Market");
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
 
-        Assert.assertTrue(searchField.isDisplayed());
-        Assert.assertTrue(label.isDisplayed());
-        Assert.assertTrue(navigationBar.isDisplayed());
+        Thread.sleep(2000);
 
-        Assert.assertEquals(copyrightInformation.getText(), "© 2023 Yummy Market inc. All Rights Reserved.");
-        Assert.assertTrue(copyrightInformation.isDisplayed());
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
 
         driver.quit();
     }
-
 }
