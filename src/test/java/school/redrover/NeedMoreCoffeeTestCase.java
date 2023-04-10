cd7cbb06caeac14d31f3db9cf017a79d67a58c34
antoninabozhok <antoninabozhok@gmail.com>
2023-04-11 00:17:08 +0700

    Delete test 006

--- src/test/java/school/redrover/NeedMoreCoffeeTestCase.java
+++ src/test/java/school/redrover/NeedMoreCoffeeTestCase.java
@@ -7,30 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 import static org.testng.Assert.assertEquals;
 
 public class NeedMoreCoffeeTestCase {
-
-        @Test
-        public void test_006() throws InterruptedException {
-            ChromeOptions chromeOptions = new ChromeOptions(); // это опции для запуска на сервеере
-            chromeOptions.addArguments("--headless", "--window-size=1920,1080"); //чтобы не открывалось окно браузера и с одним размером окна. Потому что на сервере не установлен этот браузер. Тест упадет, если оставить открытие браузера
-
-            WebDriver driver = new ChromeDriver(chromeOptions);
-            driver.manage().window().maximize();
-            driver.get("https://askomdch.com/");
-            assertEquals(driver.getTitle(), "AskOmDch – Become a Selenium automation expert!");
-            Thread.sleep(1000);
-            String textBox = driver.findElement(By.xpath("//h2[@class='has-text-align-center']")).getText();
-            Thread.sleep(500);
-            assertEquals(textBox, "Featured Products");
-            List<WebElement> products = driver.findElements(By.className("type-product"));
-            assertEquals(products.size(), 5);
-            //driver.findElement(By.xpath("//li[@class='ast-article-single product type-product post-1215 status-publish first instock product_cat-men product_cat-womens-shoes has-post-thumbnail featured taxable shipping-taxable purchasable product-type-simple']"));
-            driver.quit();
-        }
     @Test
     public void testBozhok() throws InterruptedException{
         ChromeOptions chromeOptions = new ChromeOptions(); // это опции для запуска на сервеере
