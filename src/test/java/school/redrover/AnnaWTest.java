ef5e65750e5b23373eddf5b224c5edfe0325e0e1
Anna <am-rabota@mail.ru>
2023-04-09 19:24:25 -0700

    Add first test

---
+++ src/test/java/school/redrover/AnnaWTest.java
@@ -0,0 +1,10 @@
+package school.redrover;
+
+import org.testng.annotations.Test;
+
+public class AnnaWTest {
+    @Test
+    public void annaFirstTest (){
+        System.out.println("First test!");
+    }
+}
