12065cf5dfe5d3d37464870fa5eceafdfd61adf9
Kater1naG <katerinagilels@gmail.com>
2023-04-10 16:27:05 -0400

    my first commit

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -1,4 +1,32 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class GroupForwardTest {
+    @Test
+    public void testCopy() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
+
+        driver.quit();
+    }
 }
