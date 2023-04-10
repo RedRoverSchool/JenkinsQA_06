5147504b4e6ab1477687b9baea2d38a30a18648e
Kater1naG <katerinagilels@gmail.com>
2023-04-10 15:23:38 -0400

    commit

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -10,39 +10,6 @@ import org.testng.annotations.Test;
 
 public class GroupForwardTest {
 
-//    @Test
-//    public void testPageHasCommonComponents() throws InterruptedException {
-//
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://yummymarket.com/");
-//        Thread.sleep(5000);
-//
-//
-//        WebElement searchField = driver.findElement(
-//                By.xpath("//input[@type = 'search']")
-//        );
-//        WebElement label = driver.findElement(
-//                By.xpath("//img[@alt= 'Yummy Market']")
-//        );
-//        WebElement navigationBar = driver.findElement(
-//                By.xpath("//nav[@id = 'site-navigation']")
-//        );
-//        WebElement copyrightInformation = driver.findElement(
-//                By.xpath("//div[@class='copyright-bar']")
-//        );
-//
-//        Assert.assertEquals(driver.getTitle(), "Home - Yummy Market");
-//
-//        Assert.assertTrue(searchField.isDisplayed());
-//        Assert.assertTrue(label.isDisplayed());
-//        Assert.assertTrue(navigationBar.isDisplayed());
-//
-//        Assert.assertEquals(copyrightInformation.getText(), "© 2023 Yummy Market inc. All Rights Reserved.");
-//        Assert.assertTrue(copyrightInformation.isDisplayed());
-//
-//        driver.quit();
-//    }
-
     @Test
     public void copyTest() throws InterruptedException {
 
