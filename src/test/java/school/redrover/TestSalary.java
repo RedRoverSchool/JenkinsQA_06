a1830384c85574202fa093cd3827bdf898f61de5
root <415816@bk.ru>
2023-04-11 00:14:20 +0600

    create new file

--- src/test/java/school/redrover/TestSalary.java
+++ src/test/java/school/redrover/TestSalary.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class testSalary {
+public class TestSalary {
     @Test
     public void testSalary() throws InterruptedException {
 
