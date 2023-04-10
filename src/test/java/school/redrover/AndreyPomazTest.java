1aea06b73a298dfd0081f060a84a628643c6be2e
AndreyPomaz <andreypomaz@gmail.com>
2023-04-10 08:17:56 +0700

    add tests

--- src/test/java/school/redrover/AndreyPomazTest.java
+++ src/test/java/school/redrover/AndreyPomazTest.java
@@ -33,15 +33,13 @@ public class AndreyPomazTest {
         checkBox.click();
 
         WebElement buttonW = driver.findElement(By.className("t-submit"));
-//        Assert.assertEquals(buttonW.getText(), "ХОЧУ УЧИТЬСЯ");
         buttonW.click();
         Thread.sleep(2000);
 
         WebElement error = driver.findElement(By.className("t-input-error"));
-        Assert.assertEquals(error.getText(), "Укажите, пожалуйста, корректный email");
+        Assert.assertEquals(error.getText(), "Please enter a valid email address");
         driver.quit();
     }
-
     @Test
     public void testSecond_Selenium() throws InterruptedException {
 
