ada6bdaccb2d9d6ae3fd3391d6792bef39c1b0af
AlexandraNovikA26 <ya.sasha-novik@yandex.by>
2023-04-10 18:32:48 -0500

    commit

--- src/test/java/school/redrover/GroupJavaJitsu.java
+++ src/test/java/school/redrover/GroupJavaJitsu.java
@@ -139,5 +139,37 @@ public class GroupJavaJitsu {
         driver.quit();
 
     }
+    @Test
+    public void testAlex() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://www.globalsqa.com/");
+        WebElement element= driver.findElement(By.xpath("//a[@href=\"https://www.globalsqa.com/training/appium-online-training/\"]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        Thread.sleep(2000);
+        element.click();
+        WebElement schedule=driver.findElement(By.xpath("//li[@id='Batch Schedule']"));
+        schedule.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", schedule);
+        WebElement enroll=driver.findElement(By.xpath("(//a[@href=\"https://www.instamojo.com/globalsqa/appium-training/\"])[2]"));
+        enroll.click();
+//        WebElement cart=driver.findElement(By.xpath("//button[@id=\"add-to-cart-btn\"]"));
+//        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id=\"add-to-cart-btn\"]")));
+//
+//        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(cart));
+//        cart.click();
+//        WebElement quantity=driver.findElement(By.xpath("//span[@id=\"cart-quantity-increment-NaN\"]"));
+//        quantity.click();
+//        quantity.click();
+//        driver.findElement(By.xpath("//span[contains(text(),'Buy Now')]")).click();
+//        driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Alexandra");
+//        driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Novik");
+//        driver.findElement(By.xpath("//span[contains(text(),'Continue Shopping')]")).click();
+//        WebElement welcome=driver.findElement(By.xpath("//span[contains(text(),'Welcome!')]"));
+//        WebElement description= driver.findElement(By.xpath("//p[contains(text(),'Appium is the mobile automation testing API that i')]"));
+//        Assert.assertTrue(description.isDisplayed());
+        driver.quit();
+    }
 }
 
