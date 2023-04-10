65c40717c684fd7419a97f99ef7ba489c085be14
Kater1naG <katerinagilels@gmail.com>
2023-04-10 14:45:04 -0400

    first commit

---
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -0,0 +1,45 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupForwardTest {
+
+    @Test
+    public void testPageHasCommonComponents() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://yummymarket.com/");
+        Thread.sleep(5000);
+
+
+        WebElement searchField = driver.findElement(
+                By.xpath("//input[@type = 'search']")
+        );
+        WebElement label = driver.findElement(
+                By.xpath("//img[@alt= 'Yummy Market']")
+        );
+        WebElement navigationBar = driver.findElement(
+                By.xpath("//nav[@id = 'site-navigation']")
+        );
+        WebElement copyrightInformation = driver.findElement(
+                By.xpath("//div[@class='copyright-bar']")
+        );
+
+        Assert.assertEquals(driver.getTitle(), "Home - Yummy Market");
+
+        Assert.assertTrue(searchField.isDisplayed());
+        Assert.assertTrue(label.isDisplayed());
+        Assert.assertTrue(navigationBar.isDisplayed());
+
+        Assert.assertEquals(copyrightInformation.getText(), "© 2023 Yummy Market inc. All Rights Reserved.");
+        Assert.assertTrue(copyrightInformation.isDisplayed());
+
+        driver.quit();
+    }
+
+}
