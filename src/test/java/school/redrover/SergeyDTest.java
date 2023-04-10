cfeb017ea603a3596d5497511f93329a52c2f6f2
Vlad2034 <vladislav.li.72@gmail.com>
2023-04-10 09:29:42 -0700

    Commit and push assignment

--- src/test/java/school/redrover/SergeyDTest.java
+++ src/test/java/school/redrover/SergeyDTest.java
@@ -66,4 +66,31 @@ public class SergeyDTest {
         driver.quit();
     }
 
+    @Test
+    // Commit and push assignment
+    public void testVladLi() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://www.yahoo.com/?guccounter=1");
+
+        WebElement textBox = driver.findElement(By.name("p"));
+
+        textBox.sendKeys("Amazon");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//span[text() = 'Amazon.com® Official Site - Fast Free Delivery with Prime']"));
+
+        Assert.assertEquals(text.getText(), "Amazon.com® Official Site - Fast Free Delivery with Prime");
+
+
+        driver.quit();
+    }
+
 }
