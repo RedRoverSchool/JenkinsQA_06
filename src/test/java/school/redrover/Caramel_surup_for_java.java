99f9af6a33da6a8e3a4f40e00a1c2b408caa5679
Artyom Dulya <fecc1612@gmail.com>
2023-04-11 05:43:22 +0500

    commit

--- src/test/java/school/redrover/Caramel_surup_for_java.java
+++ src/test/java/school/redrover/Caramel_surup_for_java.java
@@ -5,27 +5,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 public class Caramel_surup_for_java {
 
     @Test
     public void artyomDulyaOpenWeatherGuideClick () throws InterruptedException {
 
+        String expectedResultUrl = "https://openweather.co.uk/";
+        String expectedResultBanner = "Complete spectrum of weather data solutions.";
+
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
         driver.get("https://openweathermap.org/");
         Thread.sleep(5000);
-        WebElement orderSite = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
-        orderSite.click();
-        WebElement turnkeyBusinessCard = driver.findElement(
+        WebElement guide = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
+
+        guide.click();
+
+        WebElement complexEnterprise = driver.findElement(
                 By.xpath("//main//div[2]/div/div/p[1]/a[text()='complex enterprise systems']"));
-        turnkeyBusinessCard.click();
+        complexEnterprise.click();
+
+        ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(windows.get(1));
+
+        WebElement text = driver.findElement(By.xpath("//h1//span[@class='white-text']"));
+
+        String actualResultUrl = driver.getCurrentUrl();
+        String actualResultBanner = text.getText();
+
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+        Assert.assertEquals(actualResultBanner, expectedResultBanner);
 
         driver.quit();
-
 
     }
 }
