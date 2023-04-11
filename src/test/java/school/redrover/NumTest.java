5325feedc2ecce33a561a6ce48001dadef287fbc
JuliaNuga <130059145+JuliaNuga@users.noreply.github.com>
2023-04-11 18:42:39 +0200

    add timeout to driver

--- src/test/java/school/redrover/NumTest.java
+++ src/test/java/school/redrover/NumTest.java
@@ -6,16 +6,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class NumTest {
 
     @Test
-    public  void wikiTest() throws InterruptedException {
+    public  void wikiTest() /*throws InterruptedException */{
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
         driver.get("https://wikipedia.org");
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         Assert.assertEquals(driver.getTitle(),"Wikipedia");
         driver.quit();
     }
