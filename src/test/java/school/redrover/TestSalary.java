72cbd7e8ae3d8bbeeb375257f6e5edd0b732ae1e
sTDicheva <dicheva.t@gmail.com>
2023-04-11 18:49:17 +0300

    ignore testSalary

--- src/test/java/school/redrover/TestSalary.java
+++ src/test/java/school/redrover/TestSalary.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class TestSalary {
+    @Ignore
     @Test
     public void testSalary() throws InterruptedException {
 
