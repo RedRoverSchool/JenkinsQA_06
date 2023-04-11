da5b5dabc2af45f9fc0f09767962bc76f869b66c
JuliaNuga <130059145+JuliaNuga@users.noreply.github.com>
2023-04-11 18:23:32 +0200

    fixed indentation

--- src/test/java/school/redrover/NumTest.java
+++ src/test/java/school/redrover/NumTest.java
@@ -8,15 +8,15 @@ import org.testng.annotations.Test;
 
 public class NumTest {
 
-   @Test
-   public  void wikiTest() throws InterruptedException {
-       ChromeOptions chromeOptions = new ChromeOptions();
-       chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-       WebDriver driver = new ChromeDriver(chromeOptions);
+    @Test
+    public  void wikiTest() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
-       driver.get("https://wikipedia.org");
-       Thread.sleep(2000);
-       Assert.assertEquals(driver.getTitle(),"Wikipedia");
-       driver.quit();
-   }
+        driver.get("https://wikipedia.org");
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getTitle(),"Wikipedia");
+        driver.quit();
+    }
 }
