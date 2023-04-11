d41d96ba582a5be962d1ae6db887f56a13622724
Alex F <belovezhie@gmail.com>
2023-04-11 16:44:54 -0700

    Add new test, the second

--- src/test/java/school/redrover/AlexFTest.java
+++ src/test/java/school/redrover/AlexFTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -25,6 +26,32 @@ public class AlexFTest {
         driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
         WebElement icon = driver.findElement(By.xpath("//span[@class='onsale']"));
         Assert.assertEquals(icon.getText(), "Sale!");
+        driver.quit();
+    }
+
+    @Test
+    public void testOfTextFieldAcceptance() throws InterruptedException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement elementTextInput = driver.findElement(By.id("my-text-id"));
+        elementTextInput.sendKeys("We're all learning Java");
+
+        WebElement elementPassword = driver.findElement(By.name("my-password"));
+        elementPassword.sendKeys("Google0000!");
+
+        WebElement elementTextArea = driver.findElement(By.name("my-textarea"));
+        elementTextArea.sendKeys("To get better result you need to spend no less than 2 hours daily with exercises and challenges.");
+
+        Select selector1 = new Select(driver.findElement(By.name("my-select")));
+        selector1.selectByIndex(2);
+
         driver.quit();
     }
 }
