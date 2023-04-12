8c44667f8f92b667d275702f09b0a24598f43ff2
ExpMaxP <psmax63163@ya.ru>
2023-04-12 19:10:58 +0300

    First_PR_MaxPovytchikov

--- src/test/java/school/redrover/GroupMaxTest.java
+++ src/test/java/school/redrover/GroupMaxTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -60,10 +61,21 @@ public class GroupMaxTest {
 
     @Test
     public void testMax(){
+
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        String text = "Max";
-        Assert.assertEquals("Max", text);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement readOnlyInput = driver.findElement(By.name("my-readonly"));
+        String initial = readOnlyInput.getAttribute("value");
+
+        readOnlyInput.click();
+        readOnlyInput.sendKeys("Hello World!");
+
+        Assert.assertEquals(readOnlyInput.getAttribute("value"), initial);
+        driver.quit();
+
     }
 }
