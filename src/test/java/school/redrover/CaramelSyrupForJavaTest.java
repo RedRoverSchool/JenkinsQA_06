5540327904972d3e1d7c75969db85d90ca01053f
Elena-Galina <e.lena.galina@yandex.ru>
2023-04-12 17:30:29 +0300

    Rename our class v2

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.ArrayList;
 
-public class CaramelSyrupForJava {
+public class CaramelSyrupForJavaTest {
 
     @Test
     public void artyomDulyaOpenWeatherGuideClick() throws InterruptedException {
