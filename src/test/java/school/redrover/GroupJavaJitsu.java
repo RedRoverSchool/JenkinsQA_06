8bb7ba605178857f0babfe0fd1070bf9f95cf97f
ReginaKurtLV <rgaynul@gmail.com>
2023-04-11 19:45:03 -0700

    delete

--- src/test/java/school/redrover/GroupJavaJitsu.java
+++ src/test/java/school/redrover/GroupJavaJitsu.java
@@ -237,30 +237,6 @@ public class GroupJavaJitsu {
         System.out.println("testLogin2");
     }
 
-    @Test
-    public void goToHomePage() throws InterruptedException {
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*",  "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-
-        driver.get("https://www.google.com/");
-
-        WebElement textBox = driver.findElement(By.name("q"));
-
-        textBox.sendKeys("ubereats");
-        textBox.sendKeys(Keys.RETURN);
-
-        Thread.sleep(2000);
-
-        WebElement firstResult = driver.findElement(By.cssSelector("div.g a"));
-        firstResult.click();
-        Assert.assertEquals(driver.getTitle(), "Uber Eats US | Food Delivery and Takeout | Order Online from Restaurants Near You");
-
-        driver.quit();
-
-    }
 
     @Test
     public void homePageSoccer() {
