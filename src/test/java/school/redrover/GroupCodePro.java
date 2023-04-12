69660fdf300e0d15a3123e0d04079630e22b6a7b
Elgrueber <elenagrueber@gmail.com>
2023-04-11 20:52:37 -0400

    Add 2nd test but don't want to make register

--- src/test/java/school/redrover/GroupCodePro.java
+++ src/test/java/school/redrover/GroupCodePro.java
@@ -1,5 +1,6 @@
 package school.redrover;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +10,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupCodePro {
+
+    public static String getRandomStr(int length) {
+        return RandomStringUtils.random(length,
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+    }
+
+    public static final String EMAIL = getRandomStr(10) + "@gmail.com";
 
     @Test
     public void testKravisRegistration() throws InterruptedException {
@@ -53,5 +61,48 @@ public class GroupCodePro {
         Assert.assertEquals(pageEvents.getText(), "EVENT LIST");
 
         driver.quit();
+    }
+
+    @Test
+    public void testSSregistration() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.hbomax.com/");
+
+        WebElement buttonSignUpNow = driver
+                .findElement(By.xpath("//*[@id=\"page23957-band406944-Header406945\"]//a[text()='Sign Up Now']"));
+        buttonSignUpNow.click();
+        Thread.sleep(2000);
+
+        WebElement pageSignUp = driver
+                .findElement(By.xpath("//div[@class='exp-2688-plans']//div[text()='$9.99']"));
+        pageSignUp.click();
+        Thread.sleep(2000);
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.sendKeys("Ann");
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("Mann");
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("mail2467845@gmail.com");
+     //   Thread.sleep(2000);
+   //     WebElement confirmEmail = driver.findElement(By.id("emailDupe"));
+     //   confirmEmail.sendKeys("mail2467845@gmail.com");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("bumbar123!");
+        WebElement buttonCreateAccount = driver
+                .findElement(By.xpath("//button[@id='createAccount']"));
+        Assert.assertEquals(buttonCreateAccount.getText(), "CREATE ACCOUNT");
+
+//        buttonCreateAccount.click();
+//        Thread.sleep(2000);
+//
+//        WebElement pagePayment = driver
+//                .findElement(By.xpath("//h1[text()='Add your payment method']"));
+//
+//        Assert.assertEquals("Add your payment method", pagePayment.getText());
     }
 }
