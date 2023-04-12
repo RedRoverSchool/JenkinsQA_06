85f0b251c3017cf05ffc5352940e9e22154f0904
Elygan <elent@ukr.net>
2023-04-12 18:14:33 +0300

    Add new tests/last fixed new

--- src/test/java/school/redrover/ElenaTsTest.java
+++ src/test/java/school/redrover/ElenaTsTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
+
 import org.testng.annotations.Test;
 
 public class ElenaTsTest {
@@ -22,23 +22,6 @@ public class ElenaTsTest {
        Assert.assertEquals(  driver.getTitle(),"Electronics, Cars, Fashion, Collectibles & More | eBay" );
        driver.quit();
     }
-@Ignore
-@Test
-    public void testProductSearchByBrandName() throws InterruptedException {
-    ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.tradeinn.com/trekkinn/en");
-    WebElement searchField = driver.findElement(By.xpath("//input [@class='ais-SearchBox-input']"));
-    searchField.sendKeys("smartwool");
-    searchField.sendKeys(Keys.RETURN);
-   Thread.sleep(5000);
-    WebElement result = driver.findElement(By.xpath("//div[@class='hit-name'][1]"));
-
-
-    Assert.assertEquals(result.getText().substring(0,9), "Smartwool");
-    driver.quit();
-}
 
     @Test
     public void testFindProductByBrandName() throws InterruptedException {
