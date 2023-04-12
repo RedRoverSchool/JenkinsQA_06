1ad74d5b694ee4fbbc5886c2bc776d601fbeeb4d
tryam <tryamkin@gmail.com>
2023-04-12 15:58:54 +0200

    test 2 fix

--- src/test/java/school/redrover/TryamTestGetAtrr.java
+++ src/test/java/school/redrover/TryamTestGetAtrr.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,21 +8,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TryamTest {
+public class TryamTestGetAtrr {
     @Test
-    public void homePage() throws InterruptedException {
+    public void getAtrr(){
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.google.com/");
-        WebElement textBox = driver.findElement(By.name("q"));
         WebElement button = driver.findElement(By.name("btnK"));
-        System.out.println(button.getAttribute("value"));
-       Assert.assertEquals(driver.getTitle(),"Google");
         Assert.assertEquals(button.getAttribute("value"),"Поиск в Google");
-        Thread.sleep(2000);
-        driver.quit();
-    }
 
+    }
 }
