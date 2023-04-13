18d476990f740a64942a1ce5917c8d6c648e69c1
svetline1 <sveto4ka@list.ru>
2023-04-13 17:50:56 +0300

    Thread sleep increased

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -47,7 +47,7 @@ public class CaramelSurupForJavaTest {
         WebElement fitnessTekstil = driver.findElement(By.xpath("//div[@class='t694__overlay '][1]"));
         fitnessTekstil.click();
 
-        Thread.sleep(1000);
+        Thread.sleep(3000);
 
         WebElement phoneNumber1 = driver.findElement(By.xpath("//div[@class='t228__right_descr']/a[1]"));
         WebElement phoneNumber2 = driver.findElement(By.xpath("//div[@class='t228__right_descr']/a[2]"));
