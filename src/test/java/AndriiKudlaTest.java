108706b1c3bc2bcaeaabac0dde5ba77171735df0
Andrii Kudla <lambo911@ukr.net>
2023-04-13 19:03:13 +0300

    FirstTest

--- src/test/java/AndriiKudlaTest.java
+++ src/test/java/AndriiKudlaTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -12,6 +13,10 @@ public class AndriiKudlaTest {
 
     @Test
     public void testFirst() {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
