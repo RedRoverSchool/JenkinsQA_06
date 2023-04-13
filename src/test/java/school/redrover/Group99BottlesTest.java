b0e75ec3daa3a95c6f33c8f8377ec27d69ff2625
Ekaterina <enmedv@gmail.com>
2023-04-13 11:34:44 +0300

    add import org.testng.annotations.Ignore;

--- src/test/java/school/redrover/Group99BottlesTest.java
+++ src/test/java/school/redrover/Group99BottlesTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.Ignore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
