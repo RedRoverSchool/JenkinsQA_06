bb674fa18c9021a79305192cf50175f3d45fd78b
SerhiiHaponiuk <gaponyuk1994@gmail.com>
2023-04-13 21:00:22 +0300

    Rename of class and changes for it

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -15,23 +16,33 @@ public class CaramelSurupForJavaTest {
 
 
     @Test
-    public void serhiiHaponiukFirstSeleniumTest() throws InterruptedException {
+    public void openWeatherMapGridContainerTest() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-        driver.get("https://www.selenium.dev/");
+        driver.get("https://openweathermap.org/");
         String title = driver.getTitle();
-        Assert.assertEquals(title, "Selenium");
+
+//        WebDriverWait waitTillVisible = new WebDriverWait(driver, 50);
+        Assert.assertEquals(title, "Сurrent weather and forecast - OpenWeatherMap");
 
         Thread.sleep(5000);
-        WebElement SeleniumWebDriverButton = driver.findElement(By.xpath
-                ("//*[contains(@class, 'selenium-button selenium-webdriver')]"));
-        SeleniumWebDriverButton.click();
 
-        Thread.sleep(5000);
-        Assert.assertEquals(driver.getTitle(), "WebDriver | Selenium");
+        WebElement celsiusButton = driver.findElement(By.xpath
+                ("//div[text() = 'Metric: °C, m/s']"));
+        WebElement fahrenheitButton = driver.findElement(By.xpath
+                ("//div[text() = 'Imperial: °F, mph']"));
+        WebElement temperature = driver.findElement(By.className("heading"));
+        Assert.assertTrue(temperature.getText().contains("C"));
+        celsiusButton.isDisplayed();
+        fahrenheitButton.click();
+
+        Assert.assertTrue(temperature.getText().contains("F"));
+        celsiusButton.click();
+        Assert.assertTrue(temperature.getText().contains("C"));
+
         driver.quit();
     }
 }
