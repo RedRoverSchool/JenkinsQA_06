d37146606f7da02e9d2688f8661c1dd7fbe69c1a
Andrey Bozhko <556070@gmail.com>
2023-04-13 10:27:39 +0300

    Refactoring code1

--- src/test/java/school/redrover/JavaExpertsTest.java
+++ src/test/java/school/redrover/JavaExpertsTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class JavaExpertsGroup {
+public class JavaExpertsTest {
     @Test
     public void testFirst(){
 
