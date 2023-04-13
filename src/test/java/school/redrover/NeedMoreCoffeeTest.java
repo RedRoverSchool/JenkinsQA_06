6c49db2e35347bb77611c0ad848e4bebb2cb0950
Eleonora Butonaeva <borelka@yandex.ru>
2023-04-13 19:07:43 +0700

    add a new test

--- src/test/java/school/redrover/NeedMoreCoffeeTest.java
+++ src/test/java/school/redrover/NeedMoreCoffeeTest.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+
 
 public class NeedMoreCoffeeTest {
     @Test
@@ -50,22 +53,19 @@ public class NeedMoreCoffeeTest {
     public void testButonaeva() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-        driver.get("https://www.google.com/");
+        driver.get("https://www.kinopoisk.ru");
 
-        WebElement textBox = driver.findElement(By.name("q"));
+        WebElement textBox = driver.findElement(By.name("kp_query"));
 
-        textBox.sendKeys("java");
+        textBox.sendKeys("Король и Шут");
         textBox.sendKeys(Keys.RETURN);
 
         Thread.sleep(3000);
-
-        WebElement text = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div[1]/a/div/div/div/cite"));
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-        submitButton.click();
+        WebElement text = driver.findElement(By.xpath("//*[@id=\"block_left_pad\"]/div/div[2]/div/div[2]/p/a"));
+        Thread.sleep(3000);
+        Assert.assertEquals(text.getText(), "Король и Шут (сериал)");
 
 
         driver.quit();
