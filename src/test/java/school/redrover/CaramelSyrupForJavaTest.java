75194b168150743b46df049bfa3cd6ddbaf251d0
Artyom Dulya <fecc1612@gmail.com>
2023-04-14 11:11:08 +0500

    secondCommit

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -233,7 +234,7 @@ public class CaramelSyrupForJavaTest {
 
         driver.quit();
     }
-
+@Ignore
     @Test
     public void testArtyomDulyaThehostbest() throws InterruptedException {
 
@@ -257,7 +258,7 @@ public class CaramelSyrupForJavaTest {
 
         driver.quit();
     }
-
+@Ignore
     @Test
     public static void testArtyomDulyaHeaderButton() throws InterruptedException {
 
