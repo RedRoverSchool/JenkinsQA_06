75a242f25967d2ab785547c38c756bf2dee8cbb2
mihaibordei <workbordeimihai@gmail.com>
2023-04-13 19:55:08 -0700

    Correcting errors for bad written code

--- src/test/java/school/redrover/AlexLeoEpicGroupTest.java
+++ src/test/java/school/redrover/AlexLeoEpicGroupTest.java
@@ -162,20 +162,17 @@ public class AlexLeoEpicGroupTest {
     }
 
     @Test
-    public void MikeBTest() {
-
+    public void validateHomePageTest() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-
 
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/");
 
         driver.findElement(By.xpath("//a[@class=\"wp-block-button__link\"]")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/store");
-
 
     }
 
