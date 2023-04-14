20c95520c18573f906d5b876d2ee76b0986e933b
ElviraKh1 <vira2015@aol.com>
2023-04-14 16:14:26 -0400

    BaseTest is added

--- src/test/java/school/redrover/GroupSomeGroupTest.java
+++ src/test/java/school/redrover/GroupSomeGroupTest.java
@@ -8,36 +8,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
+import school.redrover.runner.BaseTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class GroupSomeGroupTest {
 
-    private WebDriver driver;
+public class GroupSomeGroupTest extends BaseTest{
+
     private WebDriverWait wait;
 
-    @BeforeTest
+    @BeforeMethod
     public void init() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        driver = new ChromeDriver(chromeOptions);
-        wait=new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait=new WebDriverWait(getDriver(), Duration.ofSeconds(2));
     }
 
-    @AfterTest
-    public void end() {
-        driver.quit();
-    }
 
     @Test
     public void testEmailContactPage() {
-        driver.get("https://askomdch.com/");
-        driver.findElement(By.cssSelector("#ast-desktop-header  a[href$='/contact-us/']")).click();
+        getDriver().get("https://askomdch.com/");
+        getDriver().findElement(By.cssSelector("#ast-desktop-header  a[href$='/contact-us/']")).click();
 
-        WebElement email = driver.findElement(
+        WebElement email = getDriver().findElement(
                 By.xpath("//p[contains(text(), 'Email')]/strong"));
 
         Assert.assertEquals(email.getText(), "askomdch@gmail.com");
@@ -66,13 +59,13 @@ public class GroupSomeGroupTest {
 
     @Test
     public void testCartCounter() {
-        driver.get("https://askomdch.com/");
-        driver.findElement(By.cssSelector("#ast-desktop-header  a[href$='/store/']")).click();
+        getDriver().get("https://askomdch.com/");
+        getDriver().findElement(By.cssSelector("#ast-desktop-header  a[href$='/store/']")).click();
 
-        driver.findElement(
+        getDriver().findElement(
                 By.xpath("//a[@href='?add-to-cart=1198']")).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".ast-site-header-cart-data  ul")));
-        WebElement counter = driver.findElement(By.xpath("//div[@id='ast-desktop-header']//span[@class='count']"));
+        WebElement counter = getDriver().findElement(By.xpath("//div[@id='ast-desktop-header']//span[@class='count']"));
         Assert.assertEquals(counter.getText().trim(), "1");
     }
 }
