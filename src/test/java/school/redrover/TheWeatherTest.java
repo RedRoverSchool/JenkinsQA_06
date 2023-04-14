cf7da794926be2172aaa22b385b59be7bb36541f
Katerina <katerina@a0a0.ru>
2023-04-14 19:36:26 +0300

    change itemLink

--- src/test/java/school/redrover/TheWeatherTest.java
+++ src/test/java/school/redrover/TheWeatherTest.java
@@ -10,13 +10,12 @@ import org.testng.annotations.Test;
 
 public class TheWeatherTest {
     @Test
-    public void testWeather () throws InterruptedException   {
+    public void testWeather() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         String query = "Воркута ";
-
-        WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.gismeteo.ru/");
         Thread.sleep(2000);
@@ -29,26 +28,25 @@ public class TheWeatherTest {
         System.out.println("found weather path");
         item.click();
         WebElement textTitle = driver.findElement(By.xpath("//div[@class='page-title']//h1"));
+
         Assert.assertEquals(textTitle.getText(), "Погода в Воркуте");
         driver.close();
-
     }
 
     @Test
     public void testWeatherFor10Days () throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://www.gismeteo.ru/weather-vorkuta-3960/");
         Thread.sleep(2000);
-        WebElement itemLink = driver.findElement(By.xpath("//div[@class = 'subnav-menu header-subnav-menu']/a[2]"));
+        WebElement itemLink = driver.findElement(By.xpath("//div[@class='subnav-menu header-subnav-menu']/a[2]"));
         itemLink.click();
         Thread.sleep(1000);
         WebElement textTitle = driver.findElement(By.xpath("//div[@class='page-title']//h1"));
+
         Assert.assertEquals(textTitle.getText(), "Погода в Воркуте на завтра");
         driver.close();
-
     }
 }
