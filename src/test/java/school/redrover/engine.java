2e6e0c03b170ee25e445b38f01f50779e2d5eb36
DKV project <andrii.medvediev.ext@dkv-mobility.com>
2023-04-14 21:49:58 +0200

    test 123

---
+++ src/test/java/school/redrover/engine.java
@@ -0,0 +1,15 @@
+package school.redrover;
+
+public enum engine {
+    Oil("oil"),
+    Gas("gas"),
+    Electric("electric");
+
+    private String engineTypeText;
+
+
+    engine (String engineType){
+        this.engineTypeText = engineTypeText;
+    };
+
+}
