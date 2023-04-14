a300c54ccec0dede46aafd125e1cdd10eda3ab29
wawansumskoi <volodymyr3583@gmail.com>
2023-04-14 12:48:20 +0300

    shottened  the test

--- src/test/java/school/redrover/GroupJasperAutomationTest.java
+++ src/test/java/school/redrover/GroupJasperAutomationTest.java
@@ -13,26 +13,12 @@ public class GroupJasperAutomationTest {
     @Test
     public void footballua() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        chromeOptions.addArguments( "--headless", "--window-size=1920,1080");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://football.ua/");
-        Thread.sleep(3000);
-
-        WebElement textBox = driver.findElement(By.id("searchInput"));
-        Thread.sleep(2000);
-        textBox.sendKeys("Реал Мадрид");
-        Thread.sleep(2000);
-
-
-        textBox.sendKeys(Keys.RETURN);
-
-        Thread.sleep(2000);
-
-
-        WebElement text = driver.findElement(By.linkText("Реал Мадрид"));
-        Assert.assertEquals(text.getText(), "Реал Мадрид");
-
+        WebElement element = driver.findElement(By.xpath("//*[text()='Україна']"));
+        Assert.assertEquals(element.getText(), "Україна");
         driver.quit();
     }
 
