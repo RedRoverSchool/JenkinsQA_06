dc878766f15a2617e6b9e51540cd4179f078a161
Vlad2034 <37099485+Vlad2034@users.noreply.github.com>
2023-04-13 19:50:58 -0700

    Update VladLiTest.java

--- src/test/java/school/redrover/VladLiTest.java
+++ src/test/java/school/redrover/VladLiTest.java
@@ -10,31 +10,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class VladLiTest {
-    @Test
-    // Commit and push assignment
+    
+    @Test    
     public void YahooTest() throws InterruptedException {
-
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-
         WebDriver driver = new ChromeDriver(chromeOptions);
-
         driver.get("https://www.yahoo.com/?guccounter=1");
 
         WebElement textBox = driver.findElement(By.name("p"));
-
         textBox.sendKeys("Amazon");
         textBox.sendKeys(Keys.RETURN);
-
         Thread.sleep(2000);
 
         WebElement text = driver.findElement(By.xpath("//span[text() = 'Amazon.com® Official Site - Fast Free Delivery with Prime']"));
 
         Assert.assertEquals(text.getText(), "Amazon.com® Official Site - Fast Free Delivery with Prime");
 
-
         driver.quit();
     }
-
 }
