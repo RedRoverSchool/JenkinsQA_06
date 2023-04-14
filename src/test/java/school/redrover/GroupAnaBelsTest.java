dc898cd70bfab68dd7e34955ca6cd4a391d2b6bf
Stanislav111777 <stas.mishen@gmail.com>
2023-04-14 13:36:53 -0400

    First test in gruop by StasM(changed method to xpath again )

--- src/test/java/school/redrover/GroupAnaBelsTest.java
+++ src/test/java/school/redrover/GroupAnaBelsTest.java
@@ -9,18 +9,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
-
 public class GroupAnaBelsTest {
     @Test
-    public void testStasM(){
-
+    public void testStasM() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*","--headless", "--window-size=1920,1080");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.yahoo.com/");
 
-        WebElement submitButton = driver.findElement(By.id("ybarAccountProfile"));
+        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"ybarAccountProfile\"]/a"));
         submitButton.click();
 
         String title = driver.getTitle();
@@ -28,3 +26,6 @@ public class GroupAnaBelsTest {
         driver.quit();
     }
 }
+
+
+
