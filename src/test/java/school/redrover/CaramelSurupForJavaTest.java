2dc1c2e52a725ecc8493ac48e706badda9f4b3bc
SerhiiHaponiuk <gaponyuk1994@gmail.com>
2023-04-14 19:39:13 +0300

    Rename of class and changes for it

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -6,48 +6,59 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class CaramelSurupForJavaTest {
-
-    @Ignore
     @Test
-    public void artyomDulyaOpenWeatherGuideClick () throws InterruptedException {
+    public void openWeatherMapGridContainerTest() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://openweathermap.org/");
+        String title = driver.getTitle();
+
+        Assert.assertEquals(title, "Сurrent weather and forecast - OpenWeatherMap");
+
         Thread.sleep(5000);
-        WebElement orderSite = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
-        orderSite.click();
-        WebElement turnkeyBusinessCard = driver.findElement(
-                By.xpath("//main//div[2]/div/div/p[1]/a[text()='complex enterprise systems']"));
-        turnkeyBusinessCard.click();
+
+        WebElement celsiusButton = driver.findElement(By.xpath
+                ("//div[text() = 'Metric: °C, m/s']"));
+        WebElement fahrenheitButton = driver.findElement(By.xpath
+                ("//div[text() = 'Imperial: °F, mph']"));
+        WebElement temperature = driver.findElement(By.className("heading"));
+        Assert.assertTrue(temperature.getText().contains("C"));
+        celsiusButton.isDisplayed();
+        fahrenheitButton.click();
+
+        Assert.assertTrue(temperature.getText().contains("F"));
+        celsiusButton.click();
+        Assert.assertTrue(temperature.getText().contains("C"));
 
         driver.quit();
     }
 
     @Test
-    public void serhiiHaponiukFirstSeleniumTest() throws InterruptedException {
+    public void svetaKhudovaWinnerTest() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-        driver.get("https://www.selenium.dev/");
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Selenium");
+        driver.get("https://winnerfit.ru/");
 
-        Thread.sleep(5000);
-        WebElement SeleniumWebDriverButton = driver.findElement(By.xpath
-                ("//*[contains(@class, 'selenium-button selenium-webdriver')]"));
-        SeleniumWebDriverButton.click();
+        WebElement fitnessTekstil = driver.findElement(By.xpath("//div[@class='t694__overlay '][1]"));
+        fitnessTekstil.click();
 
-        Thread.sleep(5000);
-        Assert.assertEquals(driver.getTitle(), "WebDriver | Selenium");
+        Thread.sleep(3000);
+
+        WebElement phoneNumber1 = driver.findElement(By.xpath("//div[@class='t228__right_descr']/a[1]"));
+        WebElement phoneNumber2 = driver.findElement(By.xpath("//div[@class='t228__right_descr']/a[2]"));
+
+        Assert.assertEquals(phoneNumber1.getText(), "+7 (499) 178-60-18");
+        Assert.assertEquals(phoneNumber2.getText(), "+7 (905) 714-13-70");
+
         driver.quit();
     }
 }
