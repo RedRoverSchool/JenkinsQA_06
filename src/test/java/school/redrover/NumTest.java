cccb29a89e7e10b4e784aea5f4e47b3f90ff1a3a
JuliaNuga <130059145+JuliaNuga@users.noreply.github.com>
2023-04-15 16:30:28 +0200

    added extending

--- src/test/java/school/redrover/NumTest.java
+++ src/test/java/school/redrover/NumTest.java
@@ -5,20 +5,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
-public class NumTest {
+public class NumTest extends BaseTest {
 
     @Test
     public  void wikiTest() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
-        driver.get("https://wikipedia.org");
 
-        Assert.assertEquals(driver.getTitle(),"Wikipedia");
-        driver.quit();
+       getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        getDriver().get("https://wikipedia.org");
+
+        Assert.assertEquals(getDriver().getTitle(),"Wikipedia");
+        getDriver().quit();
     }
 }
