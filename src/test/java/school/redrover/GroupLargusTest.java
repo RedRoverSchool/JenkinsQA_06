18d41895359c0a2254ef6a9123ca7565b3889079
Ivan Korolev <ikorolev13@gmail.com>
2023-04-15 13:27:39 +0300

    GroupLargusTest// changed test

--- src/test/java/school/redrover/GroupLargusTest.java
+++ src/test/java/school/redrover/GroupLargusTest.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GroupLargus {
+public class GroupLargusTest {
     @Test
     public void thirdTest() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.google.com/");
