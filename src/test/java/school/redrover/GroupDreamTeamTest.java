0a28d8d5cf1285783f3b4edb02923453deddd401
Rafis Sabitov <rafissabitov@gmail.com>
2023-04-15 17:48:29 -0500

    name changed to GroupDreamTEamTest

--- src/test/java/school/redrover/GroupDreamTeamTest.java
+++ src/test/java/school/redrover/GroupDreamTeamTest.java
@@ -12,27 +12,21 @@ import java.time.Duration;
 
 import static org.testng.Assert.assertEquals;
 
-public class GroupDreamTeam {
+public class GroupDreamTeamTest {
     @Test
     public void eightComponents() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
         String title = driver.getTitle();
         assertEquals("Web form", title);
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         WebElement textBox = driver.findElement(By.name("my-text"));
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
         textBox.sendKeys("Selenium");
         submitButton.click();
-
         WebElement message = driver.findElement(By.id("message"));
         String value = message.getText();
         assertEquals("Received!", value);
-
         driver.quit();
     }
 
