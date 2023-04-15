805a9c7a2f20dba4091b33baa04c3503d602c233
maierlana <kozyachiy.svetlana@gmail.com>
2023-04-15 01:15:16 -0400

    add test testSearchIplayAmerica

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -134,7 +134,7 @@ public class GroupForwardTest {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.google.com/");
 
         WebElement textBox = driver.findElement(By.name("q"));
