4d4f367ab3d0db0e1d417c00f6af4c105582a84f
Tatiana Todoseva <tatiana.todoseva@gmail.com>
2023-04-16 10:56:59 +0300

    First Test

---
+++ src/test/java/school/redrover/TodosevaOneMoreTest.java
@@ -0,0 +1,10 @@
+package school.redrover;
+
+import org.testng.annotations.Test;
+
+public class TodosevaOneMoreTest {
+    @Test
+    public void testOneMore(){
+        System.out.println("Test test");
+    }
+}
