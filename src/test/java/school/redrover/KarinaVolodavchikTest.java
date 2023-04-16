4c3c63037c42f43af330ed5e9dbf90ae252e0cd0
Sergen <sergalitium@gmail.com>
2023-04-16 22:20:14 +0300

    selenium test

--- src/test/java/school/redrover/KarinaVolodavchikTest.java
+++ src/test/java/school/redrover/KarinaVolodavchikTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -55,6 +56,29 @@ public class KarinaVolodavchikTest {
         Assert.assertEquals("JPetStore Demo", title);
 
         driver.quit();
+    }
+    @Test
+    public void testS() throws InterruptedException {
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
+        Thread.sleep(2000);
+
+        driver.quit();
+
     }
 
     }
