f5020a533fa6c59a2cd4e0ded62a31267849a9f3
Ivan Korolev <ikorolev13@gmail.com>
2023-04-16 22:09:07 +0300

    GroupLargusTest/IvanK/ change test with before and after method

--- src/test/java/school/redrover/GroupLargusTest.java
+++ src/test/java/school/redrover/GroupLargusTest.java
@@ -1,59 +1,46 @@
 package school.redrover;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
 import java.util.List;
 
-public class GroupLargusTest {
+public class GroupLargusTest extends BaseTest {
 
     @Test
     public void testSearchResult() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://burgerkingrus.ru/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-
-        driver.get("https://burgerkingrus.ru/");
-
-        driver.findElement(By.xpath("//input[@class='bk-input__element']"))
+        getDriver().findElement(By.xpath("//input[@class='bk-input__element']"))
                 .sendKeys("Воппер");
-        List<WebElement> listOfSearch = driver.findElements(By.xpath
+        List<WebElement> listOfSearch = getDriver().findElements(By.xpath
                 ("//div[@class='bk-search__list']//div[@class='bk-dish-card__title']"));
 
         for (WebElement elementOfList: listOfSearch) {
             Assert.assertTrue(elementOfList.getText().contains("Воппер"));
         }
-
-        driver.quit();
     }
 
     @Test
     public void testCheckedLinkSiderBar() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        Actions actions = new Actions(getDriver());
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        Actions actions = new Actions(driver);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        getDriver().get("https://www.w3schools.com/");
+        getDriver().manage().window().maximize();
 
-        driver.get("https://www.w3schools.com/");
-        driver.manage().window().maximize();
-
-        WebElement buttonLearnJava = driver.findElement
+        WebElement buttonLearnJava = getDriver().findElement
                 (By.xpath("//div[@class='w3-col l6 w3-center']//a[text()='Learn Java']"));
         js.executeScript("arguments[0].scrollIntoView(true);",buttonLearnJava);
         buttonLearnJava.click();
 
-        WebElement siderBar = driver.findElement
+        WebElement siderBar = getDriver().findElement
                 (By.id("leftmenuinnerinner"));
 
-        List<WebElement> listSiderBar = driver.findElements
+        List<WebElement> listSiderBar = getDriver().findElements
                 (By.xpath("//div[@id='leftmenuinnerinner']//a[@target='_top']"));
 
         WebElement itemListSiderBar = null;
@@ -72,46 +59,9 @@ public class GroupLargusTest {
                 .build()
                 .perform();
 
-        WebElement titleOnPage = driver.findElement
+        WebElement titleOnPage = getDriver().findElement
                 (By.xpath("//span[@class='color_h1']"));
 
         Assert.assertEquals("HashMap", titleOnPage.getText());
-        driver.quit();
-    }
-
-    @Ignore
-    @Test
-    public void testCheckSearchUsedCar() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", /*"--headless", */"--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        Actions actions = new Actions(driver);
-
-        driver.get("https://www.kia.ru/");
-        Thread.sleep(3000);
-
-        List<WebElement> buttonsChoiseAndBuy  = driver.findElements
-                    (By.xpath("//span[text()='Авто с пробегом']"));
-        actions
-                .sendKeys(Keys.ARROW_UP)
-                .moveToElement(buttonsChoiseAndBuy.get(0))
-                .click()
-                .build()
-                .perform();
-
-        Thread.sleep(1000);
-        List<WebElement> listOfSearch = driver.findElements
-                (By.xpath("//div[@class='catalog-element__descr']"));
-        System.out.println("include- " + listOfSearch.size());
-        boolean findResult = true;
-        for (WebElement elementOfList: listOfSearch) {
-            if (!elementOfList.getText().contains("км") && elementOfList.getText().equals("0 км")){
-                findResult = false;
-            }
-        }
-
-        Assert.assertTrue(findResult);
-        driver.quit();
     }
 }
