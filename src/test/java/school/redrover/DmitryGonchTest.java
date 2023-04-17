c8edb408955f08ac44507a478803557da4ab173a
Dmitry-Gonch <dimankrytyi@gmail.com>
2023-04-17 18:50:17 +0300

    Fix test

--- src/test/java/school/redrover/DmitryGonchTest.java
+++ src/test/java/school/redrover/DmitryGonchTest.java
@@ -1,7 +1,10 @@
 package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -9,15 +12,20 @@ import school.redrover.runner.BaseTest;
 public class DmitryGonchTest extends BaseTest {
 
     @Test
-    public void testFindElement() {
-        getDriver().get("https://7745.by/");
+    public void testFirsDg() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebElement texBox = getDriver().findElement(By.name("keys"));
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://7745.by/");
+
+        WebElement texBox = driver.findElement(By.name("keys"));
         texBox.sendKeys("процессор");
         texBox.sendKeys(Keys.RETURN);
 
-        WebElement text = getDriver().findElement(By.xpath("//h1[text() = 'Процессоры для компьютеров']"));
+        WebElement text = driver.findElement(By.xpath("//h1[text() = 'Процессоры для компьютеров']"));
 
         Assert.assertEquals(text.getText(), "Процессоры для компьютеров");
+        driver.quit();
     }
 }
