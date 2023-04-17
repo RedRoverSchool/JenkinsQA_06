2cf47afb315c6269d53871ffc3d7f2fbc8b34b32
Baktygul <baktygul.jekshembieva.ch@gmail.com>
2023-04-17 11:03:59 +0600

    bahaTest1

--- src/test/java/school/redrover/GroupTwentyThreeTest.java
+++ src/test/java/school/redrover/GroupTwentyThreeTest.java
@@ -1,7 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -20,5 +24,40 @@ public class GroupTwentyThreeTest extends BaseTest {
         WebElement text = getDriver().findElement(By.xpath("//h1"));
 
         Assert.assertEquals(text.getText(), "Java Tutorial");
+    }
+
+    @Test
+    public void testBaha1() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://demoqa.com/text-box");
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.sendKeys("Baktygul");
+        Thread.sleep(2000);
+
+        WebElement email = driver.findElement(By.id("userEmail"));
+        email.sendKeys("baktygul.jekshembieva@gmail.com");
+        Thread.sleep(2000);
+
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        currentAddress.sendKeys("Biskek");
+        Thread.sleep(2000);
+
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        permanentAddress.sendKeys("Kerben");
+        Thread.sleep(2000);
+
+        WebElement submit = driver.findElement(By.id("submit"));
+        submit.click();
+        Thread.sleep(2000);
+
+        WebElement name = driver.findElement(By.id("name"));
+        Assert.assertEquals(name.getText(), "Name:Baktygul");
+
+        driver.quit();
     }
 }
