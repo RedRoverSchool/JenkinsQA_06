b13051e9aa58e688ffc9b549f3e40173dcaa694f
IrinaEvseeva <evs.irine@gmail.com>
2023-04-17 16:38:50 +0300

    second try

--- src/test/java/school/redrover/IrinaETest.java
+++ src/test/java/school/redrover/IrinaETest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +10,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class IrinaETest {
+
+    @Test
+    public void testFirst() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://www.google.com/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+
+        textBox.sendKeys("selenium");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+
+        Assert.assertEquals(text.getText(), "Selenium");
+
+        driver.quit();
+    }
 
     @Test
        public void testMyFirst(){
