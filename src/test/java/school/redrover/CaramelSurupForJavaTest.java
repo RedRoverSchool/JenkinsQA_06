f1c3d61ed3057ca3e77c2841f164273398c21ca4
SerhiiHaponiuk <gaponyuk1994@gmail.com>
2023-04-17 18:45:35 +0300

    moving the class to the group's general file

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -10,36 +10,6 @@ import org.testng.annotations.Test;
 
 public class CaramelSurupForJavaTest {
     @Test
-    public void openWeatherMapGridContainerTest() throws InterruptedException {
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-
-        driver.get("https://openweathermap.org/");
-        String title = driver.getTitle();
-
-        Assert.assertEquals(title, "Сurrent weather and forecast - OpenWeatherMap");
-
-        Thread.sleep(5000);
-
-        WebElement celsiusButton = driver.findElement(By.xpath
-                ("//div[text() = 'Metric: °C, m/s']"));
-        WebElement fahrenheitButton = driver.findElement(By.xpath
-                ("//div[text() = 'Imperial: °F, mph']"));
-        WebElement temperature = driver.findElement(By.className("heading"));
-        Assert.assertTrue(temperature.getText().contains("C"));
-        celsiusButton.isDisplayed();
-        fahrenheitButton.click();
-
-        Assert.assertTrue(temperature.getText().contains("F"));
-        celsiusButton.click();
-        Assert.assertTrue(temperature.getText().contains("C"));
-
-        driver.quit();
-    }
-
-    @Test
     public void svetaKhudovaWinnerTest() throws InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
