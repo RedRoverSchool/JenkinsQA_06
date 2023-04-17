10655c28a2946e59e6224f5fc760a5bc80e497b8
Milena Sida <milenasida@gmail.com>
2023-04-17 17:02:15 -0400

    MS first commit

--- src/test/java/school/redrover/DreamTeamTest.java
+++ src/test/java/school/redrover/DreamTeamTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -70,5 +72,33 @@ public class DreamTeamTest extends BaseTest {
         WebElement text = getDriver().findElement(By.xpath("//div[1]/div/h1/span"));
         Assert.assertEquals(text.getText(), "OpenWeather");
     }
+
+    public class GoogleTest {
+
+        @Test
+        public void testGoogleMainPageTitle() {
+            // Set the path to the chromedriver executable
+            System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+            // Create a new WebDriver instance
+            WebDriver driver = new ChromeDriver();
+
+            // Navigate to the Google homepage
+            driver.get("https://www.google.com/");
+
+            // Find the first line text element on the page
+            WebElement textElement = driver.findElement(By.xpath("//div[@id='hplogo']//span"));
+
+            // Verify that the first line text element contains the text "Google"
+            String actualText = textElement.getText();
+            String expectedText = "Google";
+            Assert.assertEquals(actualText, expectedText);
+
+            // Quit the driver and close all windows
+            driver.quit();
+        }
+    }
+
 }
+
 
