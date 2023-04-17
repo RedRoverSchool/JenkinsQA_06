758ed3507b71cd2ad5ef7a41b11ca5f8deb23ec6
Baktygul <baktygul.jekshembieva.ch@gmail.com>
2023-04-17 09:28:53 +0600

    myTest2

--- src/test/java/school/redrover/GroupTwentyThreeTest.java
+++ src/test/java/school/redrover/GroupTwentyThreeTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class GroupTwentyThreeTest {
     @Test
     public void testJavaTutorial() throws InterruptedException {
@@ -26,6 +28,51 @@ public class GroupTwentyThreeTest {
         WebElement text = driver.findElement(By.xpath("//h1"));
 
         Assert.assertEquals(text.getText(), "Java Tutorial");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testBaku11() throws InterruptedException {
+//
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://demoqa.com/text-box");
+
+        Thread.sleep(2000);
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.sendKeys("Baktygul");
+
+        Thread.sleep(2000);
+
+        WebElement email = driver.findElement(By.id("userEmail"));
+        email.sendKeys("baktygul.jekshembieva@gmail.com");
+
+        Thread.sleep(2000);
+
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        currentAddress.sendKeys("Bishkek");
+
+        Thread.sleep(2000);
+
+        WebElement permanentAddres = driver.findElement(By.id("permanentAddress"));
+        permanentAddres.sendKeys("Kerben");
+
+        Thread.sleep(2000);
+
+        WebElement submit = driver.findElement(By.id("submit"));
+        submit.click();
+
+        Thread.sleep(2000);
+
+        WebElement name = driver.findElement(By.id("name"));
+        String value =name.getText();
+        assertEquals("Name:Baktygul", value);
+
+        Thread.sleep(2000);
 
         driver.quit();
     }
