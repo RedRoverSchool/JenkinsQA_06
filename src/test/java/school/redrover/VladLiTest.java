14e3449cd55a31ff6e7a590b9f6bb30ed1b55832
Vlad2034 <leevb@yandex.ru>
2023-04-17 21:25:54 -0700

    Commit and push assignment

--- src/test/java/school/redrover/VladLiTest.java
+++ src/test/java/school/redrover/VladLiTest.java
@@ -7,17 +7,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class VladLiTest {
 
+    private ChromeOptions chromeOptions;
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+
     @Test
     public void YahooTest() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.yahoo.com/?guccounter=1");
 
         WebElement textBox = driver.findElement(By.name("p"));
@@ -28,7 +41,5 @@ public class VladLiTest {
         WebElement text = driver.findElement(By.xpath("//span[text() = 'Amazon.com® Official Site - Fast Free Delivery with Prime']"));
 
         Assert.assertEquals(text.getText(), "Amazon.com® Official Site - Fast Free Delivery with Prime");
-
-        driver.quit();
     }
 }
