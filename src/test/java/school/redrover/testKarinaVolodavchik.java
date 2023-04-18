c504a4c6393c3623b3b203697aa59230bc52c476
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-18 22:49:52 +0200

    do extends from BaseTest

--- src/test/java/school/redrover/testKarinaVolodavchik.java
+++ src/test/java/school/redrover/testKarinaVolodavchik.java
@@ -8,24 +8,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class KarinaVolodavchikTest {
-
+ public class testKarinaVolodavchik extends BaseTest {
 
         @Test
         public void testFirst () throws InterruptedException {
-            // WebDriverManager.chromedriver().setup();
-            // System.setProperty("webdriver.chrome.driver","/path/to/chromedriver");
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-            WebDriver driver = new ChromeDriver(chromeOptions);
 
-            //WebDriver driver = new ChromeDriver();
             driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
             String title = driver.getTitle();
             Assert.assertEquals("Web form", title);
-
-            // driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
             WebElement textBox = driver.findElement(By.name("my-text"));
             WebElement submitButton = driver.findElement(By.cssSelector("button"));
@@ -39,24 +29,13 @@ public class KarinaVolodavchikTest {
             String value = message.getText();
             Thread.sleep(2000); // замедляет действие
             Assert.assertEquals("Received!", value);
-
-            driver.quit();
         }
-    @Test
-    public void testSecond() {
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://petstore.octoperf.com/actions/Catalog.action");
-
-        String title = driver.getTitle();
-        Assert.assertEquals("JPetStore Demo", title);
-
-        driver.quit();
-    }
-
-    }
+        @Test
+        public void testSecond() {
+            driver.get("https://petstore.octoperf.com/actions/Catalog.action");
+            String title = driver.getTitle();
+            Assert.assertEquals("JPetStore Demo", title);
+        }
+ }
 
 
