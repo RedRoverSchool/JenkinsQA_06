8acabb378157e504736e70dc7170c72bb5c05c72
alenb <dmitriiblum@gmail.com>
2023-04-17 20:33:59 -0400

    Added new task 31 and disabled task testSearchSelenium, testGetWebFormTitle, testSearchProduct

--- src/test/java/school/redrover/ZeroBugTest.java
+++ src/test/java/school/redrover/ZeroBugTest.java
@@ -87,12 +87,6 @@ public class ZeroBugTest extends BaseTest {
 
     @Test
     public void task_31 (){
-        //Given 2 arrays of ints, a and b, return true if
-        //they have the same first element or they have the same last element.
-        //Both arrays will be length 1 or more.
-
-//        int [] a = {1,2,3,4};
-//        int [] b = {1,2,3};
 
         int [] a = new int[4];
         int [] b = new int[3];
@@ -113,7 +107,5 @@ public class ZeroBugTest extends BaseTest {
         } else {
             System.out.println("First or last element of an arrays is not equals!");
         }
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
     }
 }
