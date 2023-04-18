18ded9bb97342fdb6d13db160119c985aa385530
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-18 22:59:54 +0200

    do extends from BaseTest

--- src/test/java/school/redrover/KarinaVolodavchikTest.java
+++ src/test/java/school/redrover/KarinaVolodavchikTest.java
@@ -1,14 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
- public class testKarinaVolodavchik extends BaseTest {
+ public class KarinaVolodavchikTest extends BaseTest {
 
         @Test
         public void testFirst () throws InterruptedException {
