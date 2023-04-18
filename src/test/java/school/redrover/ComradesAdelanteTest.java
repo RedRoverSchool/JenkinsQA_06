4279e4e4957950d52e2e84c4a1ec474a117fd054
MariaLuchnikova <luchnikova80@gmail.com>
2023-04-18 10:43:29 +0800

    Add modify testCountItemsOfCart

--- src/test/java/school/redrover/ComradesAdelanteTest.java
+++ src/test/java/school/redrover/ComradesAdelanteTest.java
@@ -8,14 +8,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComradesAdelanteTest {
+public class ComradesAdelanteTest extends BaseTest {
 
     @Test
     public void testHeaderOpenWeather() throws InterruptedException {
@@ -36,7 +38,7 @@ public class ComradesAdelanteTest {
         String actualResult = textHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
-        Reporter.log("Tests stop",true);
+        Reporter.log("Tests stop", true);
         driver.quit();
     }
 
@@ -69,49 +71,42 @@ public class ComradesAdelanteTest {
         String actualResult = textHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
-        Reporter.log("Tests stop",true);
+        Reporter.log("Tests stop", true);
         driver.quit();
     }
 
     @Test
-    public void MariaLuchnikovaTest() throws InterruptedException {
-
+    public void testCountItemsOfCart() throws InterruptedException {
         Reporter.log("Tests started 3", true);
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.saucedemo.com");
-
-        WebElement login = driver.findElement(By.id("user-name"));
+        getDriver().get("https://www.saucedemo.com");
+        WebElement login = getDriver().findElement(By.id("user-name"));
         login.sendKeys("standard_user");
-        WebElement password = driver.findElement(By.id("password"));
+        WebElement password = getDriver().findElement(By.id("password"));
         password.sendKeys("secret_sauce");
 
-        WebElement loginButton = driver.findElement(By.id("login-button"));
+        WebElement loginButton = getDriver().findElement(By.id("login-button"));
         loginButton.click();
 
-        //Thread.sleep(3000);
-
-        WebElement Backpack = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        WebElement Backpack = getDriver().findElement(By.id("add-to-cart-sauce-labs-backpack"));
         Backpack.click();
 
-        WebElement tShirt = driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
+        WebElement tShirt = getDriver().findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
         tShirt.click();
 
-        WebElement onesie = driver.findElement(By.id("add-to-cart-sauce-labs-onesie"));
+        WebElement onesie = getDriver().findElement(By.id("add-to-cart-sauce-labs-onesie"));
         onesie.click();
 
-        WebElement cart = driver.findElement(By.className("shopping_cart_link"));
+        WebElement cart = getDriver().findElement(By.className("shopping_cart_link"));
         cart.click();
 
-        List<WebElement> items = driver.findElements(By.className("cart_item"));
+        List<WebElement> items = getDriver().findElements(By.className("cart_item"));
 
         int countItems = items.size();
         Assert.assertEquals(countItems, 3);
-        Reporter.log("Tests stop",true);
-        driver.quit();
+        Reporter.log("Tests stop", true);
     }
+
     @Test
     public void nikolayMarkovTest() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -203,7 +198,7 @@ public class ComradesAdelanteTest {
         Assert.assertEquals(actualResultFour, expectResult);
         Assert.assertEquals(actualResultNamesList, expectResultNamesList);
 
-        Reporter.log("Tests stop",true);
+        Reporter.log("Tests stop", true);
         driver.quit();
     }
 }
