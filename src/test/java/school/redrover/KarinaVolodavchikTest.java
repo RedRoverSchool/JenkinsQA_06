f7e2840a1675d6f0c57cd46b667e822f94feac4d
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-19 20:06:48 +0200

    do extends from BaseTest

--- src/test/java/school/redrover/KarinaVolodavchikTest.java
+++ src/test/java/school/redrover/KarinaVolodavchikTest.java
@@ -1,7 +1,10 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,21 +14,23 @@ import org.testng.annotations.Test;
         public void testFirst () throws InterruptedException {
 
             driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
             String title = driver.getTitle();
             Assert.assertEquals("Web form", title);
 
             WebElement textBox = driver.findElement(By.name("my-text"));
             WebElement submitButton = driver.findElement(By.cssSelector("button"));
-            Thread.sleep(2000); // замедляет действие
+            Thread.sleep(2000);
 
             textBox.sendKeys("Selenium");
-            Thread.sleep(2000); // замедляет действие
+            Thread.sleep(2000);
             submitButton.click();
 
             WebElement message = driver.findElement(By.id("message"));
             String value = message.getText();
-            Thread.sleep(2000); // замедляет действие
+            Thread.sleep(2000);
             Assert.assertEquals("Received!", value);
+
         }
         @Test
         public void testSecond() {
