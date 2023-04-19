4e18ab7b0149f655766b488404dad1aed8a97f1e
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-19 20:14:25 +0200

    do extends from BaseTestFin

--- src/test/java/school/redrover/KarinaVolodavchikTest.java
+++ src/test/java/school/redrover/KarinaVolodavchikTest.java
@@ -1,43 +1,39 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
- public class KarinaVolodavchikTest extends BaseTest {
+public class KarinaVolodavchikTest extends BaseTest {
 
         @Test
         public void testFirst () throws InterruptedException {
+            getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+            String title = getDriver().getTitle();
 
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-            String title = driver.getTitle();
             Assert.assertEquals("Web form", title);
 
-            WebElement textBox = driver.findElement(By.name("my-text"));
-            WebElement submitButton = driver.findElement(By.cssSelector("button"));
+            WebElement textBox = getDriver().findElement(By.name("my-text"));
+            WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
             Thread.sleep(2000);
 
             textBox.sendKeys("Selenium");
             Thread.sleep(2000);
             submitButton.click();
 
-            WebElement message = driver.findElement(By.id("message"));
+            WebElement message = getDriver().findElement(By.id("message"));
             String value = message.getText();
             Thread.sleep(2000);
-            Assert.assertEquals("Received!", value);
 
+            Assert.assertEquals("Received!", value);
         }
         @Test
         public void testSecond() {
-            driver.get("https://petstore.octoperf.com/actions/Catalog.action");
-            String title = driver.getTitle();
+            getDriver().get("https://petstore.octoperf.com/actions/Catalog.action");
+            String title = getDriver().getTitle();
+
             Assert.assertEquals("JPetStore Demo", title);
         }
  }
-
-
