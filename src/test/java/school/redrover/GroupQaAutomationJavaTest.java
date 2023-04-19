4057546e27da2a7ae81777d4b583ad825f1f6541
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-19 20:53:31 +0200

    new test

---
+++ src/test/java/school/redrover/GroupQaAutomationJavaTest.java
@@ -0,0 +1,15 @@
+package school.redrover;
+
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class GroupQaAutomationJavaTest extends BaseTest {
+
+    // @Test
+
+
+
+
+
+
+}
