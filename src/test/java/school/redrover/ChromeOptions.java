e0fe194b8c9fc9590e8b44a70afe91b978663f18
Tatiana Todoseva <tatiana.todoseva@gmail.com>
2023-04-19 14:41:28 +0300

    Merge remote-tracking branch 'origin/Todoseva/firstTest' into Todoseva/firstTest

---
+++ src/test/java/school/redrover/ChromeOptions.java
@@ -0,0 +1,6 @@
+package school.redrover;
+
+public class ChromeOptions {
+    public void addArguments(String s, String s1, String s2) {
+    }
+}
--- src/test/java/school/redrover/TatianaTodosevaTest.java
+++ src/test/java/school/redrover/TatianaTodosevaTest.java
@@ -74,9 +74,6 @@ public class TatianaTodosevaTest {
         driver.quit();
     }
 
-    @Test
-    public void oneMoreTest (){
-        System.out.println("test test");
-        }
+
 }
 
