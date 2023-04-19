2a977c23ee6b8fbdabd34eb0555bc9a177e7f591
Ekaterina <enmedv@gmail.com>
2023-04-19 12:18:37 +0300

    Change testH1Text_WhenChooseLevelLanguage()

--- src/test/java/school/redrover/Group99BottlesTest.java
+++ src/test/java/school/redrover/Group99BottlesTest.java
@@ -154,23 +154,17 @@ public class Group99BottlesTest extends BaseTest {
     @Test
     public void testH1Text_WhenChooseLevelLanguage() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://www.w3schools.com/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-
-        driver.get("https://www.w3schools.com/");
-
-        driver.findElement(By.xpath("//a[@href = 'where_to_start.asp']")).click();
+        getDriver().findElement(By.xpath("//a[@href = 'where_to_start.asp']")).click();
 
         Thread.sleep(3000);
 
-        WebElement text = driver.findElement(By.xpath("//h1[text() = 'Where To Start']"));
+        WebElement text = getDriver().findElement(By.xpath("//h1[text() = 'Where To Start']"));
 
         Assert.assertEquals(text.getText(), "Where To Start");
-
-        driver.quit();
     }
+
     @Ignore
     @Test
     public void testDemoblazeAddToCart() {
