fbe42c2b4548d586ac6e27d3f964be5d91f93fb3
Olha Kovanova <kovanova.olha@gmail.com>
2023-04-19 10:41:36 +0200

    updated test by Olha Kovanova after lesson 16 (04/13)

--- src/test/java/school/redrover/OlhaKTest.java
+++ src/test/java/school/redrover/OlhaKTest.java
@@ -1,43 +1,40 @@
 package school.redrover;
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
+public class OlhaKTest extends BaseTest {
 
-
-public class OlhaKTest {
     @Test
     public void FirstTest() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        String title = getDriver().getTitle();
+        Assert.assertEquals("Web form", title);
 
-            String title = driver.getTitle();
-            Assert.assertEquals("Web form", title);
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
-            WebElement textBox = driver.findElement(By.name("my-text"));
-            WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        textBox.sendKeys("Selenium");
+        submitButton.click();
 
-            textBox.sendKeys("Selenium");
-            submitButton.click();
+        Thread.sleep(2000);
 
-            Thread.sleep(2000);
+        WebElement message = getDriver().findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
 
-            WebElement message = driver.findElement(By.id("message"));
-            String value = message.getText();
-            Assert.assertEquals("Received!", value);
+   }
 
-            driver.quit();
-        }
-
-    }
+}
 
 
