7e7c2c1f88203c1195a66bab005d517ff461f2d5
Mariana Gevorkyan <mariana.mcoa@gmail.com>
2023-04-19 11:58:59 -0400

    MGTest

--- src/test/java/school/redrover/GroupJavaJitsuTest.java
+++ src/test/java/school/redrover/GroupJavaJitsuTest.java
@@ -1,5 +1,7 @@
 package school.redrover;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -91,6 +93,7 @@ public class GroupJavaJitsuTest extends BaseTest {
 
         Assert.assertEquals(text.getText(), "Step 1: Setup wizard");
     }
+
     @Test
     public void testForm() throws InterruptedException {
         getDriver().get("https://demoqa.com");
@@ -249,7 +252,7 @@ public class GroupJavaJitsuTest extends BaseTest {
     @Test
     public void testKatya2() {
         getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
-        
+
         String title = getDriver().getTitle();
         assertEquals("Web form", title);
 
@@ -257,7 +260,7 @@ public class GroupJavaJitsuTest extends BaseTest {
 
         WebElement textBox = getDriver().findElement(By.name("my-text"));
         WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
-        
+
         textBox.sendKeys("Selenium");
         submitButton.click();
 
@@ -377,6 +380,7 @@ public class GroupJavaJitsuTest extends BaseTest {
 
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://demoqa.com/books?book=9781449365035");
     }
+
     @Test
     public void testAlert() throws InterruptedException {
         getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
@@ -420,6 +424,7 @@ public class GroupJavaJitsuTest extends BaseTest {
         WebElement enterTextResult = getDriver().findElement(By.xpath("//span[@id='promptResult']"));
         Assert.assertEquals(enterTextResult.getText(), "You entered RedRover06");
     }
+
     @Ignore
     @Test
     public void testZakazUa() {
@@ -437,13 +442,13 @@ public class GroupJavaJitsuTest extends BaseTest {
         WebElement buttonReg = getDriver().findElement(By.xpath("//button[contains(text(),'Зареєструватись')]"));
         buttonReg.click();
 
-        WebElement text=getDriver().findElement(By.xpath("//span[contains(text(),'У мене вже є акаунт')]"));
+        WebElement text = getDriver().findElement(By.xpath("//span[contains(text(),'У мене вже є акаунт')]"));
         Assert.assertEquals(text.getText(), "У мене вже є акаунт");
     }
 
 
     @Test
-    public void testFindInSearchBox(){
+    public void testFindInSearchBox() {
 
         getDriver().get("https://soccerzone.com/");
         WebElement searchBox = getDriver().findElement(By.name("search_query_adv"));
@@ -457,7 +462,7 @@ public class GroupJavaJitsuTest extends BaseTest {
     }
 
     @Test
-    public void testWrongSignIn(){
+    public void testWrongSignIn() {
         getDriver().get("https://soccerzone.com/login.php");
         WebElement username = getDriver().findElement(By.name("login_email"));
         username.sendKeys("1234@gmail.com");
@@ -469,7 +474,8 @@ public class GroupJavaJitsuTest extends BaseTest {
         button.click();
 
         WebElement text = getDriver().findElement(By.cssSelector(".alertBox-column.alertBox-message"));
-        Assert.assertTrue(text.isDisplayed());}
+        Assert.assertTrue(text.isDisplayed());
+    }
 
 
     @Test
@@ -500,33 +506,111 @@ public class GroupJavaJitsuTest extends BaseTest {
     }
 
     @Test
-    public void testActivePictureFanstore(){
+    public void testActivePictureFanstore() {
         getDriver().get("https://soccerzone.com/");
         WebElement picture = getDriver().findElement(By.cssSelector("img[src='images/stencil/original/image-manager/fanstore.png']"));
         picture.click();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(),"https://soccerzone.com/fan-shop/");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://soccerzone.com/fan-shop/");
     }
 
     @Test
-    public void testActivePictureKeepers(){
+    public void testActivePictureKeepers() {
         getDriver().get("https://soccerzone.com/");
         WebElement pictureKeepers = getDriver().findElement(By.cssSelector("img[src='images/stencil/original/image-manager/keeps.png']"));
         pictureKeepers.click();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(),"https://soccerzone.com/goalkeeper/gloves/");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://soccerzone.com/goalkeeper/gloves/");
     }
 
     @Test
-    public void testActivePictureBootroom(){
+    public void testActivePictureBootroom() {
         getDriver().get("https://soccerzone.com/");
         WebElement pictureBootroom = getDriver().findElement(By.cssSelector("img[src='images/stencil/original/image-manager/boots.png']"));
         pictureBootroom.click();
 
-        Assert.assertEquals(getDriver().getCurrentUrl(),"https://soccerzone.com/footwear/");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://soccerzone.com/footwear/");
+    }
+
+    @Test
+    public void testWineTimeContact() {
+        getDriver().get("https://www.winetime.com/");
+
+        WebElement buttonEntry = getDriver().findElement(By.xpath("//a[contains(text(),'Contact')]"));
+        buttonEntry.click();
+
+        WebElement textBoxName = getDriver().findElement(By.cssSelector("#input_1_1_3"));
+        textBoxName.sendKeys("Ana");
+
+        WebElement textBoxLast = getDriver().findElement(By.cssSelector("#input_1_1_6"));
+        textBoxLast.sendKeys("Solo");
+
+        WebElement textBoxMail = getDriver().findElement(By.cssSelector("#input_1_2"));
+        textBoxMail.sendKeys("a.olo@gmail.com");
+
+        WebElement textBoxMessage = getDriver().findElement(By.cssSelector("#input_1_3"));
+        textBoxMessage.sendKeys("ASAP");
+
+        WebElement buttonSubmit = getDriver().findElement(By.cssSelector("#gform_submit_button_1"));
+        buttonSubmit.click();
+
+        WebElement text = getDriver().findElement(By.cssSelector("body.page-template-default.page.page-id-80.custom-" +
+                "background.wp-custom-logo.group-blog:nth-child(2) div.main-container:nth-child(9) div.single:nth-" +
+                "child(4) div.content article.article div.g.post.post-80.page.type-page.status-publish.hentry " +
+                "div.single_page.single_post.clear div.post-single-content.box.mark-links header:nth-child(1)" +
+                " > h1.title"));
+        Assert.assertEquals(text.getText(), "Contact Us");
+    }
+
+
+
+   @Test
+    public void testRegisterForm() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+
+        WebElement genderFem = driver.findElement(By.xpath("//input[@id='gender-female']"));
+        genderFem.click();
+
+        WebElement firstName = driver.findElement(By.xpath("(//input[@id='FirstName'])[1]"));
+        firstName.sendKeys("Kathy");
+
+        WebElement lastName = driver.findElement(By.xpath("(//input[@id='LastName'])[1]"));
+        lastName.sendKeys("Keller");
+
+        WebElement dateBirth = driver.findElement(By.name("DateOfBirthDay"));
+        Select dateBi = new Select(dateBirth);
+        dateBi.selectByValue("10");
+
+        WebElement dateMonth = driver.findElement(By.name("DateOfBirthMonth"));
+        Select dateMo = new Select(dateMonth);
+        dateMo.selectByValue("4");
+
+        WebElement dateYear = driver.findElement(By.name("DateOfBirthYear"));
+        Select dateYe = new Select(dateYear);
+        dateYe.selectByValue("1985");
+
+        WebElement emailReq = driver.findElement(By.name("Email"));
+        emailReq.sendKeys("ritriporki@gufum.com");
+
+        WebElement compName = driver.findElement(By.name("Company"));
+        compName.sendKeys("StyleKelly");
+
+        WebElement passWord = driver.findElement(By.xpath("//input[@id='Password']"));
+        passWord.sendKeys("Valik1234");
+
+        WebElement confPass = driver.findElement(By.xpath("//input[@id='ConfirmPassword']"));
+        confPass.sendKeys("Valik1234");
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[@id='register-button']"));
+        registerButton.click();
+
+
     }
 }
-
 
 
 
