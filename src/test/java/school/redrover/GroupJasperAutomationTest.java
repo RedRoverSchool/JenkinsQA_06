eb6b7b9572fdfa735444e00871b2911962206916
Vidim83 <08091985n@gmail.com>
2023-04-20 19:31:43 +0300

    add testTryToEnter

--- src/test/java/school/redrover/GroupJasperAutomationTest.java
+++ src/test/java/school/redrover/GroupJasperAutomationTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,16 +18,20 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 
 public class GroupJasperAutomationTest extends BaseTest {
-    @Ignore
     @Test
-    public void testFootballua() throws InterruptedException {
 
-        getDriver().get("https://football.ua/");
-        WebElement element = getDriver().findElement(By.xpath("//*[text()='Україна']"));
+    public void footballua() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://football.ua/");
+        WebElement element = driver.findElement(By.xpath("//*[text()='Україна']"));
         Assert.assertEquals(element.getText(), "Україна");
+        driver.quit();
     }
 
-    @Ignore
+
     @Test
     public void testTitle() throws InterruptedException {
         getDriver().get("https://football.ua/");
@@ -87,19 +90,24 @@ public class GroupJasperAutomationTest extends BaseTest {
     }
 
     @Test
-    public void testWebFormSubmission() throws InterruptedException {
-        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+    public void testOlgaMinina() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebElement textBox = getDriver().findElement(By.name("my-text"));
-        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = getDriver().findElement(By.id("message"));
+        WebElement message = driver.findElement(By.id("message"));
         String value = message.getText();
-
         Assert.assertEquals(value, "Received!");
+
+        driver.quit();
     }
 
     @Ignore
@@ -269,19 +277,4 @@ public class GroupJasperAutomationTest extends BaseTest {
         WebElement name = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[3]/ul[1]/li[3]/a[1]"));
         assertEquals(name.getText(), "JAVA");
         driver.quit();
-    }
-
-   @Ignore
-    @Test
-    public void testFindElement() {
-        getDriver().get("https://7745.by/");
-
-        WebElement texBox = getDriver().findElement(By.name("keys"));
-        texBox.sendKeys("процессор");
-        texBox.sendKeys(Keys.RETURN);
-
-        WebElement text = getDriver().findElement(By.xpath("//h1[text() = 'Процессоры для компьютеров']"));
-
-        Assert.assertEquals(text.getText(), "Процессоры для компьютеров");
-    }
-}
+}}
