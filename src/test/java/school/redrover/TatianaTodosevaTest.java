65251c64127a73dd485f04ea4b75972a69217661
Tatiana Todoseva <tatiana.todoseva@gmail.com>
2023-04-20 12:30:52 +0300

    Merge remote-tracking branch 'origin/Todoseva/firstTest' into Todoseva/firstTest

--- src/test/java/school/redrover/TatianaTodosevaTest.java
+++ src/test/java/school/redrover/TatianaTodosevaTest.java
@@ -1,55 +1,38 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;One
-import org.testng.annotations.Test;
-
-import java.time.Duration;
-
-import static org.testng.Assert.assertEquals;
-
-
-
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+import static org.testng.Assert.assertEquals;
 
-public class TatianaTodosevaTest {
-     @Ignore
-     @Test
-    public void firstTest() throws InterruptedException {
+public class TatianaTodosevaTest extends BaseTest {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins", "--headless", "--window-size=1920,1080");
+    private String email;
+    @Test
+    public void firstTest() {
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        String title = driver.getTitle();
+        String title = getDriver().getTitle();
         assertEquals("Web form", title);
 
-        Thread.sleep(2000);
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = driver.findElement(By.id("message"));
+        WebElement message = getDriver().findElement(By.id("message"));
         String value = message.getText();
+
         assertEquals("Received!", value);
 
-        driver.quit();
     }
-    @Ignore
-    @Test
+
+    /*@Test
+
     public void secondTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
@@ -73,7 +56,62 @@ public class TatianaTodosevaTest {
 
         driver.quit();
     }
+    */
+    @Test
+    public void testRegisterUser() throws InterruptedException {
 
+         getDriver().get("http://selenium1py.pythonanywhere.com/ru/accounts/login/");
 
+         WebElement registr_email = getDriver().findElement(By.cssSelector("input[name='registration-email']"));
+         registr_email.sendKeys(email);
+
+         WebElement password1 = getDriver().findElement(By.cssSelector("input[name='registration-password1']"));
+         password1.sendKeys(email);
+
+         WebElement password2 = getDriver().findElement(By.cssSelector("input[name='registration-password2']"));
+         password2.sendKeys(email);
+
+         getDriver().findElement(By.cssSelector("button[name='registration_submit']")).click();
+
+         Thread.sleep(4000);
+
+        /*Thread.sleep(6000);
+
+        try {
+            WebElement success_registration = getDriver().findElements(By.cssSelector("alertinner.wicon"));
+        } catch (Exception ex) {
+            WebElement success_registration = null;
+        } */
+        boolean flag = false;
+        if (!getDriver().findElements(By.cssSelector("alertinner.wicon")).isEmpty()) {
+            flag = true;
+        }
+
+        assertEquals(flag, true);
+    }
+
+    @Test(priority = 2)
+    public void testLoginUser() throws InterruptedException {
+
+        getDriver().get("http://selenium1py.pythonanywhere.com/ru/accounts/login/");
+
+        WebElement login_email = getDriver().findElement(By.cssSelector("input[name='login-username']"));
+        login_email.sendKeys(email);
+
+        WebElement password = getDriver().findElement(By.cssSelector("input[name='login-password']"));
+        password.sendKeys(email);
+
+        getDriver().findElement(By.cssSelector("button[name='login_submit']")).click();
+
+        Thread.sleep(6000);
+
+        boolean flag = false;
+        if (!getDriver().findElements(By.cssSelector("alertinner.wicon")).isEmpty()) {
+            flag = true;
+        }
+
+        assertEquals(flag, true);
+    }
 }
+
 
