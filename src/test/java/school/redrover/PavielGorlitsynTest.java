ba8662164d93021a126a8e3e33e3bc3581ebffa7
Paviel Gorlitsyn <paviel.gorlitsyn@gmail.com>
2023-04-20 22:29:21 -0400

    Updated test name, method name, sleep updated, format fixed and before method implemented.

--- src/test/java/school/redrover/PavielGorlitsynTest.java
+++ src/test/java/school/redrover/PavielGorlitsynTest.java
@@ -1,44 +1,37 @@
 package school.redrover;
 
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+import java.time.Duration;
 
-public class PavielGorlitsynTest {
 
-    @Ignore
+public class PavielGorlitsynTest extends BaseTest {
+    String expectedURL= "https://www.youtube.com/";
+    String actualURL;
+
     @Test
-    public void firstTest() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+    public void testYoutubeSearch() throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.google.com/");
-        Thread.sleep(2000);
-        WebElement textBox= driver.findElement(By.xpath("//*[@name= 'q']"));
-        Thread.sleep(2000);
+        getDriver().get("https://www.google.com/");
+
+        WebElement textBox= getDriver().findElement(By.xpath("//*[@name= 'q']"));
         textBox.sendKeys("Youtube");
-        Thread.sleep(2000);
-        WebElement search= driver.findElement(By.xpath("//*[@name= 'btnK']"));
-        Thread.sleep(2000);
+
+        WebElement search= getDriver().findElement(By.xpath("//*[@name= 'btnK']"));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
         search.click();
-        WebElement header = driver.findElement(By.xpath("//h3[@class=\"LC20lb MBeuO DKV0Md\"]"));
-        Thread.sleep(6000);
+
+        WebElement header =getDriver().findElement(By.xpath("//h3[@class=\"LC20lb MBeuO DKV0Md\"]"));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
         header.click();
-        String expectedURL= "https://www.youtube.com/";
         Thread.sleep(3000);
-        String actualURL= driver.getCurrentUrl();
-        Thread.sleep(2000);
+
+        actualURL= getDriver().getCurrentUrl();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
         Assert.assertEquals(actualURL,expectedURL);
-        driver.quit();
-
-
     }
 }
-
