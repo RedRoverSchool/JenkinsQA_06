76bd7f70d4f020eaac890a406e4a67a21321d2db
tymocoderod <tymocoder@gmail.com>
2023-04-21 11:43:27 -0700

    Add test testVerifyLogoJenkinsIsPresentTC_001_33

---
+++ src/test/java/school/redrover/AlexLeoGroupTests.java
@@ -0,0 +1,16 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class AlexLeoGroupTests extends BaseTest {
+
+    @Test
+    public void testVerifyLogoJenkinsIsPresent() {
+        WebElement element = getDriver().findElement(By.cssSelector("img#jenkins-head-icon"));
+        Assert.assertTrue(element.isDisplayed());
+    }
+}
