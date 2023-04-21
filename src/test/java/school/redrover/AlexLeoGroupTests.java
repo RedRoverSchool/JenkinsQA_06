721d6ce99294ae71a8e0f8e854426df86027fdaa
tymocoderod <tymocoder@gmail.com>
2023-04-21 12:00:58 -0700

    Add testVerifyWordIconJenkinsPresentTC_002_33

--- src/test/java/school/redrover/AlexLeoGroupTests.java
+++ src/test/java/school/redrover/AlexLeoGroupTests.java
@@ -13,4 +13,11 @@ public class AlexLeoGroupTests extends BaseTest {
         WebElement element = getDriver().findElement(By.cssSelector("img#jenkins-head-icon"));
         Assert.assertTrue(element.isDisplayed());
     }
+
+    @Test
+    public void testVerifyWordIconJenkinsPresentTC_002_33() {
+        WebElement logoWord = getDriver()
+                .findElement(By.id("jenkins-name-icon"));
+        Assert.assertEquals(logoWord.getAttribute("src"), "http://localhost:8081/static/bc90ee8a/images/title.svg");
+    }
 }
