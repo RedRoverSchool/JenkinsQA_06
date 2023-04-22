059a8dacc6d45e2bd2625871794e30fd105cbd29
Iriskus <irliatuz@gmail.com>
2023-04-22 15:33:52 -0500

    Remove sleep

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -3,17 +3,29 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
+
 public class PipelineTest extends BaseTest {
+
+    public WebDriverWait webDriverWait10;
 
     public void scrollByElement(By by) throws InterruptedException {
         WebElement scroll =getDriver().findElement(by);
         new Actions(getDriver())
                 .scrollToElement(scroll)
                 .perform();
+    }
+
+    public final WebDriverWait getWait10() {
+        if (webDriverWait10 == null) {
+            webDriverWait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+        return webDriverWait10;
     }
     @Test
     public void testCreatePipeline() throws InterruptedException {
@@ -30,19 +42,12 @@ public class PipelineTest extends BaseTest {
         WebElement buttonOk = getDriver().findElement(By.cssSelector("#ok-button"));
         buttonOk.click();
 
-        Thread.sleep(3000);
-
         scrollByElement(By.cssSelector(".page-footer__links.page-footer__links--white.jenkins_ver>a"));
-        Thread.sleep(3000);
-
         WebElement save = getDriver().findElement(By.cssSelector(".jenkins-button.jenkins-button--primary"));
-        Thread.sleep(3000);
         save.click();
 
         WebElement pipelinePipe= getDriver().findElement(By.cssSelector("#main-panel>h1"));
 
         Assert.assertEquals(pipelinePipe.getText(), "Pipeline Pipe");
-
     }
-
 }
