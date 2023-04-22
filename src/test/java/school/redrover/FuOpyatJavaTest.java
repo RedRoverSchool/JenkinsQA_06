032a43994577b77ec97db8eb0233babd71c19e42
Svetlana <svetlanacherniaieva@gmail.com>
2023-04-22 09:19:36 -0700

    Add First Test

---
+++ src/test/java/school/redrover/FuOpyatJavaTest.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FuOpyatJavaTest extends BaseTest {
+
+    @Test
+    public void testTitle() {
+
+        WebElement title = getDriver().findElement(By.id("id=\"jenkins-name-icon\""));
+
+        Assert.assertEquals(title.getText(), "Jenkins");
+    }
+
+    @Test
+    public void tesLogoIsDisplayed() {
+
+        WebElement logo = getDriver().findElement(By.id("jenkins-head-icon"));
+
+        Assert.assertTrue(logo.isDisplayed());
+    }
+
+}
