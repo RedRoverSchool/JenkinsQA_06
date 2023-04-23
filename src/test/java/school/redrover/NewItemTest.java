13c6415a579024327f868d5a37349236eb871b87
Evan Mai <mai.evan@yahoo.com>
2023-04-23 15:37:38 -0700

    New test

--- src/test/java/school/redrover/NewItemTest.java
+++ src/test/java/school/redrover/NewItemTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class NewItemTest extends BaseTest {
 
@@ -22,5 +24,18 @@ public class NewItemTest extends BaseTest {
         String actualResult = h3Header.getText();
 
         Assert.assertEquals(actualResult, "Enter an item name");
+    }
+
+    @Test
+    public void testVerifyNewItemsList() {
+        List<String> listOfNewItemsExpect = Arrays.asList("Freestyle project", "Pipeline", "Multi-configuration project", "Folder", "Multibranch Pipeline", "Organization Folder");
+
+        getDriver().findElement(By.cssSelector("a[href='/view/all/newJob']")).click();
+
+        List<WebElement> listOfNewItems = getDriver().findElements(By.cssSelector("label > span"));
+
+        for (int i = 0; i < listOfNewItemsExpect.size(); i++) {
+            Assert.assertEquals(listOfNewItems.get(i).getText(), listOfNewItemsExpect.get(i));
+        }
     }
 }
