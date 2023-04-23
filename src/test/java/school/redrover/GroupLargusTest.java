5b1a8cacc5164e051c8da8d5975da5b8cce88e49
Ivan Korolev <ikorolev13@gmail.com>
2023-04-23 11:33:52 +0400

    GroupLargusTest/IvanK/ add first test at Jenkins (#916)
    * GroupLargusTest/IvanK/ add first test at Jenkins
    
    * GroupLargusTest/IvanK/ add first test at Jenkins with change
    
    * GroupLargusTest/IvanK/ add second test
    
    * GroupLargusTest/IvanK/ add second test with correct

--- src/test/java/school/redrover/GroupLargusTest.java
+++ src/test/java/school/redrover/GroupLargusTest.java
@@ -2,7 +2,10 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+<<<<<<< HEAD
 import org.openqa.selenium.support.FindBy;
+=======
+>>>>>>> de78ff3d (GroupLargusTest/IvanK/ add first test at Jenkins (#916))
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,6 +17,7 @@ import java.time.Duration;
 import java.util.Date;
 
 public class GroupLargusTest extends BaseTest {
+<<<<<<< HEAD
     private static class StringUtility {
         private static String uniqueStr(String str) {
             return str + " " + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -52,14 +56,29 @@ public class GroupLargusTest extends BaseTest {
 //        getDriver().findElement(By.xpath("//div[@id='tasks']//a[contains(@href, 'newJob')]")).click();
 //        linkNewJob.click();
         waitForClickOnElement(linkNewJob).click();
+=======
+
+    private final String projectName = "TestProject_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+    @Test
+    public void testCreatedOrgFolderIsDisplayedInProjectListOnMainPage() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+
+        getDriver().findElement(By.xpath("//div[@id='tasks']//a[contains(@href, 'newJob')]")).click();
+>>>>>>> de78ff3d (GroupLargusTest/IvanK/ add first test at Jenkins (#916))
 
         WebElement inputProjectName = getDriver()
                 .findElement(By.xpath("//input[@id='name']"));
 
+<<<<<<< HEAD
 //        wait.until(ExpectedConditions.visibilityOf(inputProjectName));
 //        inputProjectName.sendKeys(projectName);
         String projectName = StringUtility.uniqueStr("TestProject");
         waitElementVisible(inputProjectName).sendKeys(projectName);
+=======
+        wait.until(ExpectedConditions.visibilityOf(inputProjectName));
+        inputProjectName.sendKeys(projectName);
+>>>>>>> de78ff3d (GroupLargusTest/IvanK/ add first test at Jenkins (#916))
 
         WebElement itemOrgFolder = getDriver()
                 .findElement(By.xpath("//span[text()='Organization Folder']/../.."));
@@ -67,7 +86,11 @@ public class GroupLargusTest extends BaseTest {
         WebElement buttonOK = getDriver().findElement(By.cssSelector("button.jenkins-button"));
         buttonOK.click();
 
+<<<<<<< HEAD
         //WebElement buttonHome = getDriver().findElement(By.id("jenkins-home-link"));
+=======
+        WebElement buttonHome = getDriver().findElement(By.id("jenkins-home-link"));
+>>>>>>> de78ff3d (GroupLargusTest/IvanK/ add first test at Jenkins (#916))
         buttonHome.click();
 
         WebElement fieldNameInTable = getDriver()
@@ -88,7 +111,10 @@ public class GroupLargusTest extends BaseTest {
                 .findElement(By.xpath("//input[@id='name']"));
 
         wait.until(ExpectedConditions.visibilityOf(inputProjectName));
+<<<<<<< HEAD
         String projectName = StringUtility.uniqueStr("TestProject");
+=======
+>>>>>>> de78ff3d (GroupLargusTest/IvanK/ add first test at Jenkins (#916))
         inputProjectName.sendKeys(projectName);
 
         WebElement itemFolder = getDriver()
