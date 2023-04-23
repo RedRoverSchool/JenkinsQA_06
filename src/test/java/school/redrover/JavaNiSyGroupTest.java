3aa85ac023d9e2424aa67f3c5555c7e38f993158
Your Name <ivann.plotnikov@mail.ru>
2023-04-23 16:46:54 +0700

    NewClass and first test NiSyGroup

---
+++ src/test/java/school/redrover/JavaNiSyGroupTest.java
@@ -0,0 +1,37 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+
+public class JavaNiSyGroupTest extends BaseTest {
+
+    @Test
+    public void testFullNameVerification() throws InterruptedException {
+
+       Actions actions = new Actions(getDriver());
+
+       WebElement dropBox = (new WebDriverWait(getDriver(), Duration.ofSeconds(10))
+               .until(ExpectedConditions.presenceOfElementLocated(
+                       By.xpath("//a[@href='/user/admin']//button"))));
+       actions.moveToElement(dropBox).click().build().perform();
+
+       WebElement btnConfigure = getDriver().findElement(
+                By.xpath("//ul[@class='first-of-type']//a[@href = '/user/admin/configure']"));
+       actions.moveToElement(btnConfigure).click().build().perform();
+
+       String inputFullName = (new WebDriverWait(getDriver(), Duration.ofSeconds(10))
+               .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='_.fullName']"))).getAttribute("value"));
+      getDriver().findElement(By.xpath("//input[@name='_.fullName']")).getAttribute("value");
+       Assert.assertEquals(inputFullName, "admin");
+
+
+    }
+}
