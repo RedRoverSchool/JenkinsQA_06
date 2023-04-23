3cf3e34d3ef45628ff44691f2b976fc7c21f0e7a
Nataliia Diekmann <nataliia.diekmann@gmail.com>
2023-04-22 21:38:16 -0500

    Checking Configuration - version 4

--- src/test/java/school/redrover/GroupJavaJitsuTest.java
+++ src/test/java/school/redrover/GroupJavaJitsuTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-
 import java.time.Duration;
 public class GroupJavaJitsuTest extends BaseTest {
     @Test
