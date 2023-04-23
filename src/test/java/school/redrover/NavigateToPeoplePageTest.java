5a8d82f826ab175397c861da05e6a046ae33b6ed
Galina Sedunova <sgedunova@gmail.com>
2023-04-23 13:58:09 +0300

    deleted removed unnecessary import

--- src/test/java/school/redrover/NavigateToPeoplePageTest.java
+++ src/test/java/school/redrover/NavigateToPeoplePageTest.java
@@ -1,15 +1,9 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
 
 public class NavigateToPeoplePageTest extends BaseTest {
 
