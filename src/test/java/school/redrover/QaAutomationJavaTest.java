24e17243dd80682755464ba6867e92730e2bf4ae
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-04-23 13:42:42 +0200

    Class for group2

--- src/test/java/school/redrover/QaAutomationJavaTest.java
+++ src/test/java/school/redrover/QaAutomationJavaTest.java
@@ -8,9 +8,10 @@ import school.redrover.runner.BaseTest;
 
 public class QaAutomationJavaTest extends BaseTest {
     @Test
-    public void testSimple() {
+    public void testOne() throws InterruptedException {
         WebElement welcomeElement = getDriver().findElement(By.xpath("//div[@class = 'empty-state-block']/h1"));
+        Thread.sleep(2000);
 
-        Assert.assertEquals(welcomeElement.getText(), "Welcome to Jenkins!");
+        Assert.assertEquals(welcomeElement.getText(), "Добро пожаловать в Jenkins!");
     }
 }
