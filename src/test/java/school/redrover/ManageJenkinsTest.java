42dafbed61bad0aafb1b294ece160db753da8327
Aleksandr Akimov <gtwood@yandex.ru>
2023-04-23 13:41:49 +0300

    add test ерwhen check the name of a new noda

---
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -0,0 +1,31 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class ManageJenkinsTest extends BaseTest {
+
+
+    @Test
+    public void testAddNewNode() {
+        final String testNameNewNode = "testNameNewNode";
+
+        WebElement buildExecutorStatus = getDriver().findElement(By.xpath("//a[@href='/computer/']"));
+        buildExecutorStatus.click();
+        WebElement newNodeButton = getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='new']"));
+        newNodeButton.click();
+        WebElement inputNodeName = getDriver().findElement(By.xpath("//input[@id='name']"));
+        inputNodeName.sendKeys(testNameNewNode);
+        WebElement permanentAgentRadioButton = getDriver().findElement(By.xpath("//label"));
+        permanentAgentRadioButton.click();
+        WebElement createButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        createButton.click();
+        WebElement nameField = getDriver().findElement(By.xpath("//input[@name='name']"));
+        String actualValueName = nameField.getAttribute("value");
+
+        Assert.assertEquals(actualValueName, testNameNewNode);
+    }
+}
