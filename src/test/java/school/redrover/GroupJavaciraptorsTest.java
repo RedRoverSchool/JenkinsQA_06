d6609fda4ac5e93f868cd3d3f9bfd8c8ae01a653
BayAzat <azatbay.job@gmail.com>
2023-04-24 17:40:27 +0500

    add Test addDescriptionTest

--- src/test/java/school/redrover/GroupJavaciraptorsTest.java
+++ src/test/java/school/redrover/GroupJavaciraptorsTest.java
@@ -22,4 +22,25 @@ public class GroupJavaciraptorsTest extends BaseTest {
         WebElement textElement = getDriver().findElement(By.xpath("//h2[normalize-space()='Set up a distributed build']"));
         Assert.assertEquals(textElement.getText(), "Set up a distributed build");
     }
+
+    @Test
+    public void addDescriptionTest() throws InterruptedException {
+        WebElement addDescription = getDriver().findElement(By.xpath("//*[@id='description-link']"));
+        addDescription.click();
+
+        Thread.sleep(2000);
+
+        WebElement textArea = getDriver().findElement(By.xpath("//textarea[@name='description']"));
+        textArea.sendKeys("It`s my 1st Jenkins!");
+
+        Thread.sleep(2000);
+
+        WebElement buttonSave = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        buttonSave.click();
+
+        WebElement textDescription = getDriver().findElement(By.xpath("//*[@id='description']/div[1]"));
+
+        Assert.assertEquals(textDescription.getText(), "It`s my 1st Jenkins!");
+        Thread.sleep(2000);
+    }
 }
