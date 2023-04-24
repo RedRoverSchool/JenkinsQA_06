6595d550d478901c4a498feb5aeb37cceb7e0a89
Oleh Yermolenko <57632851+eratheo@users.noreply.github.com>
2023-04-24 01:40:43 -0500

    Changed xpath of drop down elements

--- src/test/java/school/redrover/TestWaitOnDropDown.java
+++ src/test/java/school/redrover/TestWaitOnDropDown.java
@@ -3,10 +3,13 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,14 +27,13 @@ public class TestWaitOnDropDown extends BaseTest {
     }
 
     @Test
-    public void testTopMenuUserDropDown() throws InterruptedException {
+    public void testTopMenuUserDropDown() {
         Actions actions = new Actions(getDriver());
         WebElement topMenuUserDropDown = getDriver().findElement(By.xpath("//div/a[@class='model-link']/button[@class='jenkins-menu-dropdown-chevron']"));
         actions.click(topMenuUserDropDown).perform();
 
-        Thread.sleep(2000);
-
-        List<WebElement> listTopElements = getDriver().findElements(By.xpath("//li[@groupindex = '0']/a/span"));
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//div/a[@class='model-link']/button[@class='jenkins-menu-dropdown-chevron']")));
+        List<WebElement> listTopElements = getDriver().findElements(By.xpath("//ul[@class='first-of-type']/li"));
         List<String> expected = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
         List<String>  actual = getNamesOfLists(listTopElements);
 
