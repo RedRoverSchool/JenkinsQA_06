95a102b754f20a7f7cfffff17fc465738e88f4ed
Oleh Yermolenko <57632851+eratheo@users.noreply.github.com>
2023-04-24 00:07:16 -0500

    Changed xpath expression to wait for menu to load

--- src/test/java/school/redrover/TestWaitOnDropDown.java
+++ src/test/java/school/redrover/TestWaitOnDropDown.java
@@ -32,7 +32,7 @@ public class TestWaitOnDropDown extends BaseTest {
         WebElement topMenuUserDropDown = getDriver().findElement(By.xpath("//div/a[@class='model-link']/button[@class='jenkins-menu-dropdown-chevron']"));
         actions.click(topMenuUserDropDown).perform();
 
-        new WebDriverWait(getDriver(), Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, '/user/') and @class = 'yuimenuitemlabel']")));
+        new WebDriverWait(getDriver(), Duration.ofSeconds(5)).until(ExpectedConditions.attributeContains(By.xpath("//div[@id = 'breadcrumb-menu']"), "style", "visibility: visible"));
         List<WebElement> listTopElements = getDriver().findElements(By.xpath("//a[contains(@href, '/user/') and @class = 'yuimenuitemlabel']"));
         List<String> expected = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
         List<String>  actual = getNamesOfLists(listTopElements);
