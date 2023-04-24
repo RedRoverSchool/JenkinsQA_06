d460bc6d77982624be6ca8e4981cbaf752d9c7b1
Oleh Yermolenko <57632851+eratheo@users.noreply.github.com>
2023-04-23 22:32:19 -0500

    Test wait for user drop down menu

---
+++ src/test/java/school/redrover/TestWaitOnDropDown.java
@@ -0,0 +1,42 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestWaitOnDropDown extends BaseTest {
+
+    public List<String> getNamesOfLists(List<WebElement> elements) {
+        List<String> texts = new ArrayList<>();
+
+        for (WebElement element : elements) {
+            texts.add(element.getText());
+        }
+
+        return texts;
+    }
+
+    @Test
+    public void testTopMenuUserDropDown() {
+        Actions actions = new Actions(getDriver());
+        WebElement topMenuUserDropDown = getDriver().findElement(By.xpath("//div/a[@class='model-link']/button[@class='jenkins-menu-dropdown-chevron']"));
+        actions.click(topMenuUserDropDown).perform();
+
+        new WebDriverWait(getDriver(), Duration.ofSeconds(5)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul[@class='first-of-type']")));
+        List<WebElement> listTopElements = getDriver().findElements(By.xpath("//ul[@class='first-of-type']/li"));
+        List<String> expected = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
+        List<String>  actual = getNamesOfLists(listTopElements);
+
+        Assert.assertEquals(actual,expected);
+    }
+}
