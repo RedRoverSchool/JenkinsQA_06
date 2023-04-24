4cdc4ccf076e4aff4bf6309355283a521e967e9c
Stoyana <dicheva.t@gmail.com>
2023-04-24 12:57:15 +0300

    remove spaces between the rows

--- src/test/java/school/redrover/GroupOlesyaTest.java
+++ src/test/java/school/redrover/GroupOlesyaTest.java
@@ -1,17 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
-
 public class GroupOlesyaTest extends BaseTest {
+
     @Test
     public void testInputHelpMsg() {
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
