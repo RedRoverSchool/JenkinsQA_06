91f26bd3ae443d44fe6c66d63100be68080c594a
Oleh Yermolenko <57632851+eratheo@users.noreply.github.com>
2023-04-24 23:47:44 -0500

    Played with xpath

--- src/test/java/school/redrover/TestWaitOnDropDown.java
+++ src/test/java/school/redrover/TestWaitOnDropDown.java
@@ -34,7 +34,7 @@ public class TestWaitOnDropDown extends BaseTest {
 
         new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href = '/user/admin/configure']")));
         List<WebElement> listTopElements = getDriver().findElements(By.xpath("//li[@groupindex = '0']/a/span"));
-        System.out.println(listTopElements.size());
+        System.out.println("OLEGTEST: " + listTopElements.size());
         List<String> expected = Arrays.asList("Builds", "Configure", "My Views", "Credentials");
         List<String> actual = getNamesOfLists(listTopElements);
 
