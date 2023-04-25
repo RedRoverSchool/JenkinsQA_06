ef824b3e7842e5df5f29206aece543a8851bd407
V.Rumeantsev <slava2045@gmail.com>
2023-04-25 15:08:36 +0300

    add test testDescriptionEdit

--- src/test/java/school/redrover/HelloWorldGroupTest.java
+++ src/test/java/school/redrover/HelloWorldGroupTest.java
@@ -13,4 +13,16 @@ public class HelloWorldGroupTest extends BaseTest{
         WebElement version = getDriver().findElement(By.xpath("//a[@rel='noopener noreferrer']"));
         Assert.assertEquals(version.getText(),"Jenkins 2.387.2");
     }
+
+    @Test
+    public void testDescriptionEdit(){
+        WebElement descr = getDriver().findElement(By.xpath("//*[@id='description-link']"));
+        descr.click();
+        WebElement descrArea = getDriver().findElement(By.xpath("//div[@id='description']//textarea"));
+        descrArea.sendKeys("hello");
+        WebElement saveBtn = getDriver().findElement(By.xpath("//*[@id='description']/form/div[2]/button"));
+        saveBtn.click();
+        WebElement descrText = getDriver().findElement(By.xpath("//*[@id='description']/div[1]"));
+        Assert.assertEquals(descrText.getText(), "hello");
+    }
 }
