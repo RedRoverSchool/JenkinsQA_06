50b3a2d7a6f70504749fd21de82dc4dc137cbcec
Anna <hanna_limutsina@senla.eu>
2023-04-25 17:18:09 +0300

    add addNewItem test

--- src/test/java/school/redrover/BestAutoTeamTest.java
+++ src/test/java/school/redrover/BestAutoTeamTest.java
@@ -88,9 +88,5 @@ public class BestAutoTeamTest extends BaseTest {
 
         Assert.assertEquals(titleOfPage.getText(), "Project New Item adding test");
         Assert.assertEquals(buttonAddDescription.getText(), "Add description");
-
-
-
-
     }
 }
