296307998b4d3247a42f09b55344fc1e1cb9da76
RykovaMariia <maryrykova24@gmail.com>
2023-04-25 23:07:41 +0400

    Menu Dropdown Chevron test

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -147,12 +147,11 @@ public class CaramelSyrupForJavaTest extends BaseTest {
         WebElement menu =
                 getDriver().findElement(By.xpath("//span[text() ='admin']/following-sibling::button"));
         Actions actions = new Actions(getDriver());
-        actions.moveToElement(menu).click(menu);
+        actions.moveToElement(menu).click(menu).perform();
         actions.moveToElement(menu).perform();
-
         List<WebElement> breadcrumbMenu =
                 getDriver().findElements(By.xpath("//ul[@class='first-of-type']/li"));
-        actions.moveToElement(menu).perform();
+        actions.moveToElement(breadcrumbMenu.get(0)).perform();
 
         Assert.assertEquals(breadcrumbMenu.get(0).getText(),"Builds");
         Assert.assertEquals(breadcrumbMenu.get(1).getText(),"Configure");
