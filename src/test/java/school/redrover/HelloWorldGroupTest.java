1430558d1c25cd733a9ad9928a18c0149aed46fb
V.Rumeantsev <slava2045@gmail.com>
2023-04-26 10:40:38 +0300

    add test testDescriptionEdit

--- src/test/java/school/redrover/HelloWorldGroupTest.java
+++ src/test/java/school/redrover/HelloWorldGroupTest.java
@@ -38,4 +38,16 @@ public class HelloWorldGroupTest extends BaseTest{
 
         Assert.assertEquals(nameFolder.getText(),"Name");
     }
+
+    @Test
+    public void testDescriptionEdit(){
+        WebElement descr = getDriver().findElement(By.xpath("//*[@id='description-link']"));
+        descr.click();
+        WebElement descrArea = getDriver().findElement(By.xpath("//div[@id='description']//textarea"));
+        descrArea.sendKeys("hello");
+        WebElement saveBtn = getDriver().findElement(By.xpath("//*[@id='description']/form/div[2]/button"));
+        saveBtn.click();
+        WebElement descrText = getDriver().findElement(By.xpath("//*[@id='description']/div[1]"));
+        Assert.assertEquals(descrText.getText(), "hello");
+    }
 }
