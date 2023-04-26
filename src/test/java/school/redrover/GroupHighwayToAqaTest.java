59cc49d986a552b7d480899d5b5e18e99c45ebf3
git_repo_user <a.romanovskiy94@gmail.com>
2023-04-26 10:39:13 +0300

    delete unused import

--- src/test/java/school/redrover/GroupHighwayToAqaTest.java
+++ src/test/java/school/redrover/GroupHighwayToAqaTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
