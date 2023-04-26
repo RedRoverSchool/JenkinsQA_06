a7031a3dd15078d7039070f50f80319027a0e286
Andrey Dushenin <onizuh8@gmail.com>
2023-04-26 13:46:20 +0300

    add new test testButtonDiscardOldBuilds

--- src/test/java/school/redrover/GroupAndreyTest.java
+++ src/test/java/school/redrover/GroupAndreyTest.java
@@ -8,7 +8,7 @@ import static org.testng.Assert.assertEquals;
 
 public class GroupAndreyTest extends BaseTest {
     @Test
-    public void testProjectCreation(){
+    public void testProjectCreation() {
         getDriver().findElement(By.xpath("//*[@href=\"newJob\"]")).click();
 
         getDriver().findElement(By.name("name")).sendKeys("qwerty");
@@ -24,4 +24,21 @@ public class GroupAndreyTest extends BaseTest {
         assertEquals(getDriver()
                 .findElement(By.xpath("//*[@id=\"job_qwerty\"]/td[3]/a/span")).getText(), "qwerty");
     }
+    @Test
+    public void testButtonDiscardOldBuilds() {
+        getDriver().findElement(By.xpath("//*[@href=\"newJob\"]")).click();
+
+        getDriver().findElement(By.name("name")).sendKeys("qwerty");
+
+        getDriver().findElement(By.xpath("//*[@class=\"hudson_model_FreeStyleProject\"]")).click();
+
+        getDriver().findElement(By.xpath("//*[@id=\"ok-button\"]")).click();
+
+        getDriver().findElement(By.className("attach-previous")).click();
+
+        assertEquals(getDriver()
+                .findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/div[3]/div[2]/div[2]/div[2]/div[4]/div[1]/div[1]"))
+                .getText(), "Strategy");
+    }
 }
+
