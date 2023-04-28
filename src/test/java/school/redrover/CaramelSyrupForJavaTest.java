1e810c91acb49a8354c99d75302781a8619cfc9c
Artyom Dulya <fecc1612@gmail.com>
2023-04-28 18:00:23 +0500

    testLearnMoreHeadles

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -278,6 +278,41 @@ public class CaramelSyrupForJavaTest extends BaseTest {
 
         Assert.assertNotEquals(changedDimension, initialDimension);
     }
+
+    @Test
+    public void testADLearnMoreHeaders() {
+
+        List<String> expectedResult = Arrays.asList("Enter an item name", "New node", "Configure Clouds", "Jenkins");
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='newJob']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//label[text()='Enter an item name']")).getText(), expectedResult.get(0));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='computer/new']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='New node']")).getText(), expectedResult.get(1));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='configureClouds']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='Configure Clouds']")).getText(), expectedResult.get(2));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//span[text()='Learn more about distributed builds']")).click();
+        List<String> windows = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(windows.get(1));
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[text()='Jenkins']")).getText(), expectedResult.get(3));
+    }
+
+    @Test
+    public void testADConfigureCloud() throws InterruptedException {
+
+        String expectedResult = "No updates";
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='configureClouds']")).click();
+        getDriver().findElement(By.xpath("//a[text()='Go to plugin manager.']")).click();
+        getDriver().findElement(By.xpath("//span[@class='task-link-wrapper ']//a[@href='/manage/pluginManager/']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[text()='No updates']")).getText(), expectedResult);
+    }
 }
 
 
