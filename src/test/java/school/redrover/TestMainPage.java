2c956413adb99c6868e8d936cb7e757e5194b756
Marina Visneacova <marina.dobic90@gmail.com>
2023-04-29 19:54:30 -0400

    testCreateJob

--- src/test/java/school/redrover/TestMainPage.java
+++ src/test/java/school/redrover/TestMainPage.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestMainPage extends BaseTest {
-    @Ignore
+
     @Test
     public void testMainPage() {
 
@@ -35,5 +36,18 @@ public class TestMainPage extends BaseTest {
         WebElement newDescription = new WebDriverWait(getDriver(), Duration.ofSeconds(3))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#description > div:nth-child(1)")));
         Assert.assertEquals(newDescription.getText(), "RRSDevgeni Jenkins_06");
+    }
+    @Test
+    public void testCreateJob() throws InterruptedException {
+        getDriver().findElement(By.xpath("(//section[@class='empty-state-section'] )[1]//li")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//label[@class='h3']")).getText(), "Enter an item name");
+
+        List<WebElement> getLabelList = getDriver().findElements(By.xpath("//span[@class='label']"));
+        List<String> expectedLabelList = Arrays.asList("Freestyle project","Pipeline","Multi-configuration project",
+                "Folder", "Multibranch Pipeline", "Organization Folder");
+        for(int i = 0; i < getLabelList.size(); i++){
+            Assert.assertEquals(getLabelList.get(i).getText(), expectedLabelList.get(i));
+        }
     }
 }
