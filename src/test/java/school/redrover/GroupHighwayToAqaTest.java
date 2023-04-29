e296cf8cde1ad737522c8ba3b67aeda8c6f052a9
Ruslan Fursa <fursa_1986@mail.ru>
2023-04-29 10:41:22 +0300

    refactor

--- src/test/java/school/redrover/GroupHighwayToAqaTest.java
+++ src/test/java/school/redrover/GroupHighwayToAqaTest.java
@@ -316,6 +316,61 @@ public class GroupHighwayToAqaTest extends BaseTest {
 
     @Test
     public void testCreateNewUser() {
+//        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
+//
+//        WebElement manageJenkinsTab = getDriver().findElement(By.xpath("//a[@href = '/manage']"));
+//        wait.until(ExpectedConditions.elementToBeClickable(manageJenkinsTab)).click();
+//
+//        WebElement manageUsersSection = getDriver().findElement(By.xpath("//a[@href = 'securityRealm/']"));
+//        manageUsersSection.click();
+//
+//        WebElement createUserBtn = getDriver().findElement(By.xpath("//a[@href = 'addUser']"));
+//        createUserBtn.click();
+//
+//        WebElement userNameField = getDriver().findElement(By.xpath("//input[@name = 'username']"));
+//        String userName = generateName();
+//        userNameField.sendKeys(userName);
+//
+//        WebElement passwordField = getDriver().findElement(By.xpath("//input[@name = 'password1']"));
+//        String password = generatePassword();
+//        passwordField.sendKeys(password);
+//
+//        WebElement confirmPasswordField = getDriver().findElement(By.xpath("//input[@name = 'password2']"));
+//        confirmPasswordField.sendKeys(password);
+//
+//        WebElement fullNameField = getDriver().findElement(By.xpath("//input[@name = 'fullname']"));
+//        String fullName = userName + " " + generateLastName();
+//        fullNameField.sendKeys(fullName);
+//
+//        WebElement emailField = getDriver().findElement(By.xpath("//input[@name = 'email']"));
+//        String email = generateEmail();
+//        emailField.sendKeys(email);
+//
+//        WebElement createBtn = getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
+//        createBtn.click();
+        String userName = createUser();
+
+        List<WebElement> users = getDriver().findElements(By.xpath("//a[@class ='jenkins-table__link model-link inside']"));
+
+        Assert.assertTrue(isUserExist(users, userName));
+        deleteUser(userName);
+    }
+    @Test
+    public void testDeleteUser() throws InterruptedException {
+        String userName = createUser();
+        deleteUser(userName);
+
+//        WebElement trashBtn = getDriver().findElement(By.xpath("//a[@href = 'user/" + userName.toLowerCase() + "/delete']"));
+//        trashBtn.click();
+//
+//        WebElement confirmBtn = getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
+//        confirmBtn.click();
+
+        List<WebElement> users = getDriver().findElements(By.xpath("//a[@class ='jenkins-table__link model-link inside']"));
+
+        Assert.assertFalse(isUserExist(users, userName));
+    }
+    private String createUser(){
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
 
         WebElement manageJenkinsTab = getDriver().findElement(By.xpath("//a[@href = '/manage']"));
@@ -339,19 +394,21 @@ public class GroupHighwayToAqaTest extends BaseTest {
         confirmPasswordField.sendKeys(password);
 
         WebElement fullNameField = getDriver().findElement(By.xpath("//input[@name = 'fullname']"));
-        String fullName = userName + " " + generateLastName();
-        fullNameField.sendKeys(fullName);
+        fullNameField.sendKeys(userName + " " + generateLastName());
 
         WebElement emailField = getDriver().findElement(By.xpath("//input[@name = 'email']"));
-        String email = generateEmail();
-        emailField.sendKeys(email);
+        emailField.sendKeys(generateEmail());
 
         WebElement createBtn = getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
         createBtn.click();
+        return userName;
+    }
+    private void deleteUser(String userName){
+        WebElement trashBtn = getDriver().findElement(By.xpath("//a[@href = 'user/" + userName.toLowerCase() + "/delete']"));
+        trashBtn.click();
 
-        List<WebElement> users = getDriver().findElements(By.xpath("//a[@class ='jenkins-table__link model-link inside']"));
-
-        Assert.assertTrue(isUserExist(users, userName));
+        WebElement confirmBtn = getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
+        confirmBtn.click();
     }
     private String generateName() {
         Faker faker = new Faker();
