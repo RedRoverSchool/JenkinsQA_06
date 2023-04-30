5fbfe1e851a29a44a03eb96a8a5abe4b497b321f
Max Styosov <avardn@gmail.com>
2023-04-30 17:03:31 +0300

    Create testButtonAddDescription and method findCheckboxDropDownElements(), fixing localization

--- src/test/java/school/redrover/GroupUkrTest.java
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -70,22 +70,23 @@ public class GroupUkrTest extends BaseTest {
         Assert.assertEquals(checkboxGeneralSettings.size(), 5);
         checkboxGeneralSettings.get(0).click();
         List<WebElement> checkboxDropDown = findCheckboxDropDownElements();
-        Assert.assertTrue(checkboxDropDown.get(0).isDisplayed());
+        Assert.assertTrue(checkboxDropDown.get(0).isDisplayed() && checkboxDropDown.get(1).isDisplayed());
         actions.scrollToElement(checkboxGeneralSettings.get(1)).build().perform();
         checkboxGeneralSettings.get(1).click();
         checkboxDropDown = findCheckboxDropDownElements();
-        Assert.assertTrue(checkboxDropDown.get(3).isDisplayed() && checkboxDropDown.get(4).isDisplayed());
+        Assert.assertTrue(checkboxDropDown.get(2).isDisplayed() && checkboxDropDown.get(3).isDisplayed());
+        actions.scrollToElement(namesOfBlockTitle.get(1)).perform();
+        checkboxGeneralSettings.get(2).click();
+        checkboxDropDown = findCheckboxDropDownElements();
+        actions.scrollToElement(checkboxDropDown.get(3)).perform();
+        Assert.assertTrue(checkboxDropDown.get(3).isDisplayed());
+        actions.scrollToElement(namesOfBlockTitle.get(0)).perform();
         checkboxGeneralSettings.get(3).click();
         checkboxDropDown = findCheckboxDropDownElements();
-        actions.scrollToElement(checkboxDropDown.get(6)).perform();
-        Assert.assertTrue(checkboxDropDown.get(5).isDisplayed() && checkboxDropDown.get(6).isDisplayed());
-        actions.scrollToElement(namesOfBlockTitle.get(0)).perform();
-        checkboxGeneralSettings.get(4).click();
-        checkboxDropDown = findCheckboxDropDownElements();
-        Assert.assertTrue(checkboxDropDown.get(7).isDisplayed());
+        Assert.assertTrue(checkboxDropDown.get(5).isDisplayed() && checkboxDropDown.get(6).isDisplayed() && checkboxDropDown.get(7).isDisplayed());
 
 
-        Assert.assertEquals(namesOfBlockTitle.get(0).getText(), "Управление исходным кодом");
+        Assert.assertEquals(namesOfBlockTitle.get(0).getText(), "Source Code Management");
         actions.scrollToElement(namesOfBlockTitle.get(1)).perform();
 
         List<WebElement> useGitRadioButton = getDriver().findElements(By.xpath("//div[@class='radioBlock-container']//label"));
@@ -101,7 +102,7 @@ public class GroupUkrTest extends BaseTest {
         }
 
 
-        Assert.assertEquals(namesOfBlockTitle.get(1).getText(), "Триггеры сборки");
+        Assert.assertEquals(namesOfBlockTitle.get(1).getText(), "Build Triggers");
         actions.scrollToElement(namesOfBlockTitle.get(2)).perform();
         List<WebElement> checkboxTriggerBlock = getDriver().findElements(By.xpath("(//div[@class='jenkins-section'])[2]//label"));
         int indexOfDropDownBlock = 14;
@@ -123,9 +124,9 @@ public class GroupUkrTest extends BaseTest {
             indexOfDropDownBlock++;
         }
         actions.scrollToElement(namesOfBlockTitle.get(5)).perform();
-        Assert.assertEquals(namesOfBlockTitle.get(2).getText(), "Среда сборки");
-        Assert.assertEquals(namesOfBlockTitle.get(4).getText(), "Шаги сборки");
-        Assert.assertEquals(namesOfBlockTitle.get(5).getText(), "Послесборочные операции");
+        Assert.assertEquals(namesOfBlockTitle.get(2).getText(), "Build Environment");
+        Assert.assertEquals(namesOfBlockTitle.get(4).getText(), "Build Steps");
+        Assert.assertEquals(namesOfBlockTitle.get(5).getText(), "Post-build Actions");
         List<WebElement> checkboxEnvironmentBuild = getDriver().findElements(By.xpath("(//div[@class='jenkins-section'])[3]//label"));
         Assert.assertEquals(checkboxEnvironmentBuild.size(), 8);
 
@@ -148,13 +149,13 @@ public class GroupUkrTest extends BaseTest {
 
         List<WebElement> addStepsBuildDropDown = getDriver().findElements(By.xpath("//li[@groupindex='0']//a"));
         Assert.assertEquals(addStepsBuildDropDown.size(), 7);
-        Assert.assertEquals(addStepsBuildDropDown.get(0).getText(), "Invoke Gradle script");
-        Assert.assertEquals(addStepsBuildDropDown.get(1).getText(), "Run with timeout");
-        Assert.assertEquals(addStepsBuildDropDown.get(2).getText(), "Set build status to \"pending\" on GitHub commit");
-        Assert.assertEquals(addStepsBuildDropDown.get(3).getText(), "Вызвать Ant");
-        Assert.assertEquals(addStepsBuildDropDown.get(4).getText(), "Вызвать цели Maven верхнего уровня");
-        Assert.assertEquals(addStepsBuildDropDown.get(5).getText(), "Выполнить команду Windows");
-        Assert.assertEquals(addStepsBuildDropDown.get(6).getText(), "Выполнить команду shell");
+        Assert.assertEquals(addStepsBuildDropDown.get(0).getText(), "Execute Windows batch command");
+        Assert.assertEquals(addStepsBuildDropDown.get(1).getText(), "Execute shell");
+        Assert.assertEquals(addStepsBuildDropDown.get(2).getText(), "Invoke Ant");
+        Assert.assertEquals(addStepsBuildDropDown.get(3).getText(), "Invoke Gradle script");
+        Assert.assertEquals(addStepsBuildDropDown.get(4).getText(), "Invoke top-level Maven targets");
+        Assert.assertEquals(addStepsBuildDropDown.get(5).getText(), "Run with timeout");
+        Assert.assertEquals(addStepsBuildDropDown.get(6).getText(), "Set build status to \"pending\" on GitHub commit");
 
         actions.moveToElement(footer).click().perform();
         addStepAfterBuild.click();
@@ -163,14 +164,14 @@ public class GroupUkrTest extends BaseTest {
         Assert.assertEquals(addStepsAfterBuildDropDown.size(), 11);
         Assert.assertEquals(addStepsAfterBuildDropDown.get(0).getText(), "Aggregate downstream test results");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(1).getText(), "Publish JUnit test result report");
-        Assert.assertEquals(addStepsAfterBuildDropDown.get(2).getText(), "Заархивировать артефакты");
-        Assert.assertEquals(addStepsAfterBuildDropDown.get(3).getText(), "Собрать другой проект");
-        Assert.assertEquals(addStepsAfterBuildDropDown.get(4).getText(), "Сохранять отпечатки файлов (fingerprints) для отслеживания использования");
+        Assert.assertEquals(addStepsAfterBuildDropDown.get(2).getText(), "Archive the artifacts");
+        Assert.assertEquals(addStepsAfterBuildDropDown.get(3).getText(), "Build other projects");
+        Assert.assertEquals(addStepsAfterBuildDropDown.get(4).getText(), "Record fingerprints of files to track usage");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(5).getText(), "Git Publisher");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(6).getText(), "Editable Email Notification");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(7).getText(), "Set GitHub commit status (universal)");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(8).getText(), "Set build status on GitHub commit [deprecated]");
-        Assert.assertEquals(addStepsAfterBuildDropDown.get(9).getText(), "Уведомление по почте");
+        Assert.assertEquals(addStepsAfterBuildDropDown.get(9).getText(), "E-mail Notification");
         Assert.assertEquals(addStepsAfterBuildDropDown.get(10).getText(), "Delete workspace when build is done");
 
         WebElement submitButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
@@ -179,7 +180,7 @@ public class GroupUkrTest extends BaseTest {
         try {
             wait.until(ExpectedConditions.elementToBeClickable((By.xpath("(//div[@id='breadcrumbBar']//a[@class='model-link'])[1]")))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='MaxFirstTest']"))).click();
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Удалить Проект']"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Delete Project']"))).click();
             wait.until(ExpectedConditions.alertIsPresent());
 
         } catch (UnhandledAlertException e) {
