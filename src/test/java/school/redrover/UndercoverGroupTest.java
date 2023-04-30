265642e51aeaf018eac76650d3c7d59266365cb3
patchworkdude <legolowin@gmail.com>
2023-04-30 19:57:47 +0200

    delete unused imports

--- src/test/java/school/redrover/UndercoverGroupTest.java
+++ src/test/java/school/redrover/UndercoverGroupTest.java
@@ -1,16 +1,11 @@
 package school.redrover;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
 
 public class UndercoverGroupTest extends BaseTest {
 
