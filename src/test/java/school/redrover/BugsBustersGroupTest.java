f533b453040eb4119f2de51ab4254206757c8603
Zoryana Blashchuk <zblashchuk@gmail.com>
2023-05-02 03:20:24 +0300

    Update BugsBustersGroupTest.java

--- src/test/java/school/redrover/BugsBustersGroupTest.java
+++ src/test/java/school/redrover/BugsBustersGroupTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import org.testng.annotations.Ignore;
 
 import java.time.Duration;
 import java.util.Arrays;
