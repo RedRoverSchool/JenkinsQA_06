f3a5caf9795f1757894d95caf7973d9af937a063
Artyom Dulya <fecc1612@gmail.com>
2023-05-03 06:36:25 +0500

    update

--- src/test/java/school/redrover/CaramelSyrupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSyrupForJavaTest.java
@@ -320,18 +320,28 @@ public class CaramelSyrupForJavaTest extends BaseTest {
     }
 
     @Test
-    public void testNameUser() {
-        getDriver().findElement(By.cssSelector("[href='/logout']")).click();
-        WebElement login = getDriver().findElement(By.cssSelector("form[name='login'] input[name='j_username']"));
-        login.click();
-        login.sendKeys("ArtyomDulya");
-        WebElement password = getDriver().findElement(By.cssSelector("form[name='login'] input[name='j_password']"));
-        password.click();
-        password.sendKeys("3fadc643bea34ad8ae5b61d8cb56ee6e");
-        getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
-        getDriver().findElement(By.cssSelector("#tasks a[href='/asynchPeople/']")).click();
-        Assert.assertEquals(getDriver().findElement(By.xpath("//td[text()='ArtyomDulya']")).getText(), "ArtyomDulya");
+    public void testADLearnMoreHeaders() {
+        List<String> expectedResult = Arrays.asList("Enter an item name", "New node", "Configure Clouds", "Jenkins");
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='newJob']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//label[text()='Enter an item name']")).getText(), expectedResult.get(0));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='computer/new']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='New node']")).getText(), expectedResult.get(1));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']//a[@href='configureClouds']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='Configure Clouds']")).getText(), expectedResult.get(2));
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//span[text()='Learn more about distributed builds']")).click();
+        List<String> windows = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(windows.get(1));
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[text()='Jenkins']")).getText(), expectedResult.get(3));
     }
+
+
 }
 
 
