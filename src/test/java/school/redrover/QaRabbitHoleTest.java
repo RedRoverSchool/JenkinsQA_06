de7b973ddf0b69ae629c867d9c09e3b436a21432
Aleksandra Minakova <s.a.n.c.h.o.u.s@yandex.ru>
2023-05-04 21:14:56 +0300

    Add two tests

--- src/test/java/school/redrover/QaRabbitHoleTest.java
+++ src/test/java/school/redrover/QaRabbitHoleTest.java
@@ -20,4 +20,20 @@ public class QaRabbitHoleTest extends BaseTest {
 
         Assert.assertEquals(dashboard.getText(), "Dashboard");
     }
+
+    @Test
+    public void testLogout() {
+        WebElement exit = getDriver().findElement((By.xpath("//a[@href='/logout']")));
+        exit.click();
+
+        WebElement modalLogin = getDriver().findElement(By.xpath("//div[@class = 'simple-page']"));
+        Assert.assertTrue(modalLogin.isDisplayed());
+    }
+
+    @Test
+    public void testCreateJobTitle() {
+        WebElement createJob = getDriver().findElement(By.xpath("//a[@href = 'newJob']/span"));
+
+        Assert.assertEquals(createJob.getText(), "Create a job");
+    }
 }
