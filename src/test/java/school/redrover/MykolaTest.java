2328c1eb9d8e72b6182aeed057922f853dfb2a64
msnata <msnata@ukr.net>
2023-05-05 01:09:26 +0300

    testRenameCreatedFolder_Ignored

--- src/test/java/school/redrover/MykolaTest.java
+++ src/test/java/school/redrover/MykolaTest.java
@@ -60,7 +60,6 @@ public class MykolaTest extends BaseTest {
         Assert.assertTrue(folderPageName.isDisplayed() && folderPageName.getText().equals(folderName));
     }
 
-    @Ignore
     @Test
     public void testDeleteCreatedFolder() {
         By newItemButtonLocator = By.xpath("//*[@href='/view/all/newJob']");
@@ -73,6 +72,7 @@ public class MykolaTest extends BaseTest {
         Assert.assertTrue(getDriver().findElement(By.tagName("h1")).isDisplayed());
     }
 
+    @Ignore
     @Test
     public void testRenameCreatedFolder() {
         By newItemButtonLocator = By.xpath("//*[@href='/view/all/newJob']");
