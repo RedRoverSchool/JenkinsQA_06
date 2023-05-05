b6a2c76bc5951a7167af2cf1f506da83fc64d3fc
AnastasiaYY <anastasiayy@yahoo.com>
2023-05-04 22:04:15 -0400

    locator change

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -162,8 +162,8 @@ public class HeaderTest extends BaseTest {
         Actions actions = new Actions(getDriver());
         actions.moveToElement(dropdownMenuButton).click().build().perform();
 
-        getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#breadcrumb-menu > div.bd > ul > li")));
-        List<WebElement> dropDownMenus = getDriver().findElements(By.cssSelector("#breadcrumb-menu > div.bd > ul > li"));
+        getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".first-of-type > .yuimenuitem")));
+        List<WebElement> dropDownMenus = getDriver().findElements(By.cssSelector(".first-of-type > .yuimenuitem"));
 
         Assert.assertEquals(listText(dropDownMenus), expectedDropDownMenuNames);
     }
