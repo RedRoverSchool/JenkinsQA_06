182bee063a0da3f9828c6f192832c3cdf3def25b
MarinaAvr <marinakhudoiarova@gmail.com>
2023-05-07 11:55:27 -0500

    Test a method to take screenshots of failed tests

---
+++ src/test/java/school/redrover/ScreenshotTest.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import school.redrover.runner.BaseTest;
+
+public class ScreenshotTest extends BaseTest {
+
+    @Test
+    public void testScreenshot() {
+
+      WebElement newItemMenu = getDriver().findElement(By.xpath("//a[@href = '/view/all/builds']"));
+      newItemMenu.click();
+      WebElement iconLegendButton = getDriver().findElement(By.xpath("//a[@href = '/legend']"));
+      iconLegendButton.click();
+
+      WebElement textProjectHealth = getDriver().findElement(By.xpath("//div/h2[text()='Project Health']"));
+      List<WebElement> listProjectHealth = getDriver().findElements(By.xpath("//div/dl[@class='app-icon-legend'][2]/dd"));
+
+      Assert.assertEquals(textProjectHealth.getText(), "Project Health");
+      Assert.assertEquals(listProjectHealth.size(), 6);
+    }
+}
