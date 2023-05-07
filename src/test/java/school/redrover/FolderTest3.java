3f43040894745cdb57beab8db1ee8cabfc2432a4
NadiaSergeeva <nadechka.ten@gmail.com>
2023-05-07 12:38:07 +0100

    RF 04.01| New item > Folder > Create Folder without configuration

--- src/test/java/school/redrover/FolderTest3.java
+++ src/test/java/school/redrover/FolderTest3.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import java.util.Arrays;
+import java.util.List;
 
 public class FolderTest3 extends BaseTest {
-    public void createBaseFolder () {
+    public void createBaseFolder (String name) {
+        getDriver().findElement(By.id("jenkins-home-link"));
+        getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"))));
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
         WebElement nameinput=getDriver().findElement(By.name("name"));
         getWait2().until(ExpectedConditions.visibilityOf(nameinput));
-        nameinput.sendKeys("folder1");
+        nameinput.sendKeys(name);
 
         getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']")).click();
         WebElement okbutton = getDriver().findElement(By.id("ok-button"));
@@ -21,11 +25,15 @@ public class FolderTest3 extends BaseTest {
 
         getWait2().until(ExpectedConditions.textToBe(By.xpath("//h1[text()='Configuration']"), "Configuration"));
     }
-    @Test
-    public void testCreateFolder (){
-        createBaseFolder();
 
-        getDriver().findElement(By.xpath("//a[normalize-space()='folder1']")).click();
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='folder1']")).getText(),"folder1");
-    }
+        @Test
+        public void testCreateFolder (){
+            List<String> positivevalues = Arrays.asList("folder1");
+             for (String value : positivevalues) {
+                createBaseFolder(value);
+                getDriver().findElement(By.xpath("//a[normalize-space()='"+value+"']")).click();
+
+                Assert.assertEquals(getDriver().findElement(By.xpath("//a[normalize-space()='"+value+"']")).getText(),value);
+            }
+        }
 }
