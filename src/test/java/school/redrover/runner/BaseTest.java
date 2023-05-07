2f45be0b90dec454fd2a9d26b0bd46cad80537b3
Helen Muronyuk <olena6623@gmail.com>
2023-05-08 00:39:32 +0200

    Refactor: remove excess import

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -1,16 +1,11 @@
 package school.redrover.runner;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.time.Duration;
 
