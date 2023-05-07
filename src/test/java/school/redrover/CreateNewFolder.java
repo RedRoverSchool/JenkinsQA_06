2eb5047735e015053940b47ae75e228a8ba15241
anton <qvotan@gmail.com>
2023-05-07 14:18:14 -0700

    AT 04.01 | Create New Folder TC Added empty line between class name and imports. Removed variables and put actions on th same line. Removed variable with getText() and added it to assert.

--- src/test/java/school/redrover/CreateNewFolder.java
+++ src/test/java/school/redrover/CreateNewFolder.java
@@ -7,32 +7,23 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import static org.testng.Assert.assertEquals;
+
 public class CreateNewFolder extends BaseTest{
 
     @Test
     public void createNewFolder(){
-
         String name = "Grouping";
-        WebElement newItemLink = getDriver().findElement(By.xpath("//div[@id='tasks']/div[1]/span/a"));
-        newItemLink.click();
 
-        WebElement enterItemName = getDriver().findElement(By.xpath("//input[@name='name']"));
-        enterItemName.sendKeys(name);
+        getDriver().findElement(By.xpath("//div[@id='tasks']/div[1]/span/a")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(name);
 
-        WebElement createFolderButton = getDriver().findElement(By.xpath("//div[@id='j-add-item-type-nested-projects']/ul/li[1]/label/span"));
-        createFolderButton.click();
+        getDriver().findElement(By.xpath("//div[@id='j-add-item-type-nested-projects']/ul/li[1]/label/span")).click();
+        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
 
-        WebElement clickOkButton = getDriver().findElement(By.xpath("//*[@id='ok-button']"));
-        clickOkButton.click();
-
-        WebElement clickSaveButton = getDriver().findElement(By.xpath("//*[@name='Submit']"));
-        clickSaveButton.click();
-
-        WebElement clickDashboardButton = getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol/li[1]/a"));
-        clickDashboardButton.click();
+        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
+        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol/li[1]/a")).click();
 
         WebElement checkFolderName = getDriver().findElement(By.xpath("//table[@id='projectstatus']/tbody/tr/td[3]/a/span"));
-        checkFolderName.getText();
 
         Assert.assertEquals(checkFolderName.getText(), name);
     }
