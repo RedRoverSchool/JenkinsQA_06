a2ddf445095ef72cb99ea7ca240e568e28b16f51
Vidim83 <08091985n@gmail.com>
2023-05-07 20:19:42 +0300

    AT 15.05 | Users > User ID > Delete user

--- src/test/java/school/redrover/DeleteUserTest.java
+++ src/test/java/school/redrover/DeleteUserTest.java
@@ -35,6 +35,7 @@ public class DeleteUserTest extends BaseTest {
 
     @Test
     public void testDeleteUser() {
+
         createUser();
 
         List<WebElement> listOfUsers = getDriver().findElements(By.xpath("//*[@class='jenkins-table__link model-link inside']"));
