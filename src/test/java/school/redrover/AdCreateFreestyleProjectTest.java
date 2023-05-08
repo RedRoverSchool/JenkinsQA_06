b427dd8cdfcc23df7017d0d35fcf04cf671eb86d
Artyom Dulya <fecc1612@gmail.com>
2023-05-09 00:35:48 +0500

    update

--- src/test/java/school/redrover/AdCreateFreestyleProjectTest.java
+++ src/test/java/school/redrover/AdCreateFreestyleProjectTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class AdCreateFreestyleProject extends BaseTest {
+public class AdCreateFreestyleProjectTest extends BaseTest {
 
     @Test
     public void createFreestyleProject() {
