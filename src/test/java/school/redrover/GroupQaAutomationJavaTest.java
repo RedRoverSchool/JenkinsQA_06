22ea6890b9c68e9673b1d39116be0887b1234dca
Karina Volodavchik <karinkaa.skor@gmail.com>
2023-05-08 20:59:16 +0200

    moved the test into correct class

--- src/test/java/school/redrover/GroupQaAutomationJavaTest.java
+++ src/test/java/school/redrover/GroupQaAutomationJavaTest.java
@@ -6,6 +6,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GroupQaAutomationJavaTest extends BaseTest {
     @Test
     public void testFirst () throws InterruptedException  {
@@ -28,4 +32,35 @@ public class GroupQaAutomationJavaTest extends BaseTest {
 
         Assert.assertEquals("Received!", value);
     }
+  /*  @Test
+    public void testCheckMenuAfterPushButtonPeople () {
+        getDriver().findElement(By.linkText("People")).click();
+
+        WebElement one = getDriver().findElement(By.xpath("//h1"));
+        Assert.assertEquals(one.getText(),"People");
+
+        List<String> expectedMenu = Arrays.asList("User ID", "Name", "Last Commit Activity", "On");
+        /*List < String > dataInTable = new ArrayList<>();
+        WebElement userId = getDriver().findElement(By.linkText("User ID"));
+        WebElement name = getDriver().findElement(By.xpath("//*[@id=\"people\"]/thead/tr/th[3]/a"));
+        WebElement lastCommitActivity = getDriver().findElement(By.xpath("//*[@id=\"people\"]/thead/tr/th[4]/a"));
+        WebElement on = getDriver().findElement(By.xpath( "//*[@id=\"people\"]/thead/tr/th[5]/a"));
+        dataInTable.add(userId.getText());
+        dataInTable.add(name.getText());
+        dataInTable.add(lastCommitActivity.getText());
+        dataInTable.add(on.getText());*/
+/*
+        List<WebElement> titles =  getDriver().findElements(By.xpath("//a[@class = 'sortheader']")); // локатор на все элементы в таблице
+        List<String> actualMenu = new ArrayList<>();
+
+        for (int i = 0; i < titles.size(); i++) {
+            if (titles.get(i).getText().contains("↑")) {
+                actualMenu.add(titles.get(i).getText().replace("↑", "").trim());
+            } else {
+                actualMenu.add(titles.get(i).getText());
+            }
+        }
+        Assert.assertEquals(actualMenu, expectedMenu);
+    }
+} */
 }
