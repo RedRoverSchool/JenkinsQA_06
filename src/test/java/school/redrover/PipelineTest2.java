2a0ccc390b356a5cbe6fa4885bfa7d1136d34a80
ReginaKurtLV <rgaynul@gmail.com>
2023-05-07 18:49:41 -0700

    AT 02.04 | Pipeline > Configure

--- src/test/java/school/redrover/PipelineTest2.java
+++ src/test/java/school/redrover/PipelineTest2.java
@@ -14,13 +14,18 @@ public class PipelineTest2 extends BaseTest {
     private static final By DESCRIPTION = By.name("description");
     private static final By SUBMIT = By.name("Submit");
     private static final By TEXT_PIPELINE = By.cssSelector(".job-index-headline.page-headline");
-    private static final By RENAME = By.xpath("//a[@href='/job/FirstPipelin/confirm-rename']");
+    private static final By RENAME = By.xpath("//a[@href='/job/FirstPipeline/confirm-rename']");
     private static final By RENAME_LINE = By.xpath("//input[@name='newName']");
-    String name = "FirstPipelin";
+    private static final By CONFIGURE = By.xpath("//a[@href='/job/FirstPipeline/configure']");
+    private static final By ENABLE_DISABLE_BUTTON = By.cssSelector("label[for='enable-disable-project']");
+    private static final By DISCARD_OLD_BUILD = By.xpath("//label[normalize-space()='Discard old builds']");
+    private static final By DAYS_TO_KEEP_BUILDS = By.name("_.daysToKeepStr");
+    private static final By MAX_DAYS = By.xpath("//input[@name='_.numToKeepStr']");
+    String name = "FirstPipeline";
     String descriptionText = "Pipeline text";
     String renameText = "Pipeline1";
-    @Test
-    public void testCreatePipelineAndRename () {
+
+    public void createPipeline() {
 
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(NAME).sendKeys(name);
@@ -28,11 +33,64 @@ public class PipelineTest2 extends BaseTest {
         getDriver().findElement(BUTTON).click();
         getDriver().findElement(DESCRIPTION).sendKeys(descriptionText);
         getDriver().findElement(SUBMIT).click();
+    }
+
+    @Test
+    public void testCreatePipeline() {
+
+        getDriver().findElement(NEW_ITEM).click();
+        getDriver().findElement(NAME).sendKeys(name);
+        getDriver().findElement(PIPELINE).click();
+        getDriver().findElement(BUTTON).click();
+        getDriver().findElement(DESCRIPTION).sendKeys(descriptionText);
+        getDriver().findElement(SUBMIT).click();
+
+        Assert.assertEquals("Pipeline " + name, getDriver().findElement(TEXT_PIPELINE).getText());
+    }
+
+    @Test
+    public void testRenamePipeline() {
+        createPipeline();
+
         getDriver().findElement(RENAME).click();
         getDriver().findElement(RENAME_LINE).clear();
         getDriver().findElement(RENAME_LINE).sendKeys(renameText);
         getDriver().findElement(SUBMIT).click();
 
         Assert.assertEquals("Pipeline " + renameText, getDriver().findElement(TEXT_PIPELINE).getText());
+    }
+
+    @Test
+    public void testSetDescriptionEnableDisablePipeline() {
+        String descriptionText = "Good job!";
+        String enableButton = "Enable";
+        String text = "Disable Project";
+
+        createPipeline();
+
+        getDriver().findElement(CONFIGURE).click();
+        getDriver().findElement(DESCRIPTION).sendKeys(descriptionText);
+        getDriver().findElement(ENABLE_DISABLE_BUTTON).click();
+        getDriver().findElement(SUBMIT).click();
+        Assert.assertEquals(enableButton, getDriver().findElement(SUBMIT).getText());
+
+        getDriver().findElement(SUBMIT).click();
+        Assert.assertEquals(text, getDriver().findElement(SUBMIT).getText());
+    }
+
+    @Test
+    public void testDiscardOldBuildsPipeline() {
+        String daysMin = "2";
+        String daysMax = "30";
+
+        createPipeline();
+
+        getDriver().findElement(CONFIGURE).click();
+        getDriver().findElement(DISCARD_OLD_BUILD).click();
+        getDriver().findElement(DAYS_TO_KEEP_BUILDS).sendKeys(daysMin);
+        getDriver().findElement(MAX_DAYS).sendKeys(daysMax);
+        getDriver().findElement(SUBMIT).click();
+
+        Assert.assertEquals("Pipeline " + name, getDriver().findElement(TEXT_PIPELINE).getText());
     }
 }
