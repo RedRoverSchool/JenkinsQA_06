cbf9d61dd08c4baa6101590ccbffdae92be3a3e4
Artyom Dulya <fecc1612@gmail.com>
2023-05-09 00:29:29 +0500

    TC 00.01 | New Item > Freestyle Project > Create Freestyle Project

--- src/test/java/school/redrover/AdCreateFreestyleProject.java
+++ src/test/java/school/redrover/AdCreateFreestyleProject.java
@@ -1,2 +1,25 @@
-package school.redrover;public class ADcreateFreestyleProject {
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class AdCreateFreestyleProject extends BaseTest {
+
+    @Test
+    public void createFreestyleProject() {
+        String expectedResult = "Engineer2";
+
+        getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Engineer2");
+        getDriver().findElement(By.cssSelector("[value='hudson.model.FreeStyleProject'] + span")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.cssSelector("#breadcrumbs > li ")).click();
+
+        String actualResult = getDriver().findElement(By.cssSelector("[href$='Engineer2/']")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
