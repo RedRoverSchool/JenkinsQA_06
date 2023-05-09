a62e7596e8178ff6814faddaccbafff672d4d096
Artyom Dulya <fecc1612@gmail.com>
2023-05-09 10:38:35 +0500

    update

--- src/test/java/school/redrover/CreateFreestyleProjectADTest.java
+++ src/test/java/school/redrover/CreateFreestyleProjectADTest.java
@@ -19,7 +19,6 @@ public class CreateFreestyleProjectADTest extends BaseTest {
         getDriver().findElement(By.cssSelector("#breadcrumbs > li ")).click();
 
         String actualResult = getDriver().findElement(By.cssSelector("[href$='Engineer2/']")).getText();
-
         Assert.assertEquals(actualResult, expectedResult);
     }
 }
