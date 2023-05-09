b1bea3fe7b3972fb69cc87e00aa1e8eb97b50a10
Kk-788 <kyrylo788@gmail.com>
2023-05-08 18:40:44 -0700

    Change selectors CSS -> XPath Add implicit wait

--- src/test/java/school/redrover/KirillKTest.java
+++ src/test/java/school/redrover/KirillKTest.java
@@ -8,30 +8,32 @@ import school.redrover.runner.BaseTest;
 
 public class KirillKTest extends BaseTest {
 
-    public static final By NEW_JOB_LINK = By.cssSelector("a[href='newJob']");
-    public static final By ITEM_NAME_INPUT = By.cssSelector("div[class='add-item-name'] > input:nth-of-type(1)");
-    public static final By SELECT_ITEM_TYPE_FOLDER = By.cssSelector("li.com_cloudbees_hudson_plugins_folder_Folder");
-    public static final By OK_BUTTON = By.cssSelector("div.footer button#ok-button");
-    public static final By FINAL_SAVE_BUTTON = By.cssSelector("div#bottom-sticker button[name='Submit']");
+    public static final By NEW_JOB_LINK = By.xpath("// a[@href='newJob']");
+    public static final By ITEM_NAME_INPUT = By.xpath("// div[@class='add-item-name'] / input[1]");
+    public static final By SELECT_ITEM_TYPE_FOLDER = By.xpath("// li[@class='com_cloudbees_hudson_plugins_folder_Folder']");
+    public static final By OK_BUTTON = By.xpath("// div[@class='footer'] // button[@id='ok-button']");
+    public static final By FINAL_SAVE_BUTTON = By.xpath("// div[@id='bottom-sticker'] // button[@name='Submit']");
 
     @Test
-    public void testFolderCreation() {
+    public void testFolderCreation(){
+
+        getWait5();
+
         WebElement newJobLink = getDriver().findElement(NEW_JOB_LINK);
-            newJobLink.click();
+        newJobLink.click();
+
         WebElement itemNameInput = getDriver().findElement(ITEM_NAME_INPUT);
-            try {
-                Assert.assertTrue(itemNameInput.isEnabled());
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            itemNameInput.click();
-            itemNameInput.sendKeys("auto-test");
+        itemNameInput.click();
+        itemNameInput.sendKeys("auto-test");
+
         WebElement selectItemTypeFolder = getDriver().findElement(SELECT_ITEM_TYPE_FOLDER);
-            selectItemTypeFolder.click();
+        selectItemTypeFolder.click();
+
         WebElement okButton = getDriver().findElement(OK_BUTTON);
-            Assert.assertEquals(okButton.getText(), "OK");
-            okButton.click();
+        Assert.assertEquals(okButton.getText(), "OK");
+        okButton.click();
+
         WebElement finalSaveButton = getDriver().findElement(FINAL_SAVE_BUTTON);
-            finalSaveButton.click();
+        finalSaveButton.click();
     }
 }
