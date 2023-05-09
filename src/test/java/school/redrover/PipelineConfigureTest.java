cc4e92f6e5b7bbf1e350bea17c56b05d2dbb1360
Olesya Kolenchenko <kololesya@gmail.com>
2023-05-09 13:09:52 +0200

    add addDescriptionPipelineProjectTest()

--- src/test/java/school/redrover/PipelineConfigureTest.java
+++ src/test/java/school/redrover/PipelineConfigureTest.java
@@ -7,7 +7,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.List;
+
 public class PipelineConfigureTest extends BaseTest {
+    final String expectedResult = "New pipeline project";
 
     public void createPipeline() {
         WebElement createJobButton = getDriver().findElement(By.xpath("//a[@href = 'newJob']"));
@@ -23,6 +26,29 @@ public class PipelineConfigureTest extends BaseTest {
         WebElement okButton = getDriver().findElement(By.id("ok-button"));
         okButton.click();
     }
+
+    public WebElement findElement(By by){
+        return getDriver().findElement(by);
+    }
+
+    public void clickLinkButton(String linkButton){
+        findElement(By.xpath(String.format("//*[text()='%s']", linkButton))).click();
+    }
+
+    public void createPipelineProject(String nameOfProject, String typeOfProject){
+        findElement(By.className("task-icon-link")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("name"))).sendKeys(nameOfProject);
+
+        List<WebElement> listItemOptions = getDriver().findElements(By.id("j-add-item-type-standalone-projects"));
+        for(WebElement element:listItemOptions){
+            if (element.getText().contains(typeOfProject)){
+                element.click();
+            }
+        }
+        findElement(By.id("ok-button")).click();
+    }
+
 
     @Test
     public void testSetDescription() {
@@ -62,135 +88,21 @@ public class PipelineConfigureTest extends BaseTest {
     }
 
     @Test
-    public void testDiscardOldBuildsIsChecked7DaysAnd5Builds() {
-        final String days = "7";
-        final String builds = "5";
+    public void addDescriptionPipelineProjectTest(){
+        String description = "This is a project for school test";
+        createPipelineProject(expectedResult, "Pipeline");
+        findElement(By.name("description")).sendKeys(description);
+        findElement(By.name("Apply")).click();
+        WebElement messageSaved = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("notification-bar")));
 
-        createPipeline();
+        Assert.assertTrue(messageSaved.isDisplayed());
 
-        WebElement discardOldBuildsLabel = getDriver()
-                .findElement(By.xpath("//label[contains(text(),'Discard old builds')]"));
-        discardOldBuildsLabel.click();
+        clickLinkButton("Dashboard");
+        clickLinkButton(expectedResult);
 
-        WebElement daysToKeepField = getDriver().findElement(By.name("_.daysToKeepStr"));
-        daysToKeepField.sendKeys(days);
+        WebElement fieldDescription = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("description")));
 
-        WebElement buildsToKeepField = getDriver().findElement(By.name("_.numToKeepStr"));
-        buildsToKeepField.sendKeys(builds);
-
-        WebElement saveButton = getDriver().findElement(By.name("Submit"));
-        saveButton.click();
-
-        WebElement configureMenu = getDriver()
-                .findElement(By.xpath("//*[@href='/job/test-pipeline/configure']"));
-        configureMenu.click();
-
-        WebElement discardOldBuildsCheckbox = getDriver().findElement(By.id("cb2"));
-        WebElement daysToKeep = getDriver().findElement(By.name("_.daysToKeepStr"));
-        WebElement buildsToKeep = getDriver().findElement(By.name("_.numToKeepStr"));
-
-        Assert.assertTrue(discardOldBuildsCheckbox.isSelected());
-        Assert.assertEquals(daysToKeep.getAttribute("value"), days);
-        Assert.assertEquals(buildsToKeep.getAttribute("value"), builds);
-    }
-
-    @Test
-    public void testDiscardOldBuildsIsChecked0Days() {
-        final String days = "0";
-        final String errorMessage = "Not a positive integer";
-
-        createPipeline();
-
-        WebElement discardOldBuildsLabel = getDriver()
-                .findElement(By.xpath("//label[contains(text(),'Discard old builds')]"));
-        discardOldBuildsLabel.click();
-
-        WebElement daysToKeepField = getDriver().findElement(By.name("_.daysToKeepStr"));
-        daysToKeepField.sendKeys(days);
-
-        WebElement discardOldBuildsCheckbox = getDriver().findElement(By.id("cb2"));
-
-        WebElement clickOutsideOfInputField = getDriver()
-                .findElement(By.xpath("//*[@name='strategy']/div/div"));
-        clickOutsideOfInputField.click();
-
-        WebElement actualErrorMessage = getWait10().until(ExpectedConditions
-                .visibilityOfElementLocated(By.xpath("//*[@name='strategy']//div[@class='error']")));
-
-        Assert.assertTrue(discardOldBuildsCheckbox.isSelected());
-        Assert.assertEquals(actualErrorMessage.getText(), errorMessage);
-    }
-
-    @Test
-    public void testDiscardOldBuildsIsChecked0Builds() {
-        final String builds = "0";
-        final String errorMessage = "Not a positive integer";
-
-        createPipeline();
-
-        WebElement discardOldBuildsLabel = getDriver()
-                .findElement(By.xpath("//label[contains(text(),'Discard old builds')]"));
-        discardOldBuildsLabel.click();
-
-        WebElement buildsToKeepField = getDriver().findElement(By.name("_.numToKeepStr"));
-        buildsToKeepField.sendKeys(builds);
-
-        WebElement discardOldBuildsCheckbox = getDriver().findElement(By.id("cb2"));
-
-        WebElement clickOutsideOfInputField = getDriver()
-                .findElement(By.xpath("//*[@name='strategy']/div/div"));
-        clickOutsideOfInputField.click();
-
-        WebElement actualErrorMessage = getWait5().until(ExpectedConditions
-                        .visibilityOfElementLocated(By.xpath("//*[@name='strategy']//div[@class='error']")));
-
-        Assert.assertTrue(discardOldBuildsCheckbox.isSelected());
-        Assert.assertEquals(actualErrorMessage.getText(), errorMessage);
-    }
-
-    @Test
-    public void testPipelineCreation() {
-        final String PIPELINE_NAME = "My_pipeline";
-
-        WebElement newItem = getDriver().findElement(By.xpath(
-                "//a[@href='/view/all/newJob']"));
-        newItem.click();
-
-        getWait2().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.id("name"))));
-
-        WebElement pipelineType = getDriver().findElement(By.cssSelector(".org_jenkinsci_plugins_workflow_job_WorkflowJob"));
-        pipelineType.click();
-
-        WebElement nameField = getDriver().findElement(By.id("name"));
-        nameField.sendKeys(PIPELINE_NAME);
-
-        WebElement okButton = getDriver().findElement(By.id("ok-button"));
-        okButton.click();
-
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.name("Submit")));
-
-        WebElement enableToggles = getDriver().findElement(By.id("toggle-switch-enable-disable-project"));
-        boolean isPipelineEnabled = Boolean.parseBoolean(getDriver().findElement(By.xpath("//input[@name='enable']"))
-                .getAttribute("value"));
-        if (isPipelineEnabled){
-            enableToggles.click();
-        }
-
-        getDriver().findElement(By.name("Submit")).click();
-
-        getWait2().until(ExpectedConditions.textToBe(By.tagName("h1"), "Pipeline " + PIPELINE_NAME));
-
-        String disabledWarning = getDriver().findElement(By.id("enable-project")).getText();
-
-        WebElement configurePipeline= getDriver().findElement(By.linkText("Configure"));
-        configurePipeline.click();
-
-        getWait5().until(ExpectedConditions.textToBe(By.tagName("h2"), "General"));
-
-        boolean isPipelineEnabledAfterDisable = Boolean.parseBoolean(getDriver().findElement(
-                By.xpath("//input[@name='enable']")).getAttribute("value"));
-
-        Assert.assertTrue(disabledWarning.contains("This project is currently disabled"));
-        Assert.assertFalse(isPipelineEnabledAfterDisable,"false");
+        Assert.assertTrue(fieldDescription.getText().contains(description));
     }
 }
