c22bbae16223745e8ab4d8db92ecf549b7cd7c88
aharu <aharutyunyan2011@gmail.com>
2023-05-09 01:17:27 -0700

    update user profil test

---
+++ src/test/java/school/redrover/CustomizeUserSettingsTest.java
@@ -0,0 +1,46 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class CustomizeUserSettingsTest extends BaseTest  {
+
+    @Test
+    public void testCredentialsEddElementID() throws InterruptedException {
+        String testUser = "Test User";
+        String newUser = "New User";
+        String password = "password";
+        String email = "testuser@test.com";
+
+        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
+        getDriver().findElement(By.cssSelector("[href='securityRealm/']")).click();
+        getDriver().findElement(By.cssSelector("[href='addUser']")).click();
+
+        getDriver().findElement(By.cssSelector("#username")).sendKeys(testUser);
+        getDriver().findElement(By.cssSelector("[name='password1']")).sendKeys(password);
+        getDriver().findElement(By.cssSelector("[name='password2']")).sendKeys(password);
+        getDriver().findElement(By.cssSelector("[name='fullname']")).sendKeys(testUser);
+        getDriver().findElement(By.cssSelector("[name='email']")).sendKeys(email);
+        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
+
+        getDriver().findElement(By.cssSelector("[href='/']")).click();
+        getDriver().findElement(By.cssSelector("[href='/asynchPeople/']")).click();
+        String oldactualresult = getDriver().findElement(By.cssSelector("#person-TestUser td:nth-child(3)")).getText();
+        Assert.assertEquals(oldactualresult, testUser);
+
+        getDriver().findElement(By.cssSelector("[href='/user/testuser/']")).click();
+        getDriver().findElement(By.cssSelector("[href='/user/testuser/configure']")).click();
+
+        WebElement newUserName = getDriver().findElement(By.cssSelector("[name='_.fullName']"));
+        newUserName.clear();
+        newUserName.sendKeys(newUser);
+        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
+        getDriver().findElement(By.cssSelector("[href='/user/testuser/']")).click();
+
+        String newactualresult = getDriver().findElement(By.cssSelector("#person-TestUser td:nth-child(3)")).getText();
+        Assert.assertEquals(newactualresult, newUser);
+    }
+}
