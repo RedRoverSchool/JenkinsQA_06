4e40b0d0b9c1466eb090291b66b162fc711e9309
KiraKomissarova <komissarova.keira@gmail.com>
2023-05-09 11:52:03 -0400

    AT 00.01 | New Item > Freestyle Project > Create Freestyle Project

---
+++ src/test/java/school/redrover/CreateUsers_1_Test.java
@@ -0,0 +1,12 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+
+public class CreateUsers_1_Test {
+    private static final By MANAGE_JENKINS_XPATH = By.xpath("//a[@href='/manage']");
+    private static final By MANAGE_USERS_XPATH = By.xpath("//dt[contains(text(),'Manage Users')]");
+    private static final By CREATE_USERS_XPATH = By.xpath("//a[@href='addUser']");
+    private static final By USERNAME_BAR_XPATH = By.xpath("//input[@name='username']");
+    private static final By PASSWORD_BAR_XPATH = By.xpath("//input[@name='password1']");
+    private static final By
+}
