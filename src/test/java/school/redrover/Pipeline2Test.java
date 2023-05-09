419793ed11c0002b51b6f442bcffcbf409549db9
anastasiiabazh <bazhenova.a.s@gmail.com>
2023-05-09 21:54:43 +0100

    AT 00.02| New Item > Create Pipeline Project with correct name

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -44,7 +44,7 @@ public class Pipeline2Test extends BaseTest {
     }
 
     @Test
-    public void TestCreatePipelineProjectCorrectName() {
+    public void testCreatePipelineProjectCorrectName() {
         WebElement newItem = getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']"));
         newItem.click();
 
