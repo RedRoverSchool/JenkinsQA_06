fafe1938bdbec0e5a562135567161f3beeb8eb84
aharu <aharutyunyan2011@gmail.com>
2023-05-10 00:18:44 -0700

    update update user profil test

--- src/test/java/school/redrover/CustomizeUserSettingsTest.java
+++ src/test/java/school/redrover/CustomizeUserSettingsTest.java
@@ -9,8 +9,9 @@ import school.redrover.runner.BaseTest;
 public class CustomizeUserSettingsTest extends BaseTest  {
 
     @Test
-    public void testCredentialsEddElementID() throws InterruptedException {
-        String testUser = "Test User";
+    public void testUpdateUserName() throws InterruptedException {
+        String testUser = "TestUser";
+        String testUserName = "Test User";
         String newUser = "New User";
         String password = "password";
         String email = "testuser@test.com";
@@ -22,14 +23,14 @@ public class CustomizeUserSettingsTest extends BaseTest  {
         getDriver().findElement(By.cssSelector("#username")).sendKeys(testUser);
         getDriver().findElement(By.cssSelector("[name='password1']")).sendKeys(password);
         getDriver().findElement(By.cssSelector("[name='password2']")).sendKeys(password);
-        getDriver().findElement(By.cssSelector("[name='fullname']")).sendKeys(testUser);
+        getDriver().findElement(By.cssSelector("[name='fullname']")).sendKeys(testUserName);
         getDriver().findElement(By.cssSelector("[name='email']")).sendKeys(email);
         getDriver().findElement(By.cssSelector("[name='Submit']")).click();
 
         getDriver().findElement(By.cssSelector("[href='/']")).click();
         getDriver().findElement(By.cssSelector("[href='/asynchPeople/']")).click();
-        String oldactualresult = getDriver().findElement(By.cssSelector("#person-TestUser td:nth-child(3)")).getText();
-        Assert.assertEquals(oldactualresult, testUser);
+        String oldactualresult = getDriver().findElement(By.cssSelector("#people>tbody>tr:nth-child(2)>td:nth-child(3)")).getText();
+        Assert.assertEquals(oldactualresult, testUserName);
 
         getDriver().findElement(By.cssSelector("[href='/user/testuser/']")).click();
         getDriver().findElement(By.cssSelector("[href='/user/testuser/configure']")).click();
@@ -37,10 +38,11 @@ public class CustomizeUserSettingsTest extends BaseTest  {
         WebElement newUserName = getDriver().findElement(By.cssSelector("[name='_.fullName']"));
         newUserName.clear();
         newUserName.sendKeys(newUser);
-        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
-        getDriver().findElement(By.cssSelector("[href='/user/testuser/']")).click();
 
-        String newactualresult = getDriver().findElement(By.cssSelector("#person-TestUser td:nth-child(3)")).getText();
+        getDriver().findElement(By.cssSelector("[name='Submit']")).click();
+        getDriver().findElement(By.cssSelector("[href='/asynchPeople/']")).click();
+
+        String newactualresult = getDriver().findElement(By.cssSelector("#people>tbody>tr:nth-child(2)>td:nth-child(3)")).getText();
         Assert.assertEquals(newactualresult, newUser);
     }
 }
