fae18905504952cd32fa270093bd3537a77d3aa0
Yuliya48 <kolyabina8@gmail.com>
2023-05-10 17:30:57 -0400

    PageHeaderAdminDropdownMenuTest add

---
+++ src/test/java/school/redrover/PageHeaderAdminDropdownMenuTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class PageHeaderAdminDropdownMenuTest extends BaseTest {
+
+    @Test
+    public void testBuildsOpenFromDropdownMenu(){
+
+        Actions act = new Actions(getDriver());
+
+        WebElement dropDownMenu = getWait2().until(ExpectedConditions.elementToBeClickable
+                (By.xpath("//a[@href='/user/admin']//button[@class='jenkins-menu-dropdown-chevron']")));
+
+        act.click(dropDownMenu).perform();
+
+        WebElement btnBuilds = getDriver().findElement(By.xpath
+                ("//div[@id='breadcrumb-menu-target']//span[.='Builds']"));
+        btnBuilds.click();
+
+        WebElement pageBuilds = getDriver().findElement(By.xpath("//h1[.='Builds for admin']"));
+
+        Assert.assertTrue(pageBuilds.isDisplayed());
+    }
+}
+
