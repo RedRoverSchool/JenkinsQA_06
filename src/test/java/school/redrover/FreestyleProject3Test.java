e4622aacdcb5d244b9adf7ee699a6d991d21740e
Artyom Dulya <fecc1612@gmail.com>
2023-05-11 11:28:05 +0500

    AT 01.07 | Freestyle project > Delete project from the dashboard list

---
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -0,0 +1,44 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
+
+public class FreestyleProject3Test extends BaseTest {
+
+    public void createFreestyleProject() {
+        getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Engineer2");
+        getDriver().findElement(By.cssSelector("[value='hudson.model.FreeStyleProject'] + span")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.cssSelector("#breadcrumbs > li ")).click();
+    }
+
+    @Test
+    public void testDeleteProjectFromTheDashboardList() {
+        String expectedResult = "Start building your software project";
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        createFreestyleProject();
+        Actions actions = new Actions(getDriver());
+
+        WebElement projectButton = getDriver().findElement(By.xpath("//tr[@class=' job-status-nobuilt']//td[3]/a"));
+        actions.moveToElement(projectButton).perform();
+        WebElement dropdown = getDriver().findElement(By.xpath("//tr[@class=' job-status-nobuilt']//td[3]/a/button"));
+        actions.moveToElement(dropdown).perform();
+        actions.click(dropdown).perform();
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        WebElement deleteProject = getDriver().findElement(By.cssSelector("[class='icon-edit-delete icon-md']"));
+        actions.click(deleteProject).perform();
+        getDriver().switchTo().alert().accept();
+
+        String actualResult = getDriver().findElement(By.xpath("//h2")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
