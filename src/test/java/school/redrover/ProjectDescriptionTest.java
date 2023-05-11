913e6ecf065749a8d89178e4820c24859968489d
Artyom Dulya <fecc1612@gmail.com>
2023-05-11 07:40:55 +0500

    update

--- src/test/java/school/redrover/ProjectDescriptionTest.java
+++ src/test/java/school/redrover/ProjectDescriptionTest.java
@@ -12,17 +12,20 @@ import java.time.Duration;
 
 public class ProjectDescriptionTest extends BaseTest {
 
-    @Test
-    public void testProjectDescription() throws InterruptedException {
-        String expectedResult = "Test";
-
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    public void createFreestyleProject() {
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Engineer2");
         getDriver().findElement(By.cssSelector("[value='hudson.model.FreeStyleProject'] + span")).click();
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.cssSelector("#breadcrumbs > li ")).click();
+    }
+
+    @Test
+    public void testProjectDescription() {
+        String expectedResult = "Test";
+        createFreestyleProject();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         WebElement description = getDriver().findElement(By.xpath("//a[@id='description-link']"));
         description.click();
@@ -37,11 +40,9 @@ public class ProjectDescriptionTest extends BaseTest {
         submit.click();
 
         getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
-
         getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).clear();
         getDriver().findElement(By.xpath("//textarea[@class='jenkins-input   ']")).sendKeys("Test");
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(), expectedResult);
     }
