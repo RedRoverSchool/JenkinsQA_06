04f6d1e62ae6162fe28a446dbfa169096586643c
Artyom Dulya <fecc1612@gmail.com>
2023-05-12 02:46:12 +0500

    update

--- src/test/java/school/redrover/NewItem3Test.java
+++ src/test/java/school/redrover/NewItem3Test.java
@@ -55,7 +55,6 @@ public class NewItem3Test extends BaseTest {
         getDriver().findElement(By.cssSelector("li:nth-child(1) > a")).click();
 
         WebElement result = getDriver().findElement(By.cssSelector("#projectstatus"));
-
         Assert.assertTrue(result.isDisplayed(), "project no display");
     }
 }
