10d4c3c5043e8edccabcb0acb431681d20107ec0
Stoyana <dicheva.t@gmail.com>
2023-05-11 13:36:27 +0300

    AT 02.05 | Pipeline > Delete Pipeline > Cancel deleting the pipeline project;
    add method to find element using text in xpath;
    
    rename method "CreatePipelineProject" to "CreateJob" and make it common;

--- src/test/java/school/redrover/DeletePipelineTest.java
+++ src/test/java/school/redrover/DeletePipelineTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,49 +11,76 @@ import school.redrover.runner.BaseTest;
 public class DeletePipelineTest extends BaseTest {
 
     private final String RANDOM_STRING = RandomStringUtils.randomAlphabetic(5);
+    private WebElement findElementUsingTextInXpath(String searchText) {
+        return getDriver().findElement(By.xpath("//*[contains(text(),'" + searchText + "')]"));
+    }
 
-    private void createPipeLineProject() {
+    private void goBackToDashboardFromJobPage() {
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+    }
+    private void createJob(String typeOfJob) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(RANDOM_STRING);
-        getDriver().findElement(By.xpath("//span[contains(text(), 'Pipeline')]")).click();
+        findElementUsingTextInXpath(typeOfJob).click();
         getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
     }
 
     @Test
     public void testDeletePipelineByTheLeftSidebar() {
-        createPipeLineProject();
+        createJob("Pipeline");
+
         String projectName = getDriver()
                 .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
                 .getText();
-        getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        goBackToDashboardFromJobPage();
+
         getDriver().findElement(By.xpath("//li[@href = '/']")).click();
         getDriver().findElement(By.xpath("//a[@href = '/view/all/']")).click();
         getDriver().findElement(By.xpath("//li[@href = '/view/all/']")).click();
-        getDriver().findElement(By.xpath("//a[@href = '/view/all/job/"+projectName+"/']")).click();
-        getDriver().findElement(By.xpath("//span[contains(text(), 'Delete Pipeline')]")).click();
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/job/" + projectName + "/']")).click();
+        findElementUsingTextInXpath("Delete Pipeline").click();
         getDriver().switchTo().alert().accept();
 
         Assert.assertTrue(getWait2().until(ExpectedConditions
                 .invisibilityOfElementWithText(By.xpath(
-                        "//*[contains(text(),'"+projectName+"')]"), projectName)));
+                        "//*[contains(text(),'" + projectName + "')]"), projectName)));
         Assert.assertTrue(getWait2().until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(
-                        "//*[contains(text(),'"+projectName+"')]"))));
+                "//*[contains(text(),'" + projectName + "')]"))));
     }
+    @Test
+    public void testDeletePipelineByDropDown() {
+        createJob("Pipeline");
 
-        @Test
-        public void testDeletePipelineByDropDown() {
-                createPipeLineProject();
-                String projectName = getDriver()
-                        .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
-                        .getText();
-                getDriver().findElement(By.name("Submit")).click();
-                getDriver().findElement(By.id("jenkins-name-icon")).click();
-                getDriver().findElement(By.xpath("//span[contains(text(),'" +projectName+ "')]")).click();
-                getDriver().findElement(By.xpath("//span[contains(text(), 'Delete Pipeline')]")).click();
-                getDriver().switchTo().alert().accept();
+        String projectName = getDriver()
+                .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
+                .getText();
 
-                Assert.assertTrue(getWait2().until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(
-                    "//*[contains(text(),'"+projectName+"')]"))));
-        }
+        goBackToDashboardFromJobPage();
+
+        findElementUsingTextInXpath(projectName).click();
+        findElementUsingTextInXpath("Delete Pipeline").click();
+        getDriver().switchTo().alert().accept();
+
+        Assert.assertTrue(getWait2().until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(
+                "//*[contains(text(),'" + projectName + "')]"))));
+    }
+    @Test
+    public void cancelDeletingPipeline() {
+        createJob("Pipeline");
+
+        String jobName = getDriver()
+                .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
+                .getText();
+
+        goBackToDashboardFromJobPage();
+
+        findElementUsingTextInXpath(jobName).click();
+        findElementUsingTextInXpath("Delete Pipeline").click();
+
+        getDriver().switchTo().alert().dismiss();
+
+        Assert.assertTrue(findElementUsingTextInXpath(jobName).isDisplayed());
+    }
 }
