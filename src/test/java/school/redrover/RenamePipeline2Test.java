180dd71806efe8e8bdb0c7f06d6232337e30a0c4
Artem <shvetsartem7@gmail.com>
2023-05-11 14:43:08 -0400

    Rename Class

--- src/test/java/school/redrover/RenamePipeline2Test.java
+++ src/test/java/school/redrover/RenamePipeline2Test.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class RnamePipeline2Test extends BaseTest {
+public class RenamePipeline2Test extends BaseTest {
 
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     private static final By TEXT_FIELD = By.id("name");
