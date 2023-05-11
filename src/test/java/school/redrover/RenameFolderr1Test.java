660d03cd87e9508417e2b05715800845bd2c6939
Mariana Gevorkyan <mariana.mcoa@gmail.com>
2023-05-10 21:10:52 -0400

    AT 04.02| New item > Folder > Rename Folder

--- src/test/java/school/redrover/RenameFolderr1Test.java
+++ src/test/java/school/redrover/RenameFolderr1Test.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class RenameFolder1Test extends BaseTest{
+public class RenameFolderr1Test extends BaseTest{
 
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     private static final By TEXT_FIELD = By.id("name");
@@ -25,10 +25,12 @@ public class RenameFolder1Test extends BaseTest{
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(TEXT_FIELD).sendKeys(projectName);
         getDriver().findElement(FOLDER_PROJECT).click();
+
         getDriver().findElement(OK_BUTTON).click();
         getDriver().findElement(SAVE_BUTTON).click();
         getDriver().findElement(RENAME_BUTTON).click();
         getDriver().findElement(RENAME_FIELD).clear();
+
         getDriver().findElement(RENAME_FIELD).sendKeys(updateName);
         getDriver().findElement(CONFIRM_BUTTON).click();
 
