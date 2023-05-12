5a03f87f5795e0097fdfb355a52525e23a1792d8
DM <davidmav19@yahoo.com>
2023-05-11 21:15:35 -0400

    AT 00.00 CreateNewItem

--- src/test/java/school/redrover/NewItemJenkinsTest.java
+++ src/test/java/school/redrover/NewItemJenkinsTest.java
@@ -13,14 +13,17 @@ public class NewItemJenkinsTest extends BaseTest {
             WebElement buttonNewJob = getDriver().findElement(By.xpath("(//a[@href='/view/all/newJob'])[1]"));
             buttonNewJob.click();
 
-            WebElement enterItemName = getDriver().findElement(By.xpath("//*[@name = \"name\"]"));
+            WebElement enterItemName = getDriver().findElement(By.xpath("//input[@name = \"name\"]"));
             enterItemName.sendKeys("job1");
 
             WebElement multiConfigProject  = getDriver().findElement(By.xpath("//li[@ class = \"hudson_matrix_MatrixProject\"]"));
             multiConfigProject.click();
 
-            WebElement okButton  = getDriver().findElement(By.xpath("//*[@class =\"btn-decorator\"]"));
+            WebElement okButton  = getDriver().findElement(By.xpath("//button[@id = \"ok-button\"]"));
             okButton.click();
+
+            WebElement saveButton  = getDriver().findElement(By.xpath("//button[@formNoValidate = \"formNoValidate\"]"));
+            saveButton.click();
         }
     }
 
