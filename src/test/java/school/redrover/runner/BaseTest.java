e4af29a72abd140c084830786e34d9f81d18c4c0
Sergey.D <sergei.demyanenko@gmail.com>
2023-05-12 00:39:17 -0400

    Add test filter

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import school.redrover.runner.order.OrderForTests;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
 
+@Listeners({FilterForTests.class, OrderForTests.class})
 public abstract class BaseTest {
 
     private WebDriverWait wait2;
