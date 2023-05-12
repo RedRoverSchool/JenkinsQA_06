e35e07d5a0fe9f6156b8297230e04b3d62e8a65b
Elena Lukacheva <elenaly4@gmail.com>
2023-05-12 10:32:45 +0300

    Create Freestyle Project Test

---
+++ src/test/java/school/redrover/CreateFreestyleProject2aTest.java
@@ -0,0 +1,25 @@
+package school.redrover;
+
+import school.redrover.runner.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateFreestyleProject2aTest extends BaseTest {
+    @Test
+    public void testCreateFreestyleProject() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob'][@class='task-link ']")).click();
+        getDriver().findElement(By.xpath("//input[@id = 'name']")).sendKeys("Project One1");
+        getDriver().findElement(By.xpath("//span [text() = 'Создать задачу со свободной конфигурацией']")).click();
+        getDriver().findElement(By.xpath("//button [@type = 'submit'][@id = 'ok-button']")).click();
+        getDriver().findElement(By.xpath("//textarea [@name = 'description']"))
+                .sendKeys("Проект по автоматизации тестирования");
+        getDriver().findElement(By.xpath("//button [@ name = 'Submit']")).click();
+        getDriver().findElement(By.xpath("//div[@id = 'breadcrumbBar']//li[@class = 'jenkins-breadcrumbs__list-item']//a[@class = 'model-link'][text() = 'Dashboard']"))
+                .click();
+        String projectName = getDriver().findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']"))
+                .getText();
+        Assert.assertEquals(projectName, "Project One1");
+
+    }
+}
