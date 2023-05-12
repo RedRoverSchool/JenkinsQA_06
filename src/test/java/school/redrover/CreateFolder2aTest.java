6316f3eac8c7ff40386becb326f0ae9e212adb33
Elena Lukacheva <elenaly4@gmail.com>
2023-05-12 11:00:40 +0300

    Create Folder Test

---
+++ src/test/java/school/redrover/CreateFolder2aTest.java
@@ -0,0 +1,29 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class CreateFolder2aTest extends BaseTest {
+
+    @Test
+    public void testCreateFolder() {
+
+        String folderName = "Demo";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob'][@class='task-link ']")).click();
+        getDriver().findElement(By.xpath("//input[@id = 'name']")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//span[text() = 'Folder']")).click();
+        getDriver().findElement(By.xpath("//button [@id = 'ok-button']")).click();
+        getDriver().findElement(By.xpath("//button [@formnovalidate='formNoValidate' and @name = 'Submit']"))
+                .click();
+        getDriver().findElement(By.xpath("//a[@href='/' and @class = 'model-link']")).click();
+
+
+        String folderNameFakt = getDriver().findElement(By.xpath("//a[@class = 'jenkins-table__link model-link inside']"))
+                .getText();
+
+        Assert.assertEquals(folderNameFakt, folderName);
+    }
+}
