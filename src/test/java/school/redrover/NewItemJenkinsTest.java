79488c2041e3f91e6d914ff6e3a40d989c8fcaf4
DM <davidmav19@yahoo.com>
2023-05-13 09:01:42 -0400

    AT 00.00 CreateNewItem

---
+++ src/test/java/school/redrover/NewItemJenkinsTest.java
@@ -0,0 +1,31 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+public class NewItemJenkinsTest extends BaseTest {
+
+   
+        @Test
+        public void createNewJob()
+        {
+            WebElement buttonNewJob = getDriver().findElement(By.xpath("(//a[@href='/view/all/newJob'])[1]"));
+            buttonNewJob.click();
+
+            WebElement enterItemName = getDriver().findElement(By.xpath("//input[@name = \"name\"]"));
+            enterItemName.sendKeys("job1");
+
+            WebElement multiConfigProject  = getDriver().findElement(By.xpath("//li[@ class = \"hudson_matrix_MatrixProject\"]"));
+            multiConfigProject.click();
+
+            WebElement okButton  = getDriver().findElement(By.xpath("//button[@id = \"ok-button\"]"));
+            okButton.click();
+
+            WebElement saveButton  = getDriver().findElement(By.xpath("//button[@formNoValidate = \"formNoValidate\"]"));
+            saveButton.click();
+        }
+    }
+
+
+
