9c1d7e57e060e902069bab148fd01a58df45f1ef
Anna Morozova <morozanna503@gmail.com>
2023-05-13 16:45:50 +0400

    change method createMultibranchPipeline and added method scrollByVisibleElement

--- src/test/java/school/redrover/MultibranchPipeline2Test.java
+++ src/test/java/school/redrover/MultibranchPipeline2Test.java
@@ -2,6 +2,8 @@ package school.redrover;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -16,14 +18,21 @@ public class MultibranchPipeline2Test extends BaseTest {
     private static final By SAVE_BUTTON = By.name("Submit");
     private static final By NEW_ITEM_BUTTON = By.linkText("New Item");
     private static final By FIELD_FOR_NAME = By.id("name");
+    private static final By MULTI_BRANCH_BUTTON = By.cssSelector("div img.icon-pipeline-multibranch-project");
 
     private void createMultibranchPipeline(){
         getDriver().findElement(NEW_ITEM_BUTTON).click();
         getDriver().findElement(FIELD_FOR_NAME).sendKeys(RANDOM_NAME_MULTIBRANCH_PIPELINE);
-        WebElement multibranchButton = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li.org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject")));
+        scrollByVisibleElement(MULTI_BRANCH_BUTTON, getDriver());
+        WebElement multibranchButton = getWait10().until(ExpectedConditions.visibilityOfElementLocated(MULTI_BRANCH_BUTTON));
         multibranchButton.click();
         getDriver().findElement(OK_BUTTON).click();
         getDriver().findElement(SAVE_BUTTON).click();
+    }
+
+    private static void scrollByVisibleElement(By by, WebDriver driver) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(by));
     }
 
     @Ignore
@@ -57,7 +66,9 @@ public class MultibranchPipeline2Test extends BaseTest {
 
         getDriver().findElement(By.cssSelector("a span [class='icon-edit-delete icon-md']")).click();
         getDriver().findElement(By.cssSelector("button.jenkins-button.jenkins-button--primary")).click();
+        WebElement welcomeText = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()='Welcome to Jenkins!']")));
+        String getTextWelcome =welcomeText.getText();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='Welcome to Jenkins!']")).getText(),"Welcome to Jenkins!");
+        Assert.assertEquals(getTextWelcome,"Welcome to Jenkins!");
     }
 }
