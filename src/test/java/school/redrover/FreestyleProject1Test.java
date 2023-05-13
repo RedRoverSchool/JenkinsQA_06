a584b97d246956face55ea54beaeb6c86ca71805
BNastasia <asya1551@gmail.com>
2023-05-13 12:41:24 -0400

    del import ignore

--- src/test/java/school/redrover/FreestyleProject1Test.java
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
