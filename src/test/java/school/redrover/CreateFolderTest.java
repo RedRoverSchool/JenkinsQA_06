c71dda6016ce6fb2699163bc94aa1f7ad6a350dc
Anastasiya Abramova <nastionasonne@mail.ru>
2023-05-14 09:30:53 +0500

    AT 03.02 | Multi-Configuration >Enable  project

--- src/test/java/school/redrover/CreateFolderTest.java
+++ src/test/java/school/redrover/CreateFolderTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
