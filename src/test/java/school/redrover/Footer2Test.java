68fd34117a2eb46b0b38aeb992c59ee304e6742f
elent <elent@ukr.net>
2023-05-14 12:02:41 +0300

    AT 13.02| Footer > REST API > Find a link to REST API

--- src/test/java/school/redrover/Footer2Test.java
+++ src/test/java/school/redrover/Footer2Test.java
@@ -10,10 +10,10 @@ import school.redrover.runner.BaseTest;
 public class Footer2Test extends BaseTest {
 
     @Test
-    public void restApiLinkTest() {
-        getDriver().findElement(By.xpath("//a[text()='REST API']")).click();
+    public void testRestApiLink() {
+        getDriver().findElement(By.xpath("//a[contains(@href,'api')]")).click();
 
-        WebElement restApiPage =getDriver().findElement(By.xpath("//h1[text()='REST API']"));
+        WebElement restApiPage =getDriver().findElement(By.xpath("//h1"));
 
         getWait2().until(ExpectedConditions.visibilityOf(restApiPage));
         Assert.assertEquals(restApiPage.getText(),"REST API");
