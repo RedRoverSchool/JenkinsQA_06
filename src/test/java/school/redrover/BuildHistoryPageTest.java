b14fe0ecd40469e937f003b2a733c512b4926504
Serge Pratasevich <serge.pratasevich@gmail.com>
2023-05-15 14:54:48 -0500

    localhost added into local.properties and created partial testAddDescriptionForBuild

--- src/test/java/school/redrover/BuildHistoryPageTest.java
+++ src/test/java/school/redrover/BuildHistoryPageTest.java
@@ -2,11 +2,46 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class BuildHistoryPageTest extends BaseTest {
+    private static final String NAME_PIPELINE = "Pipeline2023";
+    private static final String BUILD_DESCRIPTION = "For QA";
+    private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
+    private static final By LOGO_JENKINS = By.id("jenkins-name-icon");
+    private static final By INPUT_NAME_FIELD = By.xpath("//input[@id='name']");
+    private static final By PIPELINE_PROJECT = By.xpath("//label/span[text()='Pipeline']");
+    private static final By OK_BUTTON = By.id("ok-button");
+    private static final By SAVE_BUTTON = By.name("Submit");
+    private static final By H1_NAME_PIPELINE = By.xpath("//h1");
+    private static final By BUILD_HISTORY = By.xpath("//span[text()='Build History']");
+    private static final By BUILD_SCHEDULE = By.xpath("//span[@class='build-button-column-icon-reference-holder']");
+    private static final By SERIAL_NUMBER_OF_BUILD = By.xpath(
+            "//a[@class='jenkins-table__link jenkins-table__badge model-link inside']");
+    private static final By DROP_DOWN_SERIAL_NUMBER = By.xpath(
+            "//a[@class='jenkins-table__link jenkins-table__badge model-link inside']/button[@class='jenkins-menu-dropdown-chevron']");
+    private static final By EDIT_BUILD_INFORMATION = By.xpath("//span[text()='Edit Build Information']");
+    private static final By DESCRIPTION_FIELD = By.xpath("//textarea[@name='description']");
+    private static final By DESCRIPTION_TEXT = By.id("description");
+
+    private void createNewPipeline(String name){
+        getDriver().findElement(NEW_ITEM).click();
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(INPUT_NAME_FIELD));
+        getDriver().findElement(INPUT_NAME_FIELD).sendKeys(NAME_PIPELINE);
+
+        getDriver().findElement(PIPELINE_PROJECT).click();
+
+        getDriver().findElement(OK_BUTTON).click();
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(SAVE_BUTTON)).click();
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(H1_NAME_PIPELINE));
+    }
+
     @Test
     public void testNavigateToBuildHistoryPage() throws InterruptedException {
 
@@ -21,5 +56,10 @@ public class BuildHistoryPageTest extends BaseTest {
 
         Assert.assertEquals(actualBuildHistoryPageTitle, expectedBuildHistoryPageTitle);
         Assert.assertEquals(actualBuildHistoryPageUrl, expectedBuildHistoryPageUrl);
+    }
+
+    @Test
+    public void testAddDescriptionForBuild(){
+        createNewPipeline(NAME_PIPELINE);
     }
 }
