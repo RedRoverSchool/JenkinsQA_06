44271c48cfbbdb6647f5489fca861a4c9f975c8f
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2023-05-15 17:23:26 +0300

    RF FolderDeleteTest

--- src/test/java/school/redrover/FolderDeleteTest.java
+++ src/test/java/school/redrover/FolderDeleteTest.java
@@ -9,34 +9,17 @@ import school.redrover.runner.BaseTest;
 
 public class FolderDeleteTest extends BaseTest {
 
-    private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
-
-    private static final By FOLDER = By.xpath("//*[@id='j-add-item-type-nested-projects']//div[@class='desc']");
-
-    private static final By ENTER_ITEM_NAME = By.xpath("//input[@id='name']");
-
-    private static final By OK_BUTTON = By.xpath("//button[@id='ok-button']");
-
-    private static final By SUBMIT_BUTTON = By.xpath("//button[@name='Submit']");
-
-    private static final By BREADCRUMBS = By.xpath("//*[@id='breadcrumbs']//a");
-
-    private static final By DELETE_FOLDER = By.xpath("//span[@class='task-link-text']");
-
-    private static final By SUBMIT_DELETE_BUTTON = By.xpath("//button[@name='Submit']");
-
-    private static final By Title = By.xpath("//h1[contains(text(),'Добро пожаловать в Jenkins!')]");
     private void newFolder(String name) {
-        getWait2().until(ExpectedConditions.elementToBeClickable(NEW_ITEM)).click();
-        getWait2().until(ExpectedConditions.elementToBeClickable(ENTER_ITEM_NAME)).sendKeys(name);
-        getWait2().until(ExpectedConditions.elementToBeClickable(FOLDER)).click();
-        getWait2().until(ExpectedConditions.elementToBeClickable(OK_BUTTON)).click();
-        getWait2().until(ExpectedConditions.elementToBeClickable(SUBMIT_BUTTON)).click();
-        getWait2().until(ExpectedConditions.elementToBeClickable(BREADCRUMBS)).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/view/all/newJob']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='name']"))).sendKeys(name);
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='j-add-item-type-nested-projects']//div[@class='desc']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='ok-button']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='breadcrumbs']//a"))).click();
     }
 
     @Test
-    public void testNewFolder() {
+    public void testNewFolderCreate() {
         String name = "FolderTest";
         newFolder(name);
 
