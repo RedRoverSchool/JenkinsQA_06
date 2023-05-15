6022583672874db9310a85921ebd1b4fc1797b2b
Vidim83 <08091985n@gmail.com>
2023-05-15 21:00:45 +0300

    AT 03.01 | Multi-Configuration project > add description

--- src/test/java/school/redrover/MultiConfigurationProjectVDTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectVDTest.java
@@ -11,7 +11,9 @@ import school.redrover.runner.TestUtils;
 
 public class MultiConfigurationProjectVDTest extends BaseTest {
 
-    private static final String PROJECT_NAME = "Tricky Project";
+    private static final String PROJECT_NAME = "Tricky_Project";
+
+    private static final By OK_BUTTON = By.xpath("//*[@name='Submit']");
 
     @Test
     public void testCreateProject() {
@@ -25,7 +27,7 @@ public class MultiConfigurationProjectVDTest extends BaseTest {
         getDriver().findElement(By.xpath("//*[@class='hudson_matrix_MatrixProject']")).click();
         getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
 
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@name='Submit']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(OK_BUTTON)).click();
 
         WebElement expectedResult = getDriver().findElement(By.xpath("//*[@class='matrix-project-headline page-headline']"));
         Assert.assertEquals(expectedResult.getText(), "Project " + PROJECT_NAME);
@@ -36,13 +38,13 @@ public class MultiConfigurationProjectVDTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//*[@class ='jenkins-table__link model-link inside']")).click();
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/job/Tricky%20Project/configure']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='tasks']/div[5]/span/a"))).click();
 
         getWait2().until(ExpectedConditions.visibilityOfElementLocated
                         (By.xpath("//*[@name='description']")))
-                .sendKeys(PROJECT_NAME, Keys.RETURN);
+                .sendKeys(PROJECT_NAME);
 
-        getDriver().findElement(By.xpath("//*[@name='Submit']")).click();
+        getDriver().findElement(OK_BUTTON).click();
         Assert.assertEquals(getWait2()
                 .until(ExpectedConditions.visibilityOfElementLocated
                         (By.xpath("//*[@id='description']/div[1]"))).getText(), PROJECT_NAME);
