61ca05984424c9070a0f4df6c1474ceb617c32fa
msnata <msnata@ukr.net>
2023-05-16 23:23:43 +0300

    AT 01.01 Freestyle project Rename the project

--- src/test/java/school/redrover/FreestyleProject5Test.java
+++ src/test/java/school/redrover/FreestyleProject5Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -38,5 +39,33 @@ public class FreestyleProject5Test extends BaseTest {
                 .getText().trim().substring(0, 34),"This project is currently disabled" );
 
         getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']//a[@class='model-link']")).click();
+    }
+
+    @Test
+    public void testRenameFreestyleProject(){
+        TestUtils.createFreestyleProject(this, "First Freestyle Project", true);
+
+        WebElement mouseOnProject = getDriver()
+                .findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']"));
+
+        new Actions(getDriver())
+                .moveToElement(mouseOnProject)
+                .click(getWait2().until(ExpectedConditions
+                        .elementToBeClickable(By.xpath("//tr[@id='job_First Freestyle Project']/td[3]//button[@class='jenkins-menu-dropdown-chevron']"))))
+                .perform();
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@id='yui-gen6']")));
+        getDriver().findElement(By.xpath("//li[@id='yui-gen6']")).click();
+
+        WebElement inputField = getDriver().findElement(By.xpath("//input[@name='newName']"));
+                inputField.clear();
+                inputField.sendKeys("Second");
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+
+        getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']//a[@class='model-link']")).click();
+        WebElement projectName =
+                getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']/span"));
+
+        Assert.assertEquals(projectName.getText(), "Second");
     }
 }
