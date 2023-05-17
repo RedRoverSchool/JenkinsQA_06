b117d6221d59b104aefb374687b3449433e1dddb
Stoyana <dicheva.t@gmail.com>
2023-05-17 07:06:33 +0300

    AT | 00.02 | Pipeline > Create PipelineJob Add test createPipeline  Add getProjectNameFromRandomString  Rename class DeletePipelineTest to PipelineJobTest

--- src/test/java/school/redrover/PipelineJobTest.java
+++ src/test/java/school/redrover/PipelineJobTest.java
@@ -6,24 +6,29 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
-public class DeletePipelineTest extends BaseTest {
+public class PipelineJobTest extends BaseTest {
 
     private final String RANDOM_STRING = RandomStringUtils.randomAlphabetic(5);
 
-    private void createPipeLineProject() {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(RANDOM_STRING);
-        getDriver().findElement(By.xpath("//span[contains(text(), 'Pipeline')]")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-    }
-
-    @Test
-    public void testDeletePipelineByTheLeftSidebar() {
-        createPipeLineProject();
-        String projectName = getDriver()
+    private String getProjectNameFromRandomString() {
+        return getDriver()
                 .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
                 .getText();
+    }
+
+   @Test
+   public void testCreatePipeline() {
+       TestUtils.createPipeline(this, RANDOM_STRING, false);
+       String projectName = getProjectNameFromRandomString();
+       getDriver().findElement(By.id("jenkins-name-icon")).click();
+       Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href= 'job/"+projectName+"/']//span")).isDisplayed());
+   }
+    @Test
+    public void testDeletePipelineByTheLeftSidebar() {
+        TestUtils.createPipeline(this, RANDOM_STRING, false);
+        String projectName = getProjectNameFromRandomString();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
         getDriver().findElement(By.xpath("//li[@href = '/']")).click();
@@ -42,10 +47,8 @@ public class DeletePipelineTest extends BaseTest {
 
         @Test
         public void testDeletePipelineByDropDown() {
-                createPipeLineProject();
-                String projectName = getDriver()
-                        .findElement(By.xpath("(//li[@class= 'jenkins-breadcrumbs__list-item']/a)[2]"))
-                        .getText();
+                TestUtils.createPipeline(this, RANDOM_STRING, false);
+                String projectName = getProjectNameFromRandomString();
                 getDriver().findElement(By.name("Submit")).click();
                 getDriver().findElement(By.id("jenkins-name-icon")).click();
                 getDriver().findElement(By.xpath("//span[contains(text(),'" +projectName+ "')]")).click();
