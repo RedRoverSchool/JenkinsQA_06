d1f1a0fd6122fb915012cbc2336a5bcbefaa269b
Ruslan Fursa <fursa_1986@mail.ru>
2023-05-17 16:26:36 +0300

    add testConfigureNumOfExecutorsInMasterNode

--- src/test/java/school/redrover/ManageJenkins2Test.java
+++ src/test/java/school/redrover/ManageJenkins2Test.java
@@ -1,4 +1,35 @@
 package school.redrover;
 
-public class ManageJenkins2Test {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class ManageJenkins2Test extends BaseTest {
+    public static final int QNT_EXECUTORS = 8;
+    @Test
+    public void testConfigureNumOfExecutorsInMasterNode() {
+        WebElement manageJenkinsTab = getDriver().findElement(By.xpath("//a[@href = '/manage']"));
+        manageJenkinsTab.click();
+
+        WebElement configureSystemSection = getDriver().findElement(By.xpath("//a[@href = 'configure']"));
+        configureSystemSection.click();
+
+        WebElement numOfExecutorsInput = getDriver().findElement(By.xpath("//input[@name = '_.numExecutors']"));
+        numOfExecutorsInput.clear();
+        numOfExecutorsInput.sendKeys(String.valueOf(QNT_EXECUTORS));
+
+        WebElement saveBtn = getDriver().findElement(By.xpath("//button[@name = 'Submit']"));
+        saveBtn.click();
+
+        WebElement dashBoardBreadcrumb = getDriver().findElement(By.xpath("//a[@href = '/'][1]"));
+        getWait5().until(ExpectedConditions.elementToBeClickable(dashBoardBreadcrumb)).click();
+
+        String executorStatusAfterChanges = getDriver().findElement(By.
+                xpath("//td[contains (text(), 'executors busy') ]")).getText();
+
+        Assert.assertEquals(executorStatusAfterChanges, "built-in node + 1 agent (0 of " + QNT_EXECUTORS + " executors busy)");
+    }
 }
