86616d805f2c07f77e3c98b1dfbc01d534d8e21a
ArinaJur <arina.juravleva.1@gmail.com>
2023-05-18 16:35:44 -0400

    add click with Keys.ENTER

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -1,14 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -24,12 +24,6 @@ public class DashboardTest extends BaseTest {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link"))).click();
     }
 
-    private void moveToElement(WebElement element) {
-        Actions actions = new Actions(getDriver());
-
-        actions.moveToElement(element).perform();
-    }
-
     private List<String> getTexts(List<WebElement> elements) {
         List<String> texts = new ArrayList<>();
 
@@ -40,8 +34,9 @@ public class DashboardTest extends BaseTest {
         return texts;
     }
 
-    @Test(invocationCount = 1)
-    public void testMenuIsShownWhenClickingButtonNearProjectName() {
+    @Test()
+    public void testMenuIsShownWhenClickingButtonNearProjectName() throws InterruptedException {
+        final String projectName = UUID.randomUUID().toString();
         final List<String> expectedMenus = List.of(
                 "Changes",
                 "Workspace",
@@ -51,33 +46,17 @@ public class DashboardTest extends BaseTest {
                 "Rename"
         );
 
-        createFreestyleProjectWithDefaultConfigurations(UUID.randomUUID().toString());
+        createFreestyleProjectWithDefaultConfigurations(projectName);
 
-        WebElement firstProjectNameInList = getWait5().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
-                By.xpath("//table[@id='projectstatus']//tbody/tr/td[3]/a"))).get(0);
+        WebElement chevron = getWait5().until(ExpectedConditions
+                .visibilityOfElementLocated(
+                        By.xpath("//a[@href='job/" + projectName + "/']/button"))
+        );
+        chevron.sendKeys(Keys.ENTER);
 
-        WebElement dropDownMenuButton = firstProjectNameInList.findElement(By.xpath("//button"));
+        List<WebElement> menus = getDriver().findElements(
+                By.xpath("//div[@id = 'breadcrumb-menu' and @class = 'yui-module yui-overlay yuimenu visible']//li/a/span"));
 
-        Actions action = new Actions(getDriver());
-        action.moveToElement(firstProjectNameInList).perform();
-        System.out.println(dropDownMenuButton.isDisplayed());
-        System.out.println(dropDownMenuButton.isEnabled());
-
-        System.out.println(dropDownMenuButton.getAttribute("class"));
-        getWait5().until(ExpectedConditions.visibilityOf(dropDownMenuButton));
-        System.out.println(dropDownMenuButton.isDisplayed());
-        System.out.println(dropDownMenuButton.isEnabled());
-        System.out.println(dropDownMenuButton.getAttribute("class"));
-
-
-
-        action.moveToElement(dropDownMenuButton).click().build().perform();
-
-        getWait5().until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@id='breadcrumb-menu']//a//span")));
-
-        List<WebElement> menus = getDriver().findElements(By.xpath("//div[@id='breadcrumb-menu']//a//span"));
-        System.out.println(getTexts(menus));
-
-//        Assert.assertEquals(getTexts(menus), expectedMenus);
+        Assert.assertEquals(getTexts(menus), expectedMenus);
     }
 }
