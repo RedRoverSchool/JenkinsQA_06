5c2d3d3e7d17cab84fa3af60af8c2828b99f94b7
KathyGU <dendritsm@gmail.com>
2023-05-18 11:43:01 +0300

    RF 00.00 | ManageUsersTest > testMakeChangesToUserProfile

--- src/test/java/school/redrover/ManageUsersTest.java
+++ src/test/java/school/redrover/ManageUsersTest.java
@@ -1,19 +1,18 @@
 package school.redrover;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
 public class ManageUsersTest extends BaseTest {
     private final By MANAGE_JENKINS = By.xpath("//a[@href='/manage']");
     private final By MANAGE_USERS = By.xpath("//a[@href='securityRealm/']");
     private final By SUBMIT = By.xpath("//button[@name='Submit']");
     private final By USERS_TABLE = By.id("people");
+    private static final By DASHBOARD = By.xpath("//img[@id='jenkins-head-icon']");
 
     @DataProvider(name = "userdata provider")
     public Object[][] provideUserData() {
@@ -22,9 +21,9 @@ public class ManageUsersTest extends BaseTest {
                         {"Chewbacca", "Wookiee", "Wookiee", "Chewie", "Chewie@test.com"}};
     }
 
-    @Test (dataProvider = "userdata provider")
+    @Test(dataProvider = "userdata provider")
     public void testCreateUser(String username, String password, String confirmPassword, String name, String email) {
-       getWait2().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS)).click();
+        getDriver().findElement(MANAGE_JENKINS).click();
         getDriver().findElement(MANAGE_USERS).click();
         getDriver().findElement(By.xpath("//a[@href = 'addUser']")).click();
         getDriver().findElement(By.id("username")).sendKeys(username);
@@ -38,34 +37,11 @@ public class ManageUsersTest extends BaseTest {
                 .visibilityOfElementLocated(USERS_TABLE)).getText().contains(username));
     }
 
-    @Test (dependsOnMethods = "testCreateUser")
-    public void testDeleteUserFromUserList() {
-        getDriver().findElement(MANAGE_JENKINS).click();
-        getDriver().findElement(MANAGE_USERS).click();
-        getDriver().findElement(By.xpath("//a[contains(@href, 'chewbacca/delete')]")).click();
-        getDriver().findElement(SUBMIT).click();
-
-        Assert.assertFalse(getWait2().until(ExpectedConditions
-                .visibilityOfElementLocated(USERS_TABLE)).getText().contains("Chewbacca"));
-    }
-
-    @Test (dependsOnMethods = {"testCreateUser", "testDeleteUserFromUserList"})
-    public void testLogInWithDeletedUserCredentials() {
-        getDriver().findElement(By.xpath("//a[@href= '/logout']")).click();
-        getDriver().findElement(By.id("j_username")).sendKeys("Chewbacca");
-        getDriver().findElement(By.xpath("//input[@name='j_password']")).sendKeys("Wookiee");
-        getDriver().findElement(SUBMIT).click();
-
-        Assert.assertEquals(getDriver().findElement(By
-                        .xpath("//div[contains(@class, 'alert-danger')]")).getText(),
-                "Invalid username or password");
-    }
-
-    @Test (dependsOnMethods = "testCreateUser")
-    public void testMakeChangesToUserProfile() throws InterruptedException {
+    @Test(dependsOnMethods = "testCreateUser")
+    public void testMakeChangesToUserProfile()  {
         JavascriptExecutor js = (JavascriptExecutor)getDriver();
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS)).click();
+        getDriver().findElement(MANAGE_JENKINS).click();
         getDriver().findElement(MANAGE_USERS).click();
 
         WebElement user = getDriver().findElement(By.xpath("//a[@href='user/jabbathehutt/']"));
@@ -87,6 +63,29 @@ public class ManageUsersTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By
                 .xpath("//h1")).getText(), "Jabbka");
+    }
+
+    @Test(dependsOnMethods = "testMakeChangesToUserProfile")
+    public void testDeleteUserFromUserList() {
+        getDriver().findElement(MANAGE_JENKINS).click();
+        getDriver().findElement(MANAGE_USERS).click();
+        getDriver().findElement(By.xpath("//a[contains(@href, 'chewbacca/delete')]")).click();
+        getDriver().findElement(SUBMIT).click();
+
+        Assert.assertFalse(getWait2().until(ExpectedConditions
+                .visibilityOfElementLocated(USERS_TABLE)).getText().contains("Chewbacca"));
+    }
+
+    @Test(dependsOnMethods =  "testDeleteUserFromUserList")
+    public void testLogInWithDeletedUserCredentials() {
+        getDriver().findElement(By.xpath("//a[@href= '/logout']")).click();
+        getDriver().findElement(By.id("j_username")).sendKeys("Chewbacca");
+        getDriver().findElement(By.xpath("//input[@name='j_password']")).sendKeys("Wookiee");
+        getDriver().findElement(SUBMIT).click();
+
+        Assert.assertEquals(getDriver().findElement(By
+                        .xpath("//div[contains(@class, 'alert-danger')]")).getText(),
+                "Invalid username or password");
     }
 }
 
