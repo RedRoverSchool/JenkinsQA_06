d3fe03812b1c82b096a73d4b746c8cdf5abe090b
Artyom Dulya <fecc1612@gmail.com>
2023-05-19 14:27:00 +0500

    RF refactor class NewProject3Test

--- src/test/java/school/redrover/NewProject3Test.java
+++ src/test/java/school/redrover/NewProject3Test.java
@@ -25,14 +25,10 @@ public class NewProject3Test extends BaseTest {
     @Test
     public void testCreatePipProject() {
         String expectedPipeline = "Pipeline Engineer";
-        String expectedResult = "Engineer";
+        String nameProject = "Engineer";
 
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
-        WebElement itemName = getDriver().findElement(By.xpath("//input[@name = 'name']"));
-        itemName.click();
-        itemName.sendKeys(expectedResult);
-        getDriver().findElement(By.xpath("//div[@id='items']//li[2]")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        new NewJobPage(getDriver()).enterItemName(nameProject).selectPipelineAndOk();
         getDriver().findElement(By.xpath("//button[@formNoValidate='formNoValidate']")).click();
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("[class$='headline']")).getText(), expectedPipeline);
@@ -40,16 +36,14 @@ public class NewProject3Test extends BaseTest {
         getDriver().findElement(By.xpath("//ol[@id='breadcrumbs']/li[1]")).click();
         String actualResult = getDriver().findElement(By.cssSelector("[href$='Engineer/']")).getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, nameProject);
     }
 
     @Test
     public void testCreateMultiConfigurationProject() {
 
         getDriver().findElement(By.cssSelector("[href$='/newJob']")).click();
-        getDriver().findElement(By.cssSelector("input[id='name']")).sendKeys("Engineer3");
-        getDriver().findElement(By.cssSelector("[value$='MatrixProject'] + span")).click();
-        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        new NewJobPage(getDriver()).enterItemName("Engineer3").selectMultiConfigurationProjectAndOk();
         getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
         getDriver().findElement(By.cssSelector("li:nth-child(1) > a")).click();
 
