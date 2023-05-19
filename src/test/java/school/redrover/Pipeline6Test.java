233a16e4ec502ea9cc9541e3e105696223962c92
KaterynaYazici <katerynayazici7@gmail.com>
2023-05-19 21:56:19 +0300

    AT 00.02 | changed test/refactor name

--- src/test/java/school/redrover/Pipeline6Test.java
+++ src/test/java/school/redrover/Pipeline6Test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-public class CreatePipelineProject8Test extends BaseTest {
+public class Pipeline6Test extends BaseTest {
 
     @Test
     public void testNewItemSubmit() {
