f2d87f8bb2eb3a6926b1beaf46496b53b151bfbf
Artyom Dulya <fecc1612@gmail.com>
2023-05-19 11:51:32 +0500

    createNewJobPage

--- src/test/java/school/redrover/NewProject3Test.java
+++ src/test/java/school/redrover/NewProject3Test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.model.NewJobPage;
 import school.redrover.runner.BaseTest;
 
 public class NewProject3Test extends BaseTest {
@@ -13,9 +14,7 @@ public class NewProject3Test extends BaseTest {
         String expectedResult = "Engineer2";
 
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Engineer2");
-        getDriver().findElement(By.cssSelector("[value='hudson.model.FreeStyleProject'] + span")).click();
-        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        new NewJobPage(getDriver()).enterItemName("Engineer2").selectFreestyleProjectAndOk();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//ol[@id='breadcrumbs']/li[1]")).click();
 
