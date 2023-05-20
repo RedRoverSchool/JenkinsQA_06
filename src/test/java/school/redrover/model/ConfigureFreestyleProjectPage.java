2725fc21215afe23f6c65ae92def97c946cab548
Artyom Dulya <fecc1612@gmail.com>
2023-05-21 01:23:06 +0500

    Create configuration FreestyleProject Page

---
+++ src/test/java/school/redrover/model/ConfigureFreestyleProjectPage.java
@@ -0,0 +1,13 @@
+package school.redrover;
+
+import org.openqa.selenium.WebDriver;
+import school.redrover.model.base.BasePage;
+
+public class ConfigureFreestylePage extends BasePage {
+
+    public ConfigureFreestylePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public 
+}
