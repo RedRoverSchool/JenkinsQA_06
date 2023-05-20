f653fe709241eaa3d59167ca9c2c3986dec19f45
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-05-20 16:09:16 +0300

    remove unused import

--- src/test/java/school/redrover/FreestyleProject12Test.java
+++ src/test/java/school/redrover/FreestyleProject12Test.java
@@ -6,7 +6,6 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.TestUtils;
 
 public class FreestyleProject12Test extends BaseTest {
 
