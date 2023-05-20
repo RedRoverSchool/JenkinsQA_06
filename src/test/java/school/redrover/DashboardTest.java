1f4059961a0a263be69317b5f8c4575460b96bbd
JuliaChernakov <julia.chernakov@gmail.com>
2023-05-20 14:39:04 +0300

    AT 17.01 | Page-header > Logo > Return to the dashboard page

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -60,6 +60,7 @@ public class DashboardTest extends BaseTest {
         Assert.assertEquals(getTexts(menus), expectedMenus);
     }
 
+
     @Test
     public void testReturnToDashboardPage() {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
