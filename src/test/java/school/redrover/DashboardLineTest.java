e9288334305623270c59740a0a9160fbd85be383
msnata <msnata@ukr.net>
2023-05-24 00:28:32 +0300

    AT12.01_DashboardLine_DashboardDropdown-menu_VerifyOptionsName_msnata

--- src/test/java/school/redrover/DashboardLineTest.java
+++ src/test/java/school/redrover/DashboardLineTest.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.*;
+
 public class DashboardLineTest extends BaseTest {
 
     @Test
@@ -27,7 +29,22 @@ public class DashboardLineTest extends BaseTest {
 
         dashboardDropdownMenu.click();
 
-        Assert.assertEquals(getDriver().
-                findElement(By.xpath("//div[@id='breadcrumb-menu']//span[text()='People']")).getText(), "People");
+//        Assert.assertEquals(getDriver().
+//                findElement(By.xpath("//div[@id='breadcrumb-menu']//span[text()='People']")).getText(), "People");
+
+        List<String> expectedText = Arrays.asList("New Item", "People", "Build History", "Manage Jenkins", "My Views");
+
+
+        List<WebElement> menuList = getDriver()
+                .findElements(By.xpath("//div[@id='breadcrumb-menu-target']//li[@class='yuimenuitem first-of-type']/parent::ul/li"));
+//        for(WebElement el : menuList) {
+//
+//            Assert.assertEquals(el.getText(), expectedText);
+//        }
+
+//        for (int i = 0; i < 5; i++) {
+//            Assert.assertEquals(menuList.get(i).getText(), expectedText[i]);
+
+        }
     }
 }
