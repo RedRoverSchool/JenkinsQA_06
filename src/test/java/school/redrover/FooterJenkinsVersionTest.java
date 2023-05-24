ec3242c5577c9704aef924aed57378be5b318e3f
Dasha Turevich <dasha.turevich@yandex.ru>
2023-05-24 23:06:08 +0300

    RF | Combining FooterTest classes US 13.01

--- src/test/java/school/redrover/FooterJenkinsVersionTest.java
+++ src/test/java/school/redrover/FooterJenkinsVersionTest.java
@@ -2,11 +2,12 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.JenkinsVersionPage;
 import school.redrover.model.MainPage;
+import school.redrover.model.ManageJenkinsPage;
 import school.redrover.runner.BaseTest;
 
 
@@ -36,18 +37,53 @@ public class FooterJenkinsVersionTest extends BaseTest {
         Assert.assertEquals(actualJenkinsVersion, expectedJenkinsVersion, "Jenkins version does not match");
     }
 
-    @Ignore
     @Test
-    public void testClickOnJenkinsVersionOpensSiteOnNodesPage(){
+    public void testCheckJenkinsVersionInPeoplePage(){
+        WebElement peoplePageButton = getDriver().findElement(By.xpath("//a[@href='/asynchPeople/']"));
+        peoplePageButton.click();
 
-        getDriver().findElement(By.xpath("//span[@class='pane-header-title']/a")).click();
-        getDriver().findElement(By.xpath("//a[@target='_blank']")).click();
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'People')]")));
 
-        for(String winHandle : getDriver().getWindowHandles()) {
-            getDriver().switchTo().window(winHandle);
-        }
-
-        String actualSiteTitle = getDriver().findElement(By.xpath("//h1[@class='page-title']/span")).getText();
-        Assert.assertEquals(actualSiteTitle, expectedSiteTitle);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@rel='noopener noreferrer']")).getText(),"Jenkins 2.387.2");
     }
+
+    @Test
+    public void testVerifyJenkinsVersionOnManageJenkinsPage() {
+        ManageJenkinsPage manageJenkinsPage = new ManageJenkinsPage(getDriver())
+                .navigateToManageJenkinsPage()
+                .scrollToFooterPageByJenkinsVersionBTN();
+
+        Assert.assertTrue(manageJenkinsPage.getVersionJenkinsFromFooter(), "Wrong version Jenkins");
+    }
+
+    private static final String version = "Jenkins 2.387.2";
+    private static final By VERSION_NUMBER = By.xpath("//a[@href = 'https://www.jenkins.io/']");
+
+    public void assertVersion () {
+        Assert.assertEquals(getDriver().findElement(VERSION_NUMBER).getText(), version);
+    }
+
+
+    @Test
+    public void testVersionOnBuildHistoryPage () {
+        getDriver().findElement(By.xpath("//*[@id = 'tasks']/div[3]//a")).click();
+
+        assertVersion();
+    }
+
+    @Test
+    public void testVersionOnMyViewPage () {
+        getDriver().findElement(By.xpath("//*[@id = 'tasks']/div[5]//a")).click();
+
+        assertVersion();
+    }
+
+
+
+
+
+
+
+
+
 }
