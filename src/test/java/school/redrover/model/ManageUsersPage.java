798447ff145ea58e482830503bfd8b887ed97584
Ruslan Fursa <fursa_1986@mail.ru>
2023-05-24 16:36:41 +0300

    delete generate methods

--- src/test/java/school/redrover/model/ManageUsersPage.java
+++ src/test/java/school/redrover/model/ManageUsersPage.java
@@ -1,6 +1,5 @@
 package school.redrover.model;
 
-import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,29 +12,36 @@ import java.util.List;
 public class ManageUsersPage extends BasePage {
     @FindBy(xpath = "//a[@class ='jenkins-table__link model-link inside']")
     private WebElement usersTable;
+
     @FindBy(xpath = "//a[@href = '/manage']")
     private WebElement manageJenkinsTab;
+
     @FindBy(xpath = "//a[@href = 'securityRealm/']")
     private WebElement manageUsersSection;
+
     @FindBy(xpath = "//input[@name = 'username']")
     private WebElement userNameField;
+
     @FindBy(xpath = "//input[@name = 'password1']")
     private WebElement passwordField;
+
     @FindBy(xpath = "//input[@name = 'password2']")
     private WebElement confirmPasswordField;
+
     @FindBy(xpath = "//input[@name = 'fullname']")
     private WebElement fullNameField;
+
     @FindBy(xpath = "//input[@name = 'email']")
     private WebElement emailField;
+
     @FindBy(xpath = "//button[@name = 'Submit']")
     private WebElement submitBtn;
+
     @FindBy(xpath = "//a[@href='addUser']")
     private WebElement createUser;
 
     @FindBy(linkText = "Configure")
     private WebElement configureUserLink;
-    private final String password = generatePassword();
-    private final String userName = generateName();
 
     public ManageUsersPage(WebDriver driver) {
         super(driver);
@@ -74,29 +80,29 @@ public class ManageUsersPage extends BasePage {
         return this;
     }
 
-    public ManageUsersPage inputUsername() {
+    public ManageUsersPage inputUsername(String userName) {
         getWait5().until(ExpectedConditions.visibilityOf(userNameField));
         userNameField.sendKeys(userName);
         return this;
     }
 
-    public ManageUsersPage inputPassword() {
+    public ManageUsersPage inputPassword(String password) {
         passwordField.sendKeys(password);
         return this;
     }
 
-    public ManageUsersPage inputConfirmPassword() {
+    public ManageUsersPage inputConfirmPassword(String password) {
         confirmPasswordField.sendKeys(password);
         return this;
     }
 
-    public ManageUsersPage inputFullName() {
-        fullNameField.sendKeys(generateLastName());
+    public ManageUsersPage inputFullName(String lastName) {
+        fullNameField.sendKeys(lastName);
         return this;
     }
 
-    public ManageUsersPage inputEmail() {
-        emailField.sendKeys(generateEmail());
+    public ManageUsersPage inputEmail(String email) {
+        emailField.sendKeys(email);
         return this;
     }
 
@@ -105,28 +111,7 @@ public class ManageUsersPage extends BasePage {
         return this;
     }
 
-    private String generateName() {
-        Faker faker = new Faker();
-        return faker.name().firstName();
-    }
-
-    private String generatePassword() {
-        Faker faker = new Faker();
-        return faker.internet()
-                .password(5, 10, true, true, true);
-    }
-
-    private String generateLastName() {
-        Faker faker = new Faker();
-        return faker.name().lastName();
-    }
-
-    private String generateEmail() {
-        Faker faker = new Faker();
-        return faker.internet().emailAddress();
-    }
-
-    public boolean isUserExist() {
+    public boolean isUserExist(String userName) {
         List<WebElement> users = getDriver().findElements(By
                 .xpath("//a[@class ='jenkins-table__link model-link inside']"));
         for (WebElement el : users) {
