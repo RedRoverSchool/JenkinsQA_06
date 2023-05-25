d47c788768bf0e977285da258c1c219d3f6744db
Dashechka-blond <d.shevchenka@gmail.com>
2023-05-25 13:10:49 -0400

    Added method clickMultiBranchPipeline in ViewPage AT 00.05 | New Item > Create Multibranch Pipeline with using new item button

--- src/test/java/school/redrover/model/ViewPage.java
+++ src/test/java/school/redrover/model/ViewPage.java
@@ -117,5 +117,8 @@ public class ViewPage extends BasePage {
         return null;
     }
 
-
+    public ViewPage clickMultiBranchPipeline() {
+        click(getDriver().findElement(By.xpath("//span[text() ='Multibranch Pipeline']")));
+        return this;
+    }
 }
