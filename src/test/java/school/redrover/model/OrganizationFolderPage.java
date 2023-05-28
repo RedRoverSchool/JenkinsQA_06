57f906e7af998fa29f4dcc5f9310213bee77e2ac
Sergey.D <sergei.demyanenko@gmail.com>
2023-05-28 14:39:14 -0400

    Move page refactoring

--- src/test/java/school/redrover/model/OrganizationFolderPage.java
+++ src/test/java/school/redrover/model/OrganizationFolderPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.model.base.BaseModel;
 
-public class OrganizationFolderPage extends BaseMainHeaderPage {
+public class OrganizationFolderPage extends BaseMainHeaderPage<OrganizationFolderPage> {
     public OrganizationFolderPage(WebDriver driver) {
         super(driver);
     }
