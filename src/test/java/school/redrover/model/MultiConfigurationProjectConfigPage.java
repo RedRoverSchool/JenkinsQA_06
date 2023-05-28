554f7c1d66df2ceb40b89eb3e2d89ccec8bacf90
verafes <verafes@gmail.com>
2023-05-27 22:35:01 -0700

    AT 03.05 | Multi-configuration project > Configure old build https://trello.com/c/9B2q3yPq

--- src/test/java/school/redrover/model/MultiConfigurationProjectConfigPage.java
+++ src/test/java/school/redrover/model/MultiConfigurationProjectConfigPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseModel;
 
-public class MultiConfigurationProjectConfigPage extends BaseModel {
+public class MultiConfigurationProjectConfigPage extends MainPage {
 
     public MultiConfigurationProjectConfigPage(WebDriver driver) {
         super(driver);
@@ -32,4 +32,44 @@ public class MultiConfigurationProjectConfigPage extends BaseModel {
                 .findElement(By.cssSelector("[name='Submit']")))).click();
         return new MultiConfigurationProjectPage(getDriver());
     }
+
+    public MultiConfigurationProjectConfigPage clickOldBuildCheckBox(){
+        clickByJavaScript(getDriver()
+                .findElement(By.xpath("//span[@class='jenkins-checkbox']//input[@id='cb4']")));
+
+        return this;
+    }
+
+    public MultiConfigurationProjectConfigPage enterDaysToKeepBuilds(int number){
+        WebElement daysToKeepBuilds = getWait5().until(ExpectedConditions.elementToBeClickable(getDriver()
+                .findElement(By.xpath("//input[@name='_.daysToKeepStr']"))));
+        daysToKeepBuilds.click();
+        daysToKeepBuilds.sendKeys(String.valueOf(number));
+
+        return this;
+    }
+
+    public MultiConfigurationProjectConfigPage enterMaxNumOfBuildsToKeep(int number){
+        WebElement maxNumOfBuildsToKeepNumber = getWait5().until(ExpectedConditions.elementToBeClickable(getDriver()
+                .findElement(By.xpath("//input[@name='_.numToKeepStr']"))));
+        maxNumOfBuildsToKeepNumber.click();
+        maxNumOfBuildsToKeepNumber.sendKeys(String.valueOf(number));
+
+        return this;
+    }
+
+    public String getDaysToKeepBuilds(String attribute){
+        WebElement daysToKeepBuilds = getDriver()
+                .findElement(By.xpath("//input[@name='_.daysToKeepStr']"));
+
+        return daysToKeepBuilds.getAttribute(attribute);
+    }
+
+    public String getMaxNumOfBuildsToKeep(String attribute){
+        WebElement maxNumOfBuildsToKeepNumber = getDriver()
+                .findElement(By.xpath("//input[@name='_.numToKeepStr']"));
+
+        return maxNumOfBuildsToKeepNumber.getAttribute(attribute);
+    }
+
 }
