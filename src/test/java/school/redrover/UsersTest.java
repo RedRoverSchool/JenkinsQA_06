d62877b2c87c71d9974cd51f1bc470468de749fa
Dmitry E <KeDeVe@gmail.com>
2023-06-05 21:48:29 -0400

    yet another try

--- src/test/java/school/redrover/UsersTest.java
+++ src/test/java/school/redrover/UsersTest.java
@@ -4,14 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.*;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
