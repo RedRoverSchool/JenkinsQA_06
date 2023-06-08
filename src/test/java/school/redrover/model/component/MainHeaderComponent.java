f47b8be6bb177477b1b9be83ff720859d92fc455
Darya <darya.slinkova@gmail.com>
2023-06-08 21:09:30 +0300

    delete unused import statement

--- src/test/java/school/redrover/model/component/MainHeaderComponent.java
+++ src/test/java/school/redrover/model/component/MainHeaderComponent.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 import school.redrover.model.LoginPage;
 import school.redrover.model.MainPage;
 import school.redrover.model.PluginsPage;
