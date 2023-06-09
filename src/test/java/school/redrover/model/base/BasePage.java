d78141b5a74ca23cbefbfaefc14d3d7014c3b1d8
Kk-788 <kyrylo788@gmail.com>
2023-06-08 18:22:55 -0700

    Undo accidental deletion of generic parameter type restriction in BasePage

--- src/test/java/school/redrover/model/base/BasePage.java
+++ src/test/java/school/redrover/model/base/BasePage.java
@@ -2,7 +2,7 @@ package school.redrover.model.base;
 
 import org.openqa.selenium.WebDriver;
 
-public abstract class BasePage<Header, Breadcrumb> extends BaseModel {
+public abstract class BasePage<Header extends BaseComponent<?>, Breadcrumb extends BaseComponent<?>> extends BaseModel {
 
     public BasePage(WebDriver driver) {
         super(driver);
