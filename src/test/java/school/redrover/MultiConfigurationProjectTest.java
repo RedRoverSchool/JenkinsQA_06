89efdb9a877f3a0d1db26a17136a21ab8f81d3d2
Olga Kovalenko <gwch@tut.by>
2023-06-10 18:30:07 +0300

    RF 00.03| New Item > Multi Configuration project> Create Multi Configuration project with equal name>deletd from Ignore

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -44,7 +44,7 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(projectName.substring(8, 32), MULTI_CONFIGURATION_NAME);
     }
 
-    @Ignore
+
     @Test(dependsOnMethods = "testCreateProject")
     public void testCreateMultiConfigurationProjectWithEqualName() {
         final String ERROR_MESSAGE_EQUAL_NAME = "A job already exists with the name " + "‘" + MULTI_CONFIGURATION_NAME + "’";
@@ -60,7 +60,7 @@ public class MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(error, ERROR_MESSAGE_EQUAL_NAME);
     }
 
-    @Ignore
+
     @Test(dependsOnMethods = "testCreateProject")
     public void testRenameFromDropDownMenu() {
         String NewNameProject = new MainPage(getDriver())
