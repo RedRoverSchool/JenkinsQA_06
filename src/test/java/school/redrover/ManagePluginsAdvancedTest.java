1ab9166e1404bbc14cc26a1d6977961c2b8fe716
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-06-19 18:09:02 +0300

    RF ManagePluginsPage,create ManagePluginsAdvancedPage, add_FindBy

--- src/test/java/school/redrover/ManagePluginsAdvancedTest.java
+++ src/test/java/school/redrover/ManagePluginsAdvancedTest.java
@@ -8,18 +8,18 @@ import school.redrover.runner.BaseTest;
 public class ManagePluginsAdvancedTest extends BaseTest {
 
     @Test
-    public void testServerInfoBoxAdvancedSettingsPluginPage(){
-        final String expectedInfoServerText = """
+    public void testServerHelpInfo(){
+        final String expectedServerHelpInfo = """
                 If your Jenkins server sits behind a firewall and does not have the direct access to the internet, and if your server JVM is not configured appropriately ( See JDK networking properties for more details ) to enable internet connection, you can specify the HTTP proxy server name in this field to allow Jenkins to install plugins on behalf of you. Note that Jenkins uses HTTPS to communicate with the update center to download plugins.
                 Leaving this field empty means Jenkins will try to connect to the internet directly.
                 If you are unsure about the value, check the browser proxy configuration.""";
-        String ServerInfoBox = new MainPage(getDriver())
+        String ServerHelpInfo = new MainPage(getDriver())
                 .clickManageJenkinsPage()
                 .clickManagePlugins()
                 .clickAdvancedSettings()
                 .clickExtraInfoServerIcon()
                 .getExtraInfoServerTextBox();
 
-        Assert.assertEquals(ServerInfoBox, expectedInfoServerText);
+        Assert.assertEquals(ServerHelpInfo, expectedServerHelpInfo);
     }
 }
