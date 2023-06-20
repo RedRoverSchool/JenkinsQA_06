4783d9b8bb47a4e85467e76485096765552701c5
Mariia Rykova <maryrykova24@gmail.com>
2023-06-20 17:54:44 +0400

    RF | BaseProjectPage > Add FindBy > delete duplicate methods in children BaseProjectPage

--- src/test/java/school/redrover/model/BuildPage.java
+++ src/test/java/school/redrover/model/BuildPage.java
@@ -1,14 +1,10 @@
 package school.redrover.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BuildPage extends BaseMainHeaderPage<BuildPage> {
 
@@ -18,20 +14,9 @@ public class BuildPage extends BaseMainHeaderPage<BuildPage> {
     @FindBy(xpath = "//h1")
     private WebElement buildHeader;
 
-    @FindBy(xpath = "//label[@class='attach-previous ']")
-    private WebElement booleanParameterName;
-
-    @FindBy(xpath = "//input[@checked='true']")
-    private WebElement checkedParameter;
-
-    @FindBy(xpath = "//select[@name='value']/option")
-    private List<WebElement> choiceParametersList;
-
     @FindBy(xpath = "//div[@class='jenkins-form-description']")
     private WebElement description;
 
-    @FindBy(xpath = "//input[@name='value']")
-    private WebElement checkbox;
 
     public BuildPage(WebDriver driver) {
         super(driver);
@@ -49,15 +34,5 @@ public class BuildPage extends BaseMainHeaderPage<BuildPage> {
     public boolean isDisplayedBuildTitle() {
 
         return getBuildHeader().getText().contains("Build #1");
-    }
-
-    public EditBuildInformationPage clickEditBuildInformationButton(String projectName) {
-        getDriver().findElement(By.xpath("//*[@href = '/job/" + projectName + "/1/configure']")).click();
-        return new EditBuildInformationPage(getDriver());
-    }
-
-    public String getProjectDescription() {
-       return getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='description']/div[1]")))
-               .getText();
     }
 }
