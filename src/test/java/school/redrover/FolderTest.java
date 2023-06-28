d7d0e22ecfebe4ee0a783e8f0758f198c5fe47de
Artem <artem.savichev@mail.ru>
2023-06-28 18:47:29 +0600

    TC 04.04 | Folder > Configure > DisplayName - delete

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -54,7 +54,6 @@ public class FolderTest extends BaseTest {
 
     @Test
     public void testCreateFromCreateAJob() {
-
         MainPage mainPage = new MainPage(getDriver())
                 .clickCreateAJob()
                 .enterItemName(NAME)
@@ -77,7 +76,6 @@ public class FolderTest extends BaseTest {
 
     @Test
     public void testCreateFromDashboard() {
-
         MainPage mainPage = new MainPage(getDriver())
                 .getBreadcrumb()
                 .clickNewItemDashboardDropdownMenu()
@@ -93,7 +91,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testCreateFromCreateAJob")
     public void testCreateWithExistingName() {
-
         String errorMessage = new MainPage(getDriver())
                 .clickNewItem()
                 .enterItemName(NAME)
@@ -138,7 +135,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testCreateNewViewInFolder")
     public void testRenameUsingDropDownMenu() {
-
         boolean newNameIsDisplayed = new MainPage(getDriver())
                 .dropDownMenuClickRename(NAME, new FolderPage(getDriver()))
                 .enterNewName(NAME_2)
@@ -152,7 +148,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testRenameUsingDropDownMenu")
     public void testRenameToTheCurrentNameAndGetError() {
-
         CreateItemErrorPage createItemErrorPage = new MainPage(getDriver())
                 .clickJobName(NAME_2, new FolderPage(getDriver()))
                 .clickRename()
@@ -165,7 +160,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testRenameToTheCurrentNameAndGetError")
     public void testConfigureFolderNameDescriptionHealthMetrics() {
-
         FolderPage folderPage = new MainPage(getDriver())
                 .clickJobName(NAME_2, new FolderPage(getDriver()))
                 .clickConfigure()
@@ -179,9 +173,8 @@ public class FolderTest extends BaseTest {
         Assert.assertTrue(folderPage.clickConfigure().clickHealthMetrics().isRecursive());
     }
 
-    @Test(dependsOnMethods = "testConfigureFolderNameDescriptionHealthMetrics")
+    @Test(dependsOnMethods = "testDeleteDisplayName")
     public void testPreviewDescription() {
-
         String previewText = new MainPage(getDriver())
                 .clickJobName(NAME_2, new FolderPage(getDriver()))
                 .clickConfigure()
@@ -191,9 +184,8 @@ public class FolderTest extends BaseTest {
         Assert.assertEquals(previewText, DESCRIPTION);
     }
 
-    @Test(dependsOnMethods = "testConfigureFolderNameDescriptionHealthMetrics")
+    @Test(dependsOnMethods = "testPreviewDescription")
     public void testCancelDeleting() {
-
         boolean folderIsDisplayed = new MainPage(getDriver())
                 .clickJobName(NAME_2, new FolderPage(getDriver()))
                 .clickDeleteJobThatIsMainPage()
@@ -207,7 +199,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testCancelDeleting")
     public void testCreateJobsInFolder() {
-
         List<String> jobName = Arrays.asList("Freestyle_Project", "Pipeline project", "Multi Configuration Project",
                 "Folder", "Multibranch Pipeline", "Organization");
 
@@ -235,7 +226,6 @@ public class FolderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testCreateJobsInFolder")
     public void testDeleteFolder() {
-
         boolean welcomeIsDisplayed = new MainPage(getDriver())
                 .dropDownMenuClickDeleteFolders(NAME_2)
                 .clickYesButton()
@@ -315,5 +305,17 @@ public class FolderTest extends BaseTest {
                 .getJobList();
 
         Assert.assertTrue(folderName.contains(NAME));
+    }
+
+    @Test(dependsOnMethods = "testConfigureFolderNameDescriptionHealthMetrics")
+    public void testDeleteDisplayName() {
+        String folderName = new MainPage(getDriver())
+                .clickJobName(NAME_2, new FolderPage(getDriver()))
+                .clickConfigure()
+                .clearDisplayName()
+                .clickSaveButton()
+                .getJobName();
+
+        Assert.assertEquals(folderName, NAME_2);
     }
 }
