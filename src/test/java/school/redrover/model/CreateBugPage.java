590689e2fcff4dbc8af31c1ec8e35366a581d5ff
BNastasia <asya1551@gmail.com>
2023-06-29 07:55:06 -0400

    add extra line

--- src/test/java/school/redrover/model/CreateBugPage.java
+++ src/test/java/school/redrover/model/CreateBugPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class CreateBugPage extends BaseMainHeaderPage<CreateItemErrorPage> {
+
     @FindBy(xpath = "//div[@id = 'error-description']//h2")
     private WebElement errorMessage;
     public CreateBugPage(WebDriver driver) {
