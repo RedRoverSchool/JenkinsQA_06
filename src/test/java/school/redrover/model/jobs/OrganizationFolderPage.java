b99c698cded73d4f9bb93245d5c099a211021483
Anna Morozova <morozanna503@gmail.com>
2023-07-02 17:13:34 +0400

    added testScanOrgFolderLog() (#2794)

--- src/test/java/school/redrover/model/jobs/OrganizationFolderPage.java
+++ src/test/java/school/redrover/model/jobs/OrganizationFolderPage.java
@@ -2,10 +2,17 @@ package school.redrover.model.jobs;
 
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import school.redrover.model.ScanOrganizationFolderLog;
 import school.redrover.model.base.BaseOtherFoldersPage;
 import school.redrover.model.jobsconfig.OrganizationFolderConfigPage;
 
 public class OrganizationFolderPage extends BaseOtherFoldersPage<OrganizationFolderPage> {
+
+    @FindBy(xpath = "//a[contains(@href,'/computation/console')]")
+    private WebElement scanButton;
 
     public OrganizationFolderPage(WebDriver driver) {
         super(driver);
@@ -15,5 +22,11 @@ public class OrganizationFolderPage extends BaseOtherFoldersPage<OrganizationFol
     public OrganizationFolderConfigPage clickConfigure() {
         setupClickConfigure();
         return new OrganizationFolderConfigPage(this);
+    }
+
+    public ScanOrganizationFolderLog clickScanOrgFolderLog() {
+        getWait5().until(ExpectedConditions.elementToBeClickable(scanButton)).click();
+
+        return new ScanOrganizationFolderLog(getDriver());
     }
 }
