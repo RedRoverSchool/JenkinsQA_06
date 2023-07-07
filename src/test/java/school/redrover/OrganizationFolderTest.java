eefd98c0d928efbfe717d776c8edc3368dd5c04b
Vidim83 <08091985n@gmail.com>
2023-07-07 20:38:31 +0300

    RF | OrganizationFolder > RF testCreateOrgFolderWithDotInsteadOfName()

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -348,7 +348,7 @@ public class OrganizationFolderTest extends BaseTest {
                 .clickGeneratePipelineScriptButton()
                 .getTextPipelineScript();
 
-        Assert.assertEquals(pipelineSyntax, expectedText );
+        Assert.assertEquals(pipelineSyntax, expectedText);
     }
 
     @Test
@@ -363,7 +363,7 @@ public class OrganizationFolderTest extends BaseTest {
                 .getTextPipelineTitle();
 
         Assert.assertEquals(linkBookCreatingPipeline, "Creating a Jenkins Pipeline");
-        Assert.assertEquals(pipelineOneTutorial,"Pipeline");
+        Assert.assertEquals(pipelineOneTutorial, "Pipeline");
     }
 
     @Test(dataProvider = "wrong-character")
@@ -399,14 +399,14 @@ public class OrganizationFolderTest extends BaseTest {
 
     @Test
     public void testOrganizationFolderEvents() {
-        TestUtils.createJob(this,ORGANIZATION_FOLDER_NAME, TestUtils.JobType.OrganizationFolder, true);
+        TestUtils.createJob(this, ORGANIZATION_FOLDER_NAME, TestUtils.JobType.OrganizationFolder, true);
 
         String eventTitle = new MainPage(getDriver())
                 .clickJobName(ORGANIZATION_FOLDER_NAME, new OrganizationFolderPage(getDriver()))
                 .clickOrgFolderEvents()
                 .getTextFromTitle();
 
-        Assert.assertEquals(eventTitle,"Organization Folder Events");
+        Assert.assertEquals(eventTitle, "Organization Folder Events");
     }
 
     @Test(dependsOnMethods = "testCreateFromCreateAJob")
@@ -420,7 +420,7 @@ public class OrganizationFolderTest extends BaseTest {
     }
 
     @Test
-    public void testCreateOrgFolderWithDotInsteadOfName() {
+    public void testCreateWithDotInsteadOfName() {
         String errorMessage = new MainPage(getDriver())
                 .clickNewItem()
                 .enterItemName(".")
