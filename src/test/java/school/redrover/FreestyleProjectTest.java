9cb881aa3839166eb1e34109b9445d0c725a7847
Artem <artem.savichev@mail.ru>
2023-07-10 21:30:19 +0600

    AT 01.03 | FreestyleProject > Build Now > testConsoleOutputFromBuildPage

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -811,4 +811,16 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(displayName, "Project " + NEW_FREESTYLE_NAME);
     }
+
+    @Test(dependsOnMethods = "testDeleteBuildNowFromBuildPage")
+    public void testConsoleOutputFromBuildPage() {
+        boolean consoleOutputTitleDisplayed = new MainPage(getDriver())
+                .clickPlayBuildForATestButton(FREESTYLE_NAME)
+                .clickJobName(FREESTYLE_NAME, new FreestyleProjectPage(getDriver()))
+                .clickLastBuildLink()
+                .clickConsoleOutput()
+                .isDisplayedBuildTitle();
+
+        Assert.assertTrue(consoleOutputTitleDisplayed, "Error: Console Output Title is not displayed!");
+    }
 }
