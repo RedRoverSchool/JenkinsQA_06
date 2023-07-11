b1de54723682b419ad0d76a3cd29775756cca757
Anna Morozova <morozanna503@gmail.com>
2023-07-11 19:57:24 +0400

    AT 06.04 | OrganizationFolder > testCreateFromMyViewsCreateAJobArrow (#2967)
    * added testCreateFromMyViewsCreateAJobArrow
    
    * added testCreateFromMyViewsCreateAJobArrow
    
    * AT | OrganizationFolder | Create job >testCreateFromCreateAJobArrow (#2966)
    
    * added testCreateFromMyViewsCreateAJobArrow
    
    ---------
    
    Co-authored-by: AnastasiyaAbramova <128148850+AnastasiyaAbramova@users.noreply.github.com>

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -6,8 +6,10 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.*;
+import school.redrover.model.jobs.FolderPage;
 import school.redrover.model.jobs.FreestyleProjectPage;
 import school.redrover.model.jobs.OrganizationFolderPage;
+import school.redrover.model.jobsconfig.FolderConfigPage;
 import school.redrover.model.jobsconfig.FreestyleProjectConfigPage;
 import school.redrover.model.jobsconfig.OrganizationFolderConfigPage;
 import school.redrover.runner.BaseTest;
@@ -545,5 +547,21 @@ public class OrganizationFolderTest extends BaseTest {
 
         Assert.assertTrue(projectPageFromCreateAJobArrow, "Error: the Organization Folder name is not displayed on Dashboard" );
 
+    }
+
+    @Test
+    public void testCreateFromMyViewsCreateAJobArrow(){
+        MainPage projectName = new MainPage(getDriver())
+                .clickMyViewsSideMenuLink()
+                .clickCreateAJobArrow()
+                .enterItemName(ORGANIZATION_FOLDER_NAME)
+                .selectJobType(TestUtils.JobType.OrganizationFolder)
+                .clickOkButton(new OrganizationFolderConfigPage(new OrganizationFolderPage(getDriver())))
+                .getHeader()
+                .clickLogo();
+
+        Assert.assertTrue(projectName.jobIsDisplayed(ORGANIZATION_FOLDER_NAME), "Error: the Organization Folder name is not displayed on Dashboard from Home page");
+        Assert.assertTrue(projectName.clickMyViewsSideMenuLink()
+                .jobIsDisplayed(ORGANIZATION_FOLDER_NAME), "Error: the Organization Folder name is not displayed on Dashboard from MyViews page");
     }
 }
